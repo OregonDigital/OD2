a71320a5431ec7d5d9acd61077d7d1972d8700a0
Josh Gum <revgum@gmail.com>
2018-09-06 08:25:18 -0700

    adds staging and default configurations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module OD2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.active_job.queue_adapter = ENV.fetch('ACTIVE_JOB_QUEUE_ADAPTER', 'sidekiq').to_sym
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ config/puma/staging.rb
@@ -0,0 +1,16 @@
+directory '/data0/hydra/current'
+bind 'tcp://127.0.0.1:3000'
+pidfile '/data0/hydra/shared/tmp/puma/puma.pid'
+state_path '/data0/hydra/shared/tmp/puma/puma.state'
+workers 4
+preload_app!
+
+environment 'staging'
+stdout_redirect '/data0/hydra/shared/log/puma.log', '/data0/hydra/shared/log/puma.err.log', true
+
+daemonize true
+# Allow for `touch tmp/restart.txt` to force puma to restart the app
+plugin :tmp_restart
+
+# Activate the puma control application, mapping location in the nginx config
+activate_control_app 'unix:///data0/hydra/shared/sockets/pumactl.sock'
