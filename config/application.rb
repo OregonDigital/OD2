c04b5cceeb8556899b8a84e0ae671d1e86031104
Josh Gum <revgum@gmail.com>
2018-11-09 11:02:38 -0800

    adds application version to OD2 for use by system and deployment

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,15 +13,19 @@ module OD2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.autoload_paths << Rails.root.join('lib')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # load and inject local_env.yml key/values into ENV
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      if File.exist? env_file
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
     end
   end
 end
---
+++ lib/oregon_digital/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal:true
+
+module OregonDigital
+  VERSION = 'WC2-0.0.1'
+end
