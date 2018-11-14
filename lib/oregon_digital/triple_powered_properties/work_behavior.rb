9b23fdf723dd7c262af6ed78865740cb12c96eda
straleyb <straleyb@onid.oregonstate.edu>
2018-11-14 13:25:10 -0800

    adds objects to SA

---
+++ lib/oregon_digital/triple_powered_properties/work_behavior.rb
@@ -0,0 +1,88 @@
+module OregonDigital::TriplePoweredProperties
+  module WorkBehavior
+    extend ActiveSupport::Concern
+
+    included do
+
+      # store the triple powered property labels in SOLR
+      self.indexer = ScholarsArchive::TriplePoweredProperties::WorkIndexer
+
+      # server side validation for triple powered property values being valid urls
+      self.validates_with ScholarsArchive::TriplePoweredProperties::HasUrlValidator
+
+      class_attribute :triple_powered_properties
+
+      # fetch the graphs and store them in the triple store cache before saving the work
+      before_save :fetch_graphs
+
+      def triple_powered_properties
+        [:based_near]
+      end
+    end
+
+    ##
+    # Queries the labels for each graph associated with the triple powered property
+    #
+    # @param key [Symbol] the property uri with associated graphs
+    # @return [Hash<String,Array<String>>] a hash keyed on the URI with its labels as the value
+    def uri_labels(property)
+      labels = {}
+
+      return labels if uri_graphs.nil?
+      return labels if uri_graphs[property].nil?
+
+      uri_graphs[property].each do |h|
+        result = h[:result]
+        uri = h[:uri]
+        labels[uri] = []
+        if result.is_a?(String)
+          labels[uri] << result
+        else
+          labels[uri] << ScholarsArchive::TriplePoweredProperties::Triplestore.predicate_labels(result).values.flatten.compact
+        end
+      end
+      labels
+    end
+
+    def uri_graphs
+      @uri_graphs ||= build_uri_graphs
+    end
+
+    private
+
+    ##
+    # Connect to the triplestore backend, and fetch the graphs
+    def fetch_graphs
+      uri_graphs
+    end
+
+    ##
+    # Build a hash, keyed on the property symbol, of graphs related to each triple powered property
+    def build_uri_graphs
+      graphs = {}
+      # iterate through each of the properties having RDF URIs and fetch the graphs from the triplestore
+      self.triple_powered_properties.each do |property|
+        graphs[property] = fetch(property)
+      end
+      graphs
+    end
+
+    ##
+    # Fetch the graphs for each URI value set on the property
+    # @param [Symbol] the property which is triple powered
+    # @return [Array<String>] the array of graphs for each URI value in the property
+    #                         OR the original URI if fetching the graphs fails (invalid URI, for instance)
+    def fetch(property)
+      # iterate through each of the uri's stored in the property of this model
+      self[property].map do |uri|
+        begin
+          # Fetch the graph from the triplestore, or return the uri
+          graph = ScholarsArchive::TriplePoweredProperties::Triplestore.fetch(uri)
+          { uri: uri, result: graph }
+        rescue TriplestoreAdapter::TriplestoreException
+          { uri: uri, result: uri }
+        end
+      end
+    end
+  end
+end
---
+++ spec/oregon_digital/triple_powered_properties/work_behavior_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe OregonDigital::TriplePoweredProperties::WorkBehavior do
+  let(:url) { 'http://opaquenamespace.org/ns/TestVocabulary/TestTerm' }
+  subject { DefaultWork.new({ based_near: [ url ], title: ['TestTest'] }) }
+  let(:label) { 'hello' }
+
+  before do
+    allow(subject).to receive(:uri_graphs).and_return({:based_near=>[{:uri=>url, :result=>build_graph}]})
+  end
+
+  describe '#uri_labels' do
+    it 'should have triple powered properties with labels' do
+      expect(subject.triple_powered_properties).to be_a_kind_of(Array)
+      expect(subject.uri_labels(:based_near)[url]).to be_a_kind_of(Array)
+      #TODO Check to see if we want this actually nested arrays... [[]]
+      expect(subject.uri_labels(:based_near)[url].flatten).to include(label)
+    end
+  end
+end
+
+def build_graph
+  graph = RDF::Graph.new
+  graph << RDF::Statement.new(RDF::URI.new('http://opaquenamespace.org/ns/TestVocabulary/TestTerm'), RDF::Vocab::RDFS.label, label)
+  graph
+end
