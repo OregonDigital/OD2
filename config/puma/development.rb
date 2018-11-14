00450fca5e5403f60a6c0fe5811531239a986418
Josh Gum <revgum@gmail.com>
2018-11-14 11:32:23 -0800

    stabilize docker-compose configuration for development replaces unnecessary configs conflicting with CI

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -1,3 +1,8 @@
+bind 'tcp://0.0.0.0:3000'
 # worker pools kill the capability for ruby-debug-ide integration
 # workers 1
 preload_app!
+environment 'development'
+daemonize true
+# Allow for `touch tmp/restart.txt` to force puma to restart the app
+plugin :tmp_restart
--- config/puma/test.rb
+++
@@ -1,6 +0,0 @@
-directory '/data'
-bind 'tcp://0.0.0.0:3001'
-pidfile '/data/tmp/pids/puma.pid'
-preload_app!
-environment 'test'
-daemonize true
