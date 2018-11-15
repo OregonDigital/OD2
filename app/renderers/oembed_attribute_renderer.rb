9d29a6e4c14b9b9727b88897b367a10e40418cee
Corey Gillen <gillenc@uoregon.edu>
2018-11-15 09:53:46 -0800

    Error handling for non-embeddable urls

--- app/renderers/oembed_attribute_renderer.rb
+++ app/renderers/oembed_attribute_renderer.rb
@@ -1,6 +1,12 @@
 class OembedAttributeRenderer < Hyrax::Renderers::AttributeRenderer
     def attribute_value_to_html(value)
-      resource = OEmbed::Providers.get(value)
-      return resource.html
+      html = ""
+      begin
+        resource = OEmbed::Providers.get(value)
+        html << resource.html
+      rescue OEmbed::NotFound => e
+        html << "No embeddable content at '#{value}'"
+      end
+      return html
     end
 end
