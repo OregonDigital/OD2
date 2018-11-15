e65c7d47c3cb4fd6995cb7c03deca0c86338a556
straleyb <straleyb@onid.oregonstate.edu>
2018-11-15 13:25:45 -0800

    Apply classes to models

--- app/forms/hyrax/generic_form.rb
+++ app/forms/hyrax/generic_form.rb
@@ -3,6 +3,8 @@
 module Hyrax
   # Generated form for Generic
   class GenericForm < Hyrax::Forms::WorkForm
+    include ::ScholarsArchive::TriplePoweredProperties::TriplePoweredForm
+
     self.model_class = ::Generic
     self.terms += [:resource_type]
   end
