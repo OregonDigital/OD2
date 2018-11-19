798e9c514edd79df31c1a5a0914e89f18edf3cf7
straleyb <straleyb@onid.oregonstate.edu>
2018-11-19 07:44:19 -0800

    rename from od2 to oregon digital for ease

--- app/indexers/concerns/oregon_digital/indexes_linked_metadata.rb
+++ app/indexers/concerns/oregon_digital/indexes_linked_metadata.rb
@@ -1,9 +1,9 @@
-module OD2
+module OregonDigital
   # This module can be mixed in on an indexer in order to index the linked metadata fields
   module IndexesLinkedMetadata
     # We're overriding a method from Hyrax::IndexesLinkedMetadata
     def rdf_service
-      OD2::OdDeepIndexingService
+      OregonDigital::OdDeepIndexingService
     end
   end
 end
--- app/indexers/oregon_digital/od_deep_indexing_service.rb
+++ app/indexers/oregon_digital/od_deep_indexing_service.rb
@@ -1,5 +1,5 @@
 require 'linkeddata' # we need all the linked data types, because we don't know what types a service might return.
-module OD2
+module OregonDigital
   class OdDeepIndexingService < Hyrax::DeepIndexingService
   end
 end
