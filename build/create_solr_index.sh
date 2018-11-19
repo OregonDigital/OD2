69cf3ca4eca7861f4a0c434ec0bf64afe4f2c60e
Josh Gum <revgum@gmail.com>
2018-11-19 11:20:56 -0800

    retry curl allowing for solr time to come alive

--- build/create_solr_index.sh
+++ build/create_solr_index.sh
@@ -8,14 +8,15 @@ fi
 
 SOLR_HOST=$(echo $SOLR_URL | cut -d '/' -f 1,2,3)
 
+# Upload the hyrax SOLR configurations
+curl --retry 3 --retry-delay 5 --retry-connrefused -H "Content-type:application/octet-stream" --data-binary @config/solr/config.zip "${SOLR_HOST}/solr/admin/configs?action=UPLOAD&name=hyrax"
+
 # SOLR 6.x URL to check cluster status
 # Check if the target solr collection exists, create it unless a "200" is returned
 STATUS_CODE=$(curl -I "${SOLR_HOST}/solr/admin/collections?action=CLUSTERSTATUS&collection=${RAILS_ENV}" 2>/dev/null | head -n 1 | cut -d ' ' -f 2)
 
 if [ "${STATUS_CODE}" != '200' ]; then
   echo "SOLR collection admin returned ${STATUS_CODE} for '${RAILS_ENV}', creating collection."
-  # Upload the hyrax SOLR configurations
-  curl -H "Content-type:application/octet-stream" --data-binary @config/solr/config.zip "${SOLR_HOST}/solr/admin/configs?action=UPLOAD&name=hyrax"
   # SOLR 6.x URL to create a collection (which includes a core)
   curl "${SOLR_HOST}/solr/admin/collections?action=CREATE&name=${RAILS_ENV}&numShards=1&collection.configName=hyrax"
 else
