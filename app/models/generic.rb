b5fb80d7c3a11d4454a05e7b4a5702943d417ad2
straleyb <straleyb@onid.oregonstate.edu>
2018-11-19 12:08:33 -0800

    remove triple powered work behavior

--- app/models/generic.rb
+++ app/models/generic.rb
@@ -6,6 +6,7 @@ class Generic < ActiveFedora::Base
   include ::OD2::GenericMetadata
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
+  self.indexer = GenericIndexer
 
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
