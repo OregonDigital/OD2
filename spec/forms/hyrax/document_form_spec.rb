c3cdbab04ec3e89a69b0b0964916546ff2ed3712
Corey Gillen <gillenc@uoregon.edu>
2018-11-21 08:53:27 -0800

    Add some basic tests for model and presenter

--- spec/forms/hyrax/document_form_spec.rb
+++ spec/forms/hyrax/document_form_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Hyrax::DocumentForm do
   end
 
   it 'responds to terms with the proper list of terms' do
-      %i[contained_in_journal first_line first_line_chorus has_number host_item instrumentation is_volume larger_work number_of_pages table_of_contents].each do |t|
+    %i[contained_in_journal first_line first_line_chorus has_number host_item instrumentation is_volume larger_work number_of_pages table_of_contents].each do |t|
       expect(described_class.terms).to include(t)
     end
   end
--- spec/presenters/hyrax/document_presenter_spec.rb
+++ spec/presenters/hyrax/document_presenter_spec.rb
@@ -3,7 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::DocumentPresenter do
-  it "has tests" do
-    skip "Add your tests here"
+  subject { described_class.new(double, double) }
+
+  %i[contained_in_journal first_line first_line_chorus has_number host_item instrumentation is_volume larger_work number_of_pages table_of_contents].each do |t|
+    it { is_expected.to delegate_method(t).to(:solr_document) }
   end
 end
