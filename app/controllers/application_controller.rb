74df3b292d8db2c5f721c9db4b4fa043cbecdccb
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2018-11-21 11:00:38 -0800

    Adding Honeycomb integration with libhoney and honeycomb-rails (#131)
    * Adding Honeycomb integration with libhoney and honeycomb-rails
    
    * Wrapped honeycomb bits, added honeycomb key and dataset name to env
    
    * Adjust environments where Honeycomb gets initialized
    
    * Add env vars for Honeycomb to CircleCI to fix tests
    
    * Cleanup for PR #131
    
    * Removed Honeycomb env vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,17 @@ class ApplicationController < ActionController::Base
   skip_after_action :discard_flash_if_xhr
 
   protect_from_forgery with: :exception
+
+  # Append some data into Honeycomb events to support tracing
+  if %w[production staging].include? Rails.env
+    def append_info_to_payload(payload)
+      super(payload)
+      honeycomb_metadata["trace.trace_id"] = request.request_id
+      honeycomb_metadata["trace.span_id"] = request.request_id
+      honeycomb_metadata[:service_name] = "rails"
+      honeycomb_metadata[:request_id] = request.request_id
+      honeycomb_metadata[:request_uuid] = request.uuid
+      honeycomb_metadata[:name] = self.class.name
+    end
+  end
 end
