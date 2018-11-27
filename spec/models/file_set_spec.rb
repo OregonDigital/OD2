c57ba8c36d216cc20529b3d2ce45b2727695e032
Josh Gum <revgum@gmail.com>
2018-11-27 08:58:10 -0800

    fixed var name for clarity

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -3,16 +3,16 @@
 RSpec.describe FileSet do
   subject { create(:file_set) }
   let(:encoded_id) { subject.id.scan(/.{1,2}/).join('%2f') }
-  let(:iif_url) { "#{ENV.fetch('IIIF_SERVER_BASE_URL', 'http://bogus')}/#{encoded_id}" }
+  let(:iiif_url) { "#{ENV.fetch('IIIF_SERVER_BASE_URL', 'http://bogus')}/#{encoded_id}" }
 
   it 'creates a valid jpg iiif url' do
-    expect(subject.iiif_url('http://bogus')).to eq("#{iif_url}-jpg.jpg")
+    expect(subject.iiif_url('http://bogus')).to eq("#{iiif_url}-jpg.jpg")
   end
 
   context 'for a mocked jp2' do
     it 'creates a valid jp2 iif url' do
       allow(subject).to receive(:derivative_path_for_reference).and_return(File.join(Rails.root, 'spec', 'fixtures', 'test.jpg'))
-      expect(subject.iiif_url('http://bogus')).to eq("#{iif_url}-jp2.jp2")
+      expect(subject.iiif_url('http://bogus')).to eq("#{iiif_url}-jp2.jp2")
     end
   end
 end
