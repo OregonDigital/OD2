93901b34990c59885e229253ad1dae135db3923f
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2018-11-27 15:59:18 -0800

    updated image presenter

--- app/presenters/hyrax/image_presenter.rb
+++ app/presenters/hyrax/image_presenter.rb
@@ -4,6 +4,5 @@ module Hyrax
   class ImagePresenter < Hyrax::WorkShowPresenter
     delegate *OregonDigital::GenericMetadata::PROPERTIES.map(&:to_sym), to: :solr_document 
     delegate *OregonDigital::ImageMetadata::PROPERTIES.map(&:to_sym), to: :solr_document 
-    delegate :creator, :keyword, to: :solr_document
   end
 end
