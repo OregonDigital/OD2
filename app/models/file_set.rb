7c5fff351c7b0ce37e4d6b7c2cace6438bb84c45
Josh Gum <revgum@gmail.com>
2018-11-27 08:22:49 -0800

    adds spec to cover file_set enhancement

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -8,12 +8,18 @@ class FileSet < ActiveFedora::Base
   # @return [String] : a url to the iiif server with a properly encoded identifier, looks like http://IIIFSERVER/ab%2fcd%2f12%2f24%2fz-jp2.jp2
   def iiif_url(base_url)
     iiif_server_base_url = ENV.fetch('IIIF_SERVER_BASE_URL', base_url)
-    jp2_file = Hyrax::DerivativePath.derivative_path_for_reference(self, 'jp2')
+    jp2_file = derivative_path_for_reference('jp2')
     file_type = File.exist?(jp2_file) ? '-jp2.jp2' : '-jpg.jpg'
 
     iiif_identifier = id.scan(/.{1,2}/).join('%2f')
     # iiif_identifier : ab%2fcd%2f12%2f24%2fz-jp2.jp2
     iiif_identifier += file_type
     "#{iiif_server_base_url}/#{iiif_identifier}"
+  end
+
+  private
+
+  def derivative_path_for_reference(destination_name)
+    Hyrax::DerivativePath.derivative_path_for_reference(self, destination_name)
   end
 end
