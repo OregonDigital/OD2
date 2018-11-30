a9e238161093cf386ae207d166b58cf804281fbf
Josh Gum <revgum@gmail.com>
2018-11-30 10:34:03 -0800

    adds a production/staging solr script for deployment

--- build/create_solr_index_production_staging.sh
+++ build/create_solr_index_production_staging.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-set -e
 
-if [ "${RAILS_ENV}" = 'production' ] || [ "${RAILS_ENV}" = 'staging' ]; then
-  echo "Cannot auto-create SOLR collection for ${RAILS_ENV}, exiting"
-  exit 1
-fi
+# This script is intended to be used when first bringing up a production or staging cluster.
+# Existing production or staging clusters have persistent volumes backing SOLR so the collection
+# should not need to be created.
+set -e
 
 SOLR_HOST=$(echo $SOLR_URL | cut -d '/' -f 1,2,3)
 
@@ -14,11 +13,12 @@ curl --retry 3 --retry-delay 5 --retry-connrefused -H "Content-type:application/
 # SOLR 7.x URL to check cluster status
 # Check if the target solr collection exists, create it unless a "200" is returned
 STATUS_CODE=$(curl -I "${SOLR_HOST}/solr/admin/collections?action=CLUSTERSTATUS&collection=${RAILS_ENV}" 2>/dev/null | head -n 1 | cut -d ' ' -f 2)
+DATA_DIR="/opt/solr/server/data/${RAILS_ENV}"
 
 if [ "${STATUS_CODE}" != '200' ]; then
   echo "SOLR collection admin returned ${STATUS_CODE} for '${RAILS_ENV}', creating collection."
   # SOLR 7.x URL to create a collection (which includes a core)
-  curl "${SOLR_HOST}/solr/admin/collections?action=CREATE&name=${RAILS_ENV}&numShards=1&collection.configName=hyrax"
+  curl "${SOLR_HOST}/solr/admin/collections?action=CREATE&name=${RAILS_ENV}&numShards=1&collection.configName=hyrax&property.dataDir=${DATA_DIR}"
 else
   echo "SOLR collection ${RAILS_ENV} exists."
   exit 0
