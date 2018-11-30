41dcd261a8f919622abc54795ec235e285f7a81b
Corey Gillen <gillenc@uoregon.edu>
2018-11-30 09:26:15 -0800

    Controller spec
    Controller fixes

--- app/controllers/oregon_digital/oembeds_controller.rb
+++ app/controllers/oregon_digital/oembeds_controller.rb
@@ -1,6 +1,6 @@
 module OregonDigital
   class OembedsController < ApplicationController
-    
+
     attr_accessor :curation_concern
     helper_method :curation_concern
     load_and_authorize_resource class: ActiveFedora::Base, instance_name: :curation_concern
@@ -16,6 +16,11 @@ module OregonDigital
       add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
       add_breadcrumb t(:'oregon_digital.oembeds.index.manage_oembeds'), Rails.application.routes.url_helpers.oembeds_path
       add_breadcrumb t(:'oregon_digital.oembeds.edit.oembed_update'), '#'
+    end
+
+    # This is an override of Hyrax::ApplicationController
+    def deny_access(exception)
+      redirect_to root_path, alert: exception.message
     end
 
     with_themed_layout 'dashboard'
--- spec/controllers/oregon_digital/oembeds_controller_spec.rb
+++ spec/controllers/oregon_digital/oembeds_controller_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe OregonDigital::OembedsController do
   let(:user) { create(:user) }
+  let(:a_work) { create(:work, user: user) }
+  let(:not_my_work) { create(:work) }
   before { sign_in user }
 
   describe '#index' do
@@ -10,6 +12,27 @@ RSpec.describe OregonDigital::OembedsController do
       get :index
       expect(response).to be_success
       expect(response).to render_template('dashboard')
+    end
+  end
+
+  describe '#edit' do
+    context 'when I do not have edit permissions for the object' do
+      it 'redirects' do
+        get :edit, params: { id: not_my_work }
+        expect(response.status).to eq 302
+        expect(flash[:alert]).to eq 'You are not authorized to access this page.'
+      end
+    end
+    context 'when I have permission to edit the object' do
+      it 'shows me the page' do
+        expect(controller).to receive(:add_breadcrumb).with('Home', subject.root_path)
+        expect(controller).to receive(:add_breadcrumb).with('Dashboard', subject.hyrax.dashboard_path)
+        expect(controller).to receive(:add_breadcrumb).with('Manage oEmbeds', Rails.application.routes.url_helpers.oembeds_path)
+        expect(controller).to receive(:add_breadcrumb).with('Update oEmbed', '#')
+        get :edit, params: { id: a_work }
+        expect(response).to be_success
+        expect(response).to render_template('dashboard')
+      end
     end
   end
 end
