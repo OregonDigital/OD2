1a86981bd91cc30d0f996ac87e160978bac86c4a
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2018-11-30 15:38:03 -0800

    Wrap the Honeycomb metadata push for just staging/prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,11 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def append_info_to_payload(payload)
-    super(payload)
-    Rack::Honeycomb.add_field(request.env, 'version', ENV.fetch('DEPLOYED_VERSION', OregonDigital::VERSION))
-    Rack::Honeycomb.add_field(request.env, 'classname', self.class.name)
+  if %w[production staging].include? Rails.env
+    def append_info_to_payload(payload)
+      super(payload)
+      Rack::Honeycomb.add_field(request.env, 'version', ENV.fetch('DEPLOYED_VERSION', OregonDigital::VERSION))
+      Rack::Honeycomb.add_field(request.env, 'classname', self.class.name)
+    end
   end
 end
