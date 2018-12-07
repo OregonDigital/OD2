f54da3ff38efa2b6954b5c80056a07bf87c750d4
Corey Gillen <gillenc@uoregon.edu>
2018-12-07 15:33:24 -0800

    Cache oEmbed embeddable response

--- app/controllers/blacklight/oembed/embed_controller.rb
+++ app/controllers/blacklight/oembed/embed_controller.rb
@@ -1,3 +1,6 @@
+# Override blacklight-oembed gem controller. We're only interested in
+# get_embed_content, but keep the changes compatible incase we can use 
+# Blacklight later
 module Blacklight::Oembed
   class EmbedController < ActionController::Base
 
@@ -9,7 +12,10 @@ module Blacklight::Oembed
 
     def get_embed_content(url, add_params)
       begin
-        OEmbed::Providers.get(url, **add_params).html.html_safe
+        # Retrieve embeddable content from cache, or store if not found
+        Rails.cache.fetch("embed/#{url}", expires_in: 12.hours) do
+          OEmbed::Providers.get(url, **add_params).html.html_safe
+        end
       rescue OEmbed::NotFound => e
         # Create OembedError for oEmbed Errors dashboard
         works = Generic.search_with_conditions(oembed_url: url)
