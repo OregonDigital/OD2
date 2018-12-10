5642a75cc798a2858d67fc288a1300e3a9794a89
Josh Gum <revgum@gmail.com>
2018-12-10 14:07:36 -0800

    style fix

--- config/initializers/libhoney.rb
+++ config/initializers/libhoney.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal:true
+
 Libhoney::LogTransmissionClient.class_eval do
   def add(event)
     if @verbose
-      metadata = "Honeycomb dataset '#{event.dataset}' | #{event.timestamp.iso8601}"
-      metadata << " (sample rate: #{event.sample_rate})" if event.sample_rate != 1
-      @output.print("#{metadata} | ")
+      data = "Honeycomb dataset '#{event.dataset}' | #{event.timestamp.iso8601}"
+      data << " (sample rate: #{event.sample_rate})" if event.sample_rate != 1
+      @output.print("#{data} | ")
     end
     @output.puts(event.data.to_s.to_json)
   end
