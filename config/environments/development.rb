dbd41d41159c76a4080ac6bd8af3ef1ba5d5aa91
straleyb <straleyb@onid.oregonstate.edu>
2018-12-12 12:32:44 -0800

    set up random email for tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,11 +44,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  domain: ENV['GMAIL_DOMAIN'],
   authentication: 'plain',
   enable_starttls_auto: true,
-  user_name: ENV['GMAIL_USERNAME'],
-  password: ENV['GMAIL_PASSWORD']
+  user_name: 'etstestmail123',
+  password: 'etstest123'
   }
 
   # Print deprecation notices to the Rails logger.
