0ea478a82fc93dd6ddba613f45d113076e83bf86
straleyb <straleyb@onid.oregonstate.edu>
2018-12-14 12:29:12 -0800

    adds more style update

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Users::OmniauthCallbacksController, type: :controller do
   let(:user) { create(:user) }
   let(:user_ob) { User }
   let(:provider) { instance_double('Provider') }
-  let(:merged_env) { request.env.merge( 'omniauth.auth' => provider ) }
+  let(:merged_env) { request.env.merge('omniauth.auth' => provider) }
   let(:req) { @request }
 
   context 'when #cas' do
