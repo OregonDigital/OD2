976d63483529c33449a504f8053cc3dfc6d23a57
straleyb <straleyb@onid.oregonstate.edu>
2018-12-17 09:40:20 -0800

    remove rails helper and move non fixing things to rubocop yml

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
   let(:user) { create(:user) }
   let(:user_ob) { User }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe User do
   let(:user) { described_class }
   let(:access_token) { instance_double('AccessToken') }
