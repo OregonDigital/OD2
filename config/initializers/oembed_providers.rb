f915057e57a5a1dceb12015d135cc58e8855f9bb
Corey Gillen <corey.j.gillen@gmail.com>
2018-12-19 11:50:46 -0800

    Add OSU MediaSpace provider (#232)
    * Add OSU MediaSpace provider
    
    * Test oEmbed provider initialization
    
    * Make new spec folder for config and initializers
    
    * Fix test

--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -3,3 +3,10 @@
 require 'oembed'
 
 OEmbed::Providers.register_all
+
+## Register OSU Mediaspace Provider
+osu_ms_provider = OEmbed::Provider.new('https://media.oregonstate.edu/oembed')
+osu_ms_provider << 'https://media.oregonstate.edu/id*'
+osu_ms_provider << 'https://media.oregonstate.edu/media/id*'
+osu_ms_provider << 'https://media.oregonstate.edu/media/t*'
+OEmbed::Providers.register(osu_ms_provider)
