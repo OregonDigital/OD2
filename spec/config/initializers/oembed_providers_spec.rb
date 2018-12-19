703934132e7ea506780aa98a625cf082dfe8809b
Corey Gillen <gillenc@uoregon.edu>
2018-12-19 11:23:27 -0800

    Fix test

--- spec/config/initializers/oembed_providers_spec.rb
+++ spec/config/initializers/oembed_providers_spec.rb
@@ -2,26 +2,20 @@
 
 RSpec.describe 'Oembed Provider Initialization' do
   it 'registers https://media.oregonstate.edu/id*' do
-    let(:provider) do
+    expect(
       OEmbed::Providers.find('https://media.oregonstate.edu/id/test')
-    end
-
-    expect(provider).not_to be_nil
+    ).not_to be_nil
   end
 
   it 'registers https://media.oregonstate.edu/media/id*' do
-    let(:provider) do
+    expect(
       OEmbed::Providers.find('https://media.oregonstate.edu/media/id/test')
-    end
-
-    expect(provider).not_to be_nil
+    ).not_to be_nil
   end
 
   it 'registers https://media.oregonstate.edu/media/t*' do
-    let(:provider) do
+    expect(
       OEmbed::Providers.find('https://media.oregonstate.edu/media/t/test')
-    end
-
-    expect(provider).not_to be_nil
+    ).not_to be_nil
   end
 end
