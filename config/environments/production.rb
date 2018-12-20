1ed7e06883bd9bc0baeabe221e94e80bfd1aeb3f
Josh Gum <revgum@gmail.com>
2018-12-20 15:19:24 -0800

    updates configuration for smtp to match staging and production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,14 +38,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: ENV.fetch('SMTP_ADDRESS', 'smtp.gmail.com'),
-    port: ENV.fetch('SMTP_PORT', 587),
-    user_name: ENV.fetch('SMTP_USERNAME', 'etstestmail123'),
-    password: ENV.fetch('SMTP_PASSWORD', 'etstest123')
-  }
+  # Don't actually send emails in development
+  config.action_mailer.delivery_method = :test
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
