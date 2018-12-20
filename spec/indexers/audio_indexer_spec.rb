5c729ce44876ca5aa7c3b365410d81c208835646
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 14:18:58 -0800

    use joshs tests instead

--- spec/indexers/audio_indexer_spec.rb
+++ spec/indexers/audio_indexer_spec.rb
@@ -1,18 +1,9 @@
 # frozen_string_literal:true
 
 RSpec.describe AudioIndexer do
-  let(:solr_doc) { {} }
-  let(:attr) { { dcmi_type: 'MyType' }.with_indifferent_access }
-  let(:dc) { described_class.new(gw) }
-  let(:dc_call) { dc.generate_solr_document }
-  let(:gw) { Audio.new }
+  let(:indexer) { described_class.new(work) }
+  let(:solr_doc) { indexer.generate_solr_document }
+  let(:work) { build(:audio, dcmi_type: 'MyType') }
 
-  context 'when #generate_solr_document is called' do
-    before do
-      allow(gw).to receive(:attributes).and_return(attr)
-    end
-    it 'calls the proper methods' do
-      expect(dc_call['dcmi_type_tesim']).to eq 'MyType'
-    end
-  end
+  it { expect(solr_doc['dcmi_type_tesim']).to eq 'MyType' }
 end
