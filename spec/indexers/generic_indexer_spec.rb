00e66b9606c202a497fbc1fabc32095cf233a4cb
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 12:19:31 -0800

    update for gw

--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe GenericIndexer do
   let(:solr_doc) { {} }
   let(:attributes) { { title: at, dcmi_type: 'MyType' } }
   let(:at) { instance_double('ActiveTriples::Relation') }
-  let(:dc) { described_class.new(solr_doc) }
+  let(:dc) { described_class.new(gw) }
   let(:dc_call) { dc.generate_solr_document }
+  let(:gw) { instance_double('Generic') }
 
   context 'when #generate_solr_document is called' do
     before do
       allow(dc).to receive(:generate_solr_document).and_return(solr_doc)
-      allow(solr_doc).to receive(:attributes).and_return(attributes)
+      allow(gw).to receive(:attributes).and_return(attributes)
       allow(at).to receive(:is_a?).and_return(true)
       allow(at).to receive(:to_a).and_return(['MyTitle'])
     end
