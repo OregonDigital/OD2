5583cb0b08902990168418a1b8fc6cfab35a0373
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 15:06:44 -0800

    replaces dcmi type with resource type

--- app/indexers/oregon_digital/deep_indexing_service.rb
+++ app/indexers/oregon_digital/deep_indexing_service.rb
@@ -6,9 +6,9 @@ module OregonDigital
   # Used to index linked data
   class DeepIndexingService < Hyrax::DeepIndexingService
     class_attribute :stored_and_facetable_fields, :stored_fields, :symbol_fields
-    self.stored_and_facetable_fields = %i[license dcmi_type]
+    self.stored_and_facetable_fields = %i[license]
     self.stored_fields = OregonDigital::GenericMetadata::PROPERTIES.map(&:to_sym)
-    self.stored_fields -= %i[license dcmi_type]
+    self.stored_fields -= %i[license]
     self.symbol_fields = %i[]
   end
 end
