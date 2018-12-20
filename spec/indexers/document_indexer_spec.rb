c8e40768eb59bdece3279ad62b3e7f60e5152f5f
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 15:39:50 -0800

    fix more tests

--- spec/indexers/document_indexer_spec.rb
+++ spec/indexers/document_indexer_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe DocumentIndexer do
   let(:indexer) { described_class.new(work) }
   let(:solr_doc) { indexer.generate_solr_document }
-  let(:work) { build(:document, resource_type: 'MyType') }
+  let(:work) { build(:document, resource_type: ['MyType']) }
 
   it { expect(solr_doc['resource_type_tesim']).to eq 'MyType' }
 end
