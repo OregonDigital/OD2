6d2bab649e8cf28ff2e7dec9eccc77dadb91bb86
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 13:07:05 -0800

    any instance

--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -16,7 +16,8 @@ RSpec.describe GenericIndexer do
       allow(at).to receive(:to_a).and_return(['MyTitle'])
     end
     it 'calls the proper methods' do
-      expect(dc_call).to receive(:index_value_for_singular).once
+      dc_call
+      expect_any_instance_of(dc).to receive(:index_value_for_singular).once
       # expect(described_class).to receive(:index_value_for_singular).once
     end
   end
