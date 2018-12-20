eb8676e6958a30f41045729bd61299818fb595a8
straleyb <straleyb@onid.oregonstate.edu>
2018-12-20 13:44:25 -0800

    add tests for other indexers

--- spec/indexers/document_indexer_spec.rb
+++ spec/indexers/document_indexer_spec.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal:true
 
 RSpec.describe DocumentIndexer do
-  xit 'does things' do
+  let(:solr_doc) { {} }
+  let(:attr) { { dcmi_type: 'MyType' }.with_indifferent_access }
+  let(:dc) { described_class.new(gw) }
+  let(:dc_call) { dc.generate_solr_document }
+  let(:gw) { Document.new }
+
+  context 'when #generate_solr_document is called' do
+    before do
+      allow(gw).to receive(:attributes).and_return(attr)
+    end
+    it 'calls the proper methods' do
+      expect(dc_call['dcmi_type_tesim']).to eq 'MyType'
+    end
   end
 end
--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -2,15 +2,13 @@
 
 RSpec.describe GenericIndexer do
   let(:solr_doc) { {} }
-  let(:attr) { { title: at, dcmi_type: 'MyType' }.with_indifferent_access }
-  let(:at) { instance_double('ActiveTriples::Relation') }
+  let(:attr) { { dcmi_type: 'MyType' }.with_indifferent_access }
   let(:dc) { described_class.new(gw) }
   let(:dc_call) { dc.generate_solr_document }
   let(:gw) { Generic.new }
 
   context 'when #generate_solr_document is called' do
     before do
-      # allow(dc).to receive(:generate_solr_document).and_return(solr_doc)
       allow(gw).to receive(:attributes).and_return(attr)
     end
     it 'calls the proper methods' do
