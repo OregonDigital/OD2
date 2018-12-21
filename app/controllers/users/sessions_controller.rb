634d29fe04758a0e86dfd5f9b9e1477c2031a071
straleyb <straleyb@onid.oregonstate.edu>
2018-12-21 09:58:06 -0800

    update controller to support orst better

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,10 +11,21 @@ class Users::SessionsController < Devise::SessionsController
 
   def redirect_if_university
     domain = Mail::Address.new(params[:user][:email]).domain
-    case domain.to_s.split('.').last(2).first
+    split_domain = domain.to_s.split('.').last(2).first 
+    translated_domain = translate_domain(split_domain)
+    case translated_domain
     when 'uoregon' then redirect_to new_uo_session_path
     when 'oregonstate' then redirect_to new_osu_session_path
-    when 'orst' then redirect_to new_osu_session_path
     end
+  end
+
+  def translate_domain(domain)
+    translated_domain = ''
+    if domain == 'oregonstate' || domain == 'orst'
+      translated_domain = 'oregonstate'  
+    elsif domain == 'uoregon'
+      translated_domain == 'uoregon'
+    end
+    translated_domain
   end
 end
