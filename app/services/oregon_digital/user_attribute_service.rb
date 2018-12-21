6c740d9dd6d036dcac81663304840632560af3a8
straleyb <straleyb@onid.oregonstate.edu>
2018-12-21 12:24:00 -0800

    more rubocop

--- app/services/oregon_digital/user_attribute_service.rb
+++ app/services/oregon_digital/user_attribute_service.rb
@@ -1,6 +1,8 @@
-module OregonDigital
-  class UserAttributeService
+# frozen_string_literal: true
 
+module OregonDigital
+  # This object is used to inspect specific attributes of a user
+  class UserAttributeService
     def initialize(user_params)
       @domain = Mail::Address.new(user_params[:email]).to_s
       @parsed_domain = @domain.split('.')[-2]
@@ -14,7 +16,7 @@ module OregonDigital
     private
 
     def domain_hash
-      { 
+      {
         oregonstate: @rails_paths.new_osu_session_path.to_s,
         orst: @rails_paths.new_osu_session_path.to_s,
         uoregon: @rails_paths.new_uo_session_path.to_s
