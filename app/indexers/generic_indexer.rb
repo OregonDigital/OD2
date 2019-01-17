21e674bf303fad48a2ede65d941a5f903c39aacf
straleyb <straleyb@onid.oregonstate.edu>
2019-01-17 10:33:24 -0800

    remove indexer code

--- app/indexers/generic_indexer.rb
+++ app/indexers/generic_indexer.rb
@@ -7,28 +7,6 @@ class GenericIndexer < Hyrax::WorkIndexer
 
   def generate_solr_document
     super.tap do |solr_doc|
-      OregonDigital::GenericMetadata::PROPERTIES.map(&:to_s).each do |prop|
-        attr = object.attributes[prop]
-        if attr.is_a? ActiveTriples::Relation
-          index_value_for_multiple(solr_doc, attr, prop)
-        else
-          index_value_for_singular(solr_doc, attr, prop)
-        end
-      end
     end
-  end
-
-  private
-
-  def index_value_for_multiple(solr_doc, attr, prop)
-    solr_doc["#{prop}_tesim"] = attr.to_a.blank? ? [''] : attr.to_a
-    solr_doc["#{prop}_sim"] = attr.to_a.blank? ? [''] : attr.to_a
-    solr_doc["#{prop}_ssim"] = attr.to_a.blank? ? [''] : attr.to_a
-  end
-
-  def index_value_for_singular(solr_doc, attr, prop)
-    solr_doc["#{prop}_tesim"] = attr.nil? ? '' : attr
-    solr_doc["#{prop}_sim"] = attr.nil? ? '' : attr
-    solr_doc["#{prop}_ssim"] = attr.nil? ? '' : attr
   end
 end
