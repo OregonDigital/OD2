a49645c74a17ee769fba5e83c2e32252d45a03cd
Corey Gillen <gillenc@uoregon.edu>
2019-01-23 15:14:18 -0800

    Add missing base metadata to generic
    Straighten out metadata form inheritance

--- app/forms/hyrax/audio_form.rb
+++ app/forms/hyrax/audio_form.rb
@@ -5,15 +5,5 @@ module Hyrax
   class AudioForm < Hyrax::GenericForm
     include ::OregonDigital::TriplePoweredProperties::TriplePoweredForm
     self.model_class = ::Audio
-
-    def primary_terms
-      # Push the required fields to the top of the form
-      # Then make sure they arent rendered again lower in the form
-      required_fields + (OregonDigital::GenericMetadata::PROPERTIES.map(&:to_sym) - required_fields)
-    end
-
-    def secondary_terms
-      []
-    end
   end
 end
--- app/forms/hyrax/image_form.rb
+++ app/forms/hyrax/image_form.rb
@@ -7,15 +7,5 @@ module Hyrax
     include ::OregonDigital::TriplePoweredProperties::TriplePoweredForm
     self.model_class = ::Image
     self.terms += OregonDigital::ImageMetadata::PROPERTIES.map(&:to_sym)
-
-    def primary_terms
-      # Push the required fields to the top of the form
-      # Then make sure they arent rendered again lower in the form
-      required_fields + (self.terms - required_fields)
-    end
-
-    def secondary_terms
-      []
-    end
   end
 end
