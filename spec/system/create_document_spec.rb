aa513b5b9d05157a9493f974cf1ee06c68e992cb
Corey Gillen <gillenc@uoregon.edu>
2019-01-25 06:51:56 -0800

    Choose a resource type that exists

--- spec/system/create_document_spec.rb
+++ spec/system/create_document_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Create a Document', js: true, type: :system do
         fill_in('Identifier', with: 'Test ID')
       end
       select('In Copyright', from: 'Rights')
-      select('Article', from: 'Resource type')
+      select('Image', from: 'Resource type')
       # Selenium/chrome on CircleCI requires the focus to change after the previous method
       find('body').click
 
