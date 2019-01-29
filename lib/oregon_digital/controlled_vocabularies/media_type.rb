4591007f718769147f222331b275af4666d1b988
straleyb <straleyb@onid.oregonstate.edu>
2019-01-29 11:13:49 -0800

    fixed stack too deep issue

--- lib/oregon_digital/controlled_vocabularies/media_type.rb
+++ lib/oregon_digital/controlled_vocabularies/media_type.rb
@@ -5,23 +5,16 @@ module OregonDigital
 
       # Return a tuple of url & label
       def solrize
+        Rails.logger.info '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        Rails.logger.info val
         return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label.first.to_s == rdf_subject.to_s
         [rdf_subject.to_s, { label: "#{rdf_label.first}$#{rdf_subject}" }]
       end
 
       def append_to_solr_doc(solr_doc, solr_field_key, field_info, val)
-        return super unless object.controlled_properties.include?(solr_field_key.to_sym)
-
         Rails.logger.info '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
         Rails.logger.info val
-        case val
-        when ActiveTriples::Resource
-          append_label_and_uri(solr_doc, solr_field_key, field_info, val)
-        when String
-          append_label(solr_doc, solr_field_key, field_info, val)
-        else
-          raise ArgumentError, "Can't handle #{val.class}"
-        end
+        super
       end
     end
   end
