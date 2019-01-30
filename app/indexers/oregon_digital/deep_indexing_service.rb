5f609863ff4dc98616e9bb4c7e6725b7f4114e33
straleyb <straleyb@onid.oregonstate.edu>
2019-01-30 07:31:37 -0800

    fixes indexing

--- app/indexers/oregon_digital/deep_indexing_service.rb
+++ app/indexers/oregon_digital/deep_indexing_service.rb
@@ -22,5 +22,37 @@ module OregonDigital
         raise ArgumentError, "Can't handle #{val.class}"
       end
     end
+
+
+    private
+
+    def fetch_external
+      object.controlled_properties.each do |property|
+        object[property].each do |value|
+          resource = value.respond_to?(:resource) ? value.resource : value
+          next unless resource.is_a?(ActiveTriples::Resource)
+          next if value.is_a?(ActiveFedora::Base)
+          fetch_with_persistence(resource)
+        end
+      end
+    end
+
+    def fetch_with_persistence(resource)
+      old_label = resource.rdf_label.first
+      fetch_value(resource)
+      return unless old_label == resource.rdf_subject.to_s || old_label.nil?
+      fetch_value(resource)
+      return if old_label == resource.rdf_label.first || resource.rdf_label.first == resource.rdf_subject.to_s
+      resource.persist! # Stores the fetched values into our marmotta triplestore
+    end
+
+    def fetch_value(value)
+      Rails.logger.info "Fetching #{value.rdf_subject} from the authorative source. (this is slow)"
+      value.fetch(headers: { 'Accept'.freeze => default_accept_header })
+    rescue IOError, SocketError => e
+      # IOError could result from a 500 error on the remote server
+      # SocketError results if there is no server to connect to
+      Rails.logger.error "Unable to fetch #{value.rdf_subject} from the authorative source.\n#{e.message}"
+    end
   end
 end
--- lib/oregon_digital/controlled_vocabularies/media_type.rb
+++ lib/oregon_digital/controlled_vocabularies/media_type.rb
@@ -5,17 +5,10 @@ module OregonDigital
 
       # Return a tuple of url & label
       def solrize
-        Rails.logger.info '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-        Rails.logger.info val
         return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label.first.to_s == rdf_subject.to_s
         [rdf_subject.to_s, { label: "#{rdf_label.first}$#{rdf_subject}" }]
       end
 
-      def append_to_solr_doc(solr_doc, solr_field_key, field_info, val)
-        Rails.logger.info '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-        Rails.logger.info val
-        super
-      end
     end
   end
 end
