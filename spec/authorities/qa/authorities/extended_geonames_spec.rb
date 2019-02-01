79e8626e3b2f6d284ba30765b0572a376d19e250
straleyb <straleyb@onid.oregonstate.edu>
2019-02-01 12:39:01 -0800

    blahhh

--- spec/authorities/qa/authorities/extended_geonames_spec.rb
+++ spec/authorities/qa/authorities/extended_geonames_spec.rb
@@ -2,8 +2,8 @@
 
 RSpec.describe Qa::Authorities::ExtendedGeonames do
   let(:geonames_instance) { described_class.new }
-  let(:response) { [{'geonames':[{ 'geonameId': 'myId', 'name': 'myName', 'adminName1': 'adminName1', 'countryName': 'countryName', 'fcl': 'A' }] }] }
-  let(:label_hash) { {'geonameId': 'myId', 'name': 'myName', 'adminName1': 'adminName1', 'countryName': 'countryName'} }
+  let(:response) { [{ 'geonames': [{ 'geonameId': 'myId', 'name': 'myName', 'adminName1': 'adminName1', 'countryName': 'countryName', 'fcl': 'A' }] }] }
+  let(:label_hash) { { 'geonameId': 'myId', 'name': 'myName', 'adminName1': 'adminName1', 'countryName': 'countryName' } }
   let(:fcl) { 'Area' }
 
   it { expect(geonames_instance.label.call(label_hash, fcl)).to eq 'my_label'}
@@ -12,6 +12,6 @@ RSpec.describe Qa::Authorities::ExtendedGeonames do
       allow(geonames_instance).to receive(:json).with(:anything).and_return(response)
       allow(geonames_instance).to receive(:build_query_url).with('http://my.queryuri.com').and_return(response)
     end
-    it { expect(geonames_instance.search('http://my.queryuri.com')).to eq [{'id': 'http://sws.geonames.org/myId/', 'label': 'name, adminName1, countryName, Area'}] }
+    it { expect(geonames_instance.search('http://my.queryuri.com')).to eq [{ 'id': 'http://sws.geonames.org/myId/', 'label': 'name, adminName1, countryName, Area' }] }
   end
 end
