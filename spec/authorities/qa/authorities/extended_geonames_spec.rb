0f5f201f0a87ede76e7c747f4af834c5bf2a3188
straleyb <straleyb@onid.oregonstate.edu>
2019-02-01 12:35:03 -0800

    expect

--- spec/authorities/qa/authorities/extended_geonames_spec.rb
+++ spec/authorities/qa/authorities/extended_geonames_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Qa::Authorities::ExtendedGeonames do
   let(:label_hash) { {'geonameId': 'myId', 'name': 'myName', 'adminName1': 'adminName1', 'countryName': 'countryName'} }
   let(:fcl) { 'Area' }
 
-  it { geonames_instance.label.call(label_hash, fcl) eq 'my_label'}
+  it { expect(geonames_instance.label.call(label_hash, fcl)).to eq 'my_label'}
   describe "#search" do
     before do
       allow(geonames_instance).to receive(:json).with(:anything).and_return(response)
       allow(geonames_instance).to receive(:build_query_url).with('http://my.queryuri.com').and_return(response)
     end
-    it { geonames_instance.search('http://my.queryuri.com') eq [{'id': 'http://sws.geonames.org/myId/', 'label': 'name, adminName1, countryName, Area'}] }
+    it { expect(geonames_instance.search('http://my.queryuri.com')).to eq [{'id': 'http://sws.geonames.org/myId/', 'label': 'name, adminName1, countryName, Area'}] }
   end
 end
