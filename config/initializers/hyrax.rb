8b4434a5dea1dedf6bf19fbdba1afc338781e703
Brandon R. Straley <brandon.straley@oregonstate.edu>
2019-02-04 14:32:24 -0800

    Update hyrax.rb

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -112,7 +112,7 @@ Hyrax.config do |config|
 
   # Location autocomplete uses geonames to search for named regions
   # Username for connecting to geonames
-  config.geonames_username = 'etsdev'
+  config.geonames_username = Env.fetch('GEONAMES_USERNAME', 'etsdev')
 
   # Should the acceptance of the licence agreement be active (checkbox), or
   # implied when the save button is pressed? Set to true for active
