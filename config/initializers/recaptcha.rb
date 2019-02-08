2cf724384d6317a9905b1c46cc69b6d9e3cb87e0
Josh Gum <revgum@gmail.com>
2019-02-07 16:03:33 -0800

    loads recaptcha only in staging or production

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
-if Hyrax.config.recaptcha?
+if Hyrax.config.recaptcha? && (Rails.env.staging? || Rails.env.production?)
   Recaptcha.configure do |config|
     config.site_key  = Hyrax.config.recaptcha_site_key
     config.secret_key = Hyrax.config.recaptcha_secret_key
