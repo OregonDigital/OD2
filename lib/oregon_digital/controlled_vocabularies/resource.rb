20135c07920a6bed620cd5d940f3b88458c41565
Corey Gillen <gillenc@uoregon.edu>
2019-02-08 11:55:41 -0800

    Add validation to controlled vocabulary objects

--- lib/oregon_digital/controlled_vocabularies/resource.rb
+++ lib/oregon_digital/controlled_vocabularies/resource.rb
@@ -4,6 +4,9 @@ module OregonDigital
   module ControlledVocabularies
     # Generic object for storing labels and uris
     class Resource < ActiveTriples::Resource
+      # Validate resource is in the vocabulary
+      validate :uri_in_vocab
+
       # Return a tuple of url & label
       def solrize
         return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label_uri_same?
@@ -14,6 +17,10 @@ module OregonDigital
 
       def rdf_label_uri_same?
         rdf_label.first.to_s == rdf_subject.to_s
+      end
+
+      def uri_in_vocab
+        errors.add(:uri, "URI #{id} not in vocabulary") unless self.class.respond_to?(:in_vocab?) && self.class.in_vocab?(id)
       end
     end
   end
