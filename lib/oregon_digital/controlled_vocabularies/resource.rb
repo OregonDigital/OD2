ad6bad74191d78c987e45c7cb1e79757f37b98c1
Corey Gillen <corey.j.gillen@gmail.com>
2019-02-12 13:59:04 -0800

    URI validation for Controlled Vocabularies (#354)
    * Add validation to controlled vocabulary objects
    
    * Add validation of controlled vocab on work model
    
    * reimplement controlled vocab checking like OD1
    
    * specs and rubocop

--- lib/oregon_digital/controlled_vocabularies/resource.rb
+++ lib/oregon_digital/controlled_vocabularies/resource.rb
@@ -4,6 +4,28 @@ module OregonDigital
   module ControlledVocabularies
     # Generic object for storing labels and uris
     class Resource < ActiveTriples::Resource
+      ##
+      # Override ActiveTriples::Resource to enforce vocabulary membership
+      #
+      # @see ActiveTriples::Resource
+      def initialize(*args, &block)
+        super
+        # If rdf_subject wasn't set, we're done
+        return if node?
+
+        # Reset rdf_subject and set it through the proper method
+        resource_uri = @rdf_subject
+        @rdf_subject = nil
+        set_subject!(resource_uri)
+      end
+
+      # Override ActiveTriples::Resource.set_subject! to throw exception if term isn't in vocab
+      def set_subject!(uri_or_str)
+        raise ControlledVocabularyError, "Term not in controlled vocabulary: #{uri_or_str}" unless
+          uri_in_vocab?(uri_or_str.to_s)
+        super
+      end
+
       # Return a tuple of url & label
       def solrize
         return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label_uri_same?
@@ -15,6 +37,12 @@ module OregonDigital
       def rdf_label_uri_same?
         rdf_label.first.to_s == rdf_subject.to_s
       end
+
+      def uri_in_vocab?(uri)
+        self.class.respond_to?(:in_vocab?) && self.class.in_vocab?(uri)
+      end
     end
+
+    class ControlledVocabularyError < StandardError; end
   end
 end
