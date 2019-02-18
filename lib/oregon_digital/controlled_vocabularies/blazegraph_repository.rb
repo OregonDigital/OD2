15994bdef1eb6cc4253449bd4d2547a6574e5fd0
straleyb <straleyb@onid.oregonstate.edu>
2019-02-18 07:53:27 -0800

    update

--- lib/oregon_digital/controlled_vocabularies/blazegraph_repository.rb
+++ lib/oregon_digital/controlled_vocabularies/blazegraph_repository.rb
@@ -20,24 +20,13 @@ module OregonDigital
         rest_client.insert([statement])
       end
 
-      # def each_statement
-      #   rest_client.get_statements.each_statement
-      # end
+      def insert_statements(statements)
+        rest_client.insert(statements)
+      end
 
       def delete_statement(statement)
         rest_client.delete([statement])
       end
-
-      # protected
-
-      # def query_pattern(pattern, **options, &block)
-      #   return enum_for(:query_pattern, pattern, options) unless block_given?
-      #   Error
-        # statements = pattern[:subject]
-        # statements.fetch unless statements.node?
-        # rest_client.insert([statements])
-        # pattern[:subject]
-      # end
     end
   end
 end
