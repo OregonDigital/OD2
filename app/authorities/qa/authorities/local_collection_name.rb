d20c6f3457cbdf06c0adb6114b7a3abb9966f438
straleyb <straleyb@onid.oregonstate.edu>
2019-02-22 08:46:01 -0800

    fix

--- app/authorities/qa/authorities/local_collection_name.rb
+++ app/authorities/qa/authorities/local_collection_name.rb
@@ -4,15 +4,19 @@ module Qa::Authorities
   # Local Collection Name QA Object
   class LocalCollectionName < BaseAuthority
     self.label = lambda do |item|
-      [item['rdfs:label']['@value']].compact.join(', ')
+      [item.first['rdfs:label']['@value']].compact.join(', ')
     end
 
     def search(q)
       if OregonDigital::ControlledVocabularies::LocalCollectionName.in_vocab?(q)
-        parse_authority_response(json(q + '.jsonld'))
+        parse_authority_response(find_term(json(q + '.jsonld'), q))
       else
         []
       end
+    end
+
+    def find_term(response, q)
+      [response]
     end
   end
 end
--- spec/authorities/qa/authorities/local_collection_name_spec.rb
+++ spec/authorities/qa/authorities/local_collection_name_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Qa::Authorities::LocalCollectionName do
   it { expect(local_collection_name_instance.label.call(response.first)).to eq 'mylabel' }
   describe '#search' do
     before do
-      allow(local_collection_name_instance).to receive(:json).with(anything).and_return(response.first)
+      allow(local_collection_name_instance).to receive(:json).with(anything).and_return(response)
+      allow(local_collection_name_instance).to receive(:label).with(anything).and_return({'mylabel'})
     end
     context 'with a uri in the vocabulary' do
       it { expect(local_collection_name_instance.search('http://opaquenamespace.org/ns/localCollectionName/term_id')).to eq [{ id: 'my_id', label: 'mylabel' }.with_indifferent_access] }
