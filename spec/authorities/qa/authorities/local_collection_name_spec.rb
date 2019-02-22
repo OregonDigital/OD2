fdbf0320f8d3f3f6abdd2188ad58062f1ed54446
straleyb <straleyb@onid.oregonstate.edu>
2019-02-22 08:08:43 -0800

    got it?

--- spec/authorities/qa/authorities/local_collection_name_spec.rb
+++ spec/authorities/qa/authorities/local_collection_name_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Qa::Authorities::LocalCollectionName do
   let(:local_collection_name_instance) { described_class.new }
-  let(:response) { { 'rdfs:label': { '@value': 'mylabel' }.with_indifferent_access, '@id': 'my_id' }.with_indifferent_access }
+  let(:response) { [{ 'rdfs:label': { '@value': 'mylabel' }.with_indifferent_access, '@id': 'my_id' }.with_indifferent_access] }
 
   it { expect(local_collection_name_instance.label.call(response)).to eq 'mylabel' }
   describe '#search' do
