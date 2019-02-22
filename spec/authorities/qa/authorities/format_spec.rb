ec4e951047572fbf5c156f37fe7fc218da0ead6b
straleyb <straleyb@onid.oregonstate.edu>
2019-02-22 07:52:49 -0800

    with indifferent access

--- spec/authorities/qa/authorities/format_spec.rb
+++ spec/authorities/qa/authorities/format_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Qa::Authorities::Format do
   let(:format_instance) { described_class.new }
-  let(:response) { [{}, { 'http://www.w3.org/2000/01/rdf-schema#label': [{ '@value': 'mylabel' }], '@id': 'my_id' }.with_indifferent_access] }
+  let(:response) { [{}, { 'http://www.w3.org/2000/01/rdf-schema#label': [{ '@value': 'mylabel' }.with_indifferent_access], '@id': 'my_id' }.with_indifferent_access] }
   let(:label) { 'mylabel' }
 
   it { expect(format_instance.label.call(response.second)).to eq 'mylabel' }
