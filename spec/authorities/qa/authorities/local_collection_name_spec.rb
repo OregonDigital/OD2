621cadbf3375ec495703baca6793d1e9ddd2ab7b
straleyb <straleyb@onid.oregonstate.edu>
2019-02-22 09:10:04 -0800

    fix

--- spec/authorities/qa/authorities/local_collection_name_spec.rb
+++ spec/authorities/qa/authorities/local_collection_name_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Qa::Authorities::LocalCollectionName do
   describe '#search' do
     before do
       allow(local_collection_name_instance).to receive(:json).with(anything).and_return(response)
-      allow(local_collection_name_instance).to receive(:find_term).with(anything).and_return(response)
+      allow(local_collection_name_instance).to receive(:find_term).with(anything, anything).and_return(response)
     end
     context 'with a uri in the vocabulary' do
       it { expect(local_collection_name_instance.search('http://opaquenamespace.org/ns/localCollectionName/term_id')).to eq [{ id: 'my_id', label: 'mylabel' }.with_indifferent_access] }
