a547b3598dbb00b0b4f01a1de7f25c05708332ee
straleyb <straleyb@onid.oregonstate.edu>
2019-03-11 12:40:02 -0700

    support multiple endpoints

--- app/authorities/qa/authorities/repository.rb
+++ app/authorities/qa/authorities/repository.rb
@@ -4,18 +4,24 @@ module Qa::Authorities
   # EthnographicTerm QA Object
   class Repository < BaseAuthority
     self.label = lambda do |item|
-      [item.first['http://www.loc.gov/mads/rdf/v1#authoritativeLabel'].first['@value']].compact.join(', ')
+      [find_label(item)].compact.join(', ')
     end
 
     def search(q)
       if OregonDigital::ControlledVocabularies::Repository.in_vocab?(q)
-        Rails.logger.info "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        Rails.logger.info json(q)
-        parse_authority_response(find_term(json(q), q))
+        parse_authority_response(find_term(json(q + '.jsonld'), q))
       else
         []
       end
     end
+
+    def self.find_label(item)
+      label = item.first['http://www.w3.org/2000/01/rdf-schema#label'] 
+      label.blank? ? item.first["http://www.w3.org/2004/02/skos/core#prefLabel"].first['@value'] : label
+    end
+
+    def find_term(response, q)
+      response.select { |resp| resp['@id'] == q }
+    end
   end
 end
-
--- lib/oregon_digital/controlled_vocabularies/repository.rb
+++ lib/oregon_digital/controlled_vocabularies/repository.rb
@@ -6,15 +6,16 @@ module OregonDigital
     class Repository < Resource
       # Return T/F if a URI is in the vocab
       def self.in_vocab?(uri)
-        valid_uri = %r{^http[s]?:\/\/vocab.getty.edu\/ulan\/.*}
-        valid_uri.match?(uri)
-        true
+        all_uris.each do |valid_uri|
+          return true if valid_uri.match?(uri)
+        end
       end
 
-      def check_all_uris(uri)
+      def self.all_uris
         [
           %r{^http[s]?:\/\/vocab.getty.edu\/ulan\/.*},
-
+          %r{^http[s]?:\/\/id.loc.gov\/authorities\/names\/.*},
+          %r{^http[s]?:\/\/id.loc.gov\/authorities\/names\/.*}
         ]
       end
     end
