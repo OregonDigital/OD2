a5223b88e61905c1097439b8933e7756ee013d59
straleyb <straleyb@onid.oregonstate.edu>
2019-03-12 14:33:59 -0700

    fix terms

--- app/authorities/qa/authorities/format.rb
+++ app/authorities/qa/authorities/format.rb
@@ -3,13 +3,14 @@
 module Qa::Authorities
   # Format QA Object
   class Format < BaseAuthority
-    self.label = lambda do |item|
+    self.label = lambda do |item, vocabulary|
       [item.first['http://www.w3.org/2000/01/rdf-schema#label'].first['@value']].compact.join(', ')
     end
 
     def search(q)
+      vocabulary = nil
       if OregonDigital::ControlledVocabularies::MediaType.in_vocab?(q)
-        parse_authority_response(find_term(json(q), q))
+        parse_authority_response(find_term(json(q), q), vocabulary)
       else
         []
       end
--- app/authorities/qa/authorities/tgn.rb
+++ app/authorities/qa/authorities/tgn.rb
@@ -3,13 +3,14 @@
 module Qa::Authorities
   # EthnographicTerm QA Object
   class Tgn < BaseAuthority
-    self.label = lambda do |item|
+    self.label = lambda do |item, _vocabulary|
       [item.first['http://www.w3.org/2000/01/rdf-schema#label'].first['@value']].compact.join(', ')
     end
 
     def search(q)
+      vocabulary = nil
       if OregonDigital::ControlledVocabularies::HistoricPlace.in_vocab?(q)
-        parse_authority_response(json(q + '.jsonld'))
+        parse_authority_response(json(q + '.jsonld'), vocabulary)
       else
         []
       end
