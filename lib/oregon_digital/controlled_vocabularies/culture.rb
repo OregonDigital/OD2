926886cd8c318f777ce0f87c248f754260b6d318
Corey Gillen <corey.j.gillen@gmail.com>
2019-03-15 09:50:05 -0700

    Add Culture controlled vocabulary (#417)
    * Add culture controlled vocab
    
    * Specs
    
    * Rubocop fixes

---
+++ lib/oregon_digital/controlled_vocabularies/culture.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module OregonDigital
+  module ControlledVocabularies
+    # Repository object for storing labels and uris
+    class Culture < Resource
+      def self.all_endpoints
+        [
+          OregonDigital::ControlledVocabularies::Vocabularies::GettyAat,
+          OregonDigital::ControlledVocabularies::Vocabularies::LocSubjects,
+          OregonDigital::ControlledVocabularies::Vocabularies::LocNames,
+          OregonDigital::ControlledVocabularies::Vocabularies::OnsCulture
+        ]
+      end
+    end
+  end
+end
