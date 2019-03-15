edd637f0f015baf907ae9c116170fbb972657182
Corey Gillen <corey.j.gillen@gmail.com>
2019-03-15 08:22:31 -0700

    Add Creator controlled vocabulary (#407)
    * Add Creator controlled vocabulary
    
    Control Arranger by Creator
    
    * Control the rest of the creators
    
    * Specs
    
    * Fix wikidata load time
    
    * Fix spec

---
+++ app/authorities/qa/authorities/creator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Qa::Authorities
+  # Repository QA Object
+  class Creator < BaseAuthority
+    def controlled_vocabulary
+      OregonDigital::ControlledVocabularies::Creator
+    end
+
+    # WikiData doesn't support a native .jsonld implementation, so we shim in the id here
+    # And parse the label as regular JSON
+    def json(url)
+      json = super
+      return json unless controlled_vocabulary.query_to_vocabulary(url) == OregonDigital::ControlledVocabularies::Vocabularies::WdEntity
+      json['@id'] = url
+      json
+    end
+  end
+end
---
+++ spec/oregon_digital/controlled_vocabularies/creator_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal:true
+
+RSpec.describe OregonDigital::ControlledVocabularies::Creator do
+  let(:vocab) { described_class }
+
+  describe '#in_vocab?' do
+    context 'when in vocab' do
+      it { expect(vocab.in_vocab?('http://vocab.getty.edu/ulan/500302766')).to be true }
+    end
+    context 'when not in vocab' do
+      it { expect(vocab.in_vocab?('http://my.queryuri.com')).to be false }
+    end
+  end
+  describe '#query_to_vocabulary' do
+    context 'when in vocab' do
+      it { expect(vocab.query_to_vocabulary('http://vocab.getty.edu/ulan/500302766')).to be OregonDigital::ControlledVocabularies::Vocabularies::Ulan }
+    end
+    context 'when not in vocab' do
+      it { expect(vocab.query_to_vocabulary('http://my.queryuri.com')).to be nil }
+    end
+  end
+end
