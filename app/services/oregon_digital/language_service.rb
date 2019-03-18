ee5234e43d4d4d5bdace9eb749bf438224a096bc
Corey Gillen <gillenc@uoregon.edu>
2019-03-18 14:28:54 -0700

    Add Language controlled vocabulary

---
+++ app/services/oregon_digital/language_service.rb
@@ -0,0 +1,8 @@
+module OregonDigital
+  # Provide select options for the license (dcterms:rights) field
+  class LanguageService < Hyrax::QaSelectService
+    def initialize(_authority_name = nil)
+      super('languages')
+    end
+  end
+end
