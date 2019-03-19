9584cf3c03a5f39d8863f9e755041308b393bd01
Corey Gillen <gillenc@uoregon.edu>
2019-03-19 11:47:57 -0700

    Add a spec and fix that spec

--- lib/oregon_digital/controlled_vocabularies/institution.rb
+++ lib/oregon_digital/controlled_vocabularies/institution.rb
@@ -15,8 +15,13 @@ module OregonDigital
       # Return a tuple of url & label
       def solrize
         label = rdf_label.select { |lang_label| lang_label.language == I18n.locale }.first
-        return [rdf_subject.to_s, { label: "#{label}$#{rdf_subject}" }] unless label.empty?
-        super
+        return [rdf_subject.to_s] if label.to_s.blank? || rdf_label_uri_same?
+        [rdf_subject.to_s, { label: "#{label}$#{rdf_subject}" }]
+      end
+
+      private
+      def rdf_label_uri_same?
+        rdf_label.select { |lang_label| lang_label.language == I18n.locale }.first.to_s == rdf_subject.to_s
       end
     end
   end
