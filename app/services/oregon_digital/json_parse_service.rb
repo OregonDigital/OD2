c19ea2fb9f4cf2c13a398497313091223f8ad500
straleyb <straleyb@onid.oregonstate.edu>
2019-03-20 12:26:55 -0700

    woops

--- app/services/oregon_digital/json_parse_service.rb
+++ app/services/oregon_digital/json_parse_service.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module OregonDigital
+  # Parses JSON
   class JsonParseService
     def self.json(query)
       JSON.parse(query_json(query).body)
--- app/services/oregon_digital/xml_parse_service.rb
+++ app/services/oregon_digital/xml_parse_service.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module OregonDigital
+  # Parses XML
   class XmlParseService
     def self.xml(query)
       Nokogiri::XML(query_xml(query).body)
