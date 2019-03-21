39a1ec0a6a05b97e27db6868fc5f7977501b9e89
straleyb <straleyb@onid.oregonstate.edu>
2019-03-21 11:15:44 -0700

    move duped code to base object

--- lib/oregon_digital/controlled_vocabularies/vocabularies/ons_base.rb
+++ lib/oregon_digital/controlled_vocabularies/vocabularies/ons_base.rb
@@ -3,10 +3,15 @@
 module OregonDigital
   module ControlledVocabularies::Vocabularies
     # Receives information pulled from the endpoint and can parse and generate queries
-    class OnsPeople < OnsBase
-      def self.expression
-        %r{^http[s]?:\/\/opaquenamespace.org\/ns\/people\/.*}
+    class OnsClass
+      def self.label(data)
+        data.first['rdfs:label']['@value']
+      end
+
+      def self.as_query(q)
+        q + '.jsonld'
       end
     end
   end
 end
+
