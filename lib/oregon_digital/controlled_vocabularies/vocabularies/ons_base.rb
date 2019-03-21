9d4f1d42e70fbef0f97883b157dc54481e2a187b
straleyb <straleyb@onid.oregonstate.edu>
2019-03-21 11:32:04 -0700

    oops

--- lib/oregon_digital/controlled_vocabularies/vocabularies/ons_base.rb
+++ lib/oregon_digital/controlled_vocabularies/vocabularies/ons_base.rb
@@ -3,7 +3,7 @@
 module OregonDigital
   module ControlledVocabularies::Vocabularies
     # Receives information pulled from the endpoint and can parse and generate queries
-    class OnsClass
+    class OnsBase
       def self.label(data)
         data.first['rdfs:label']['@value']
       end
