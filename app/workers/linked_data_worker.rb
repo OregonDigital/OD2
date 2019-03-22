b213dc9821b985b232d091f4804f056cd4f99f60
Corey Gillen <gillenc@uoregon.edu>
2019-03-22 10:14:10 -0700

    Specs

--- app/workers/linked_data_worker.rb
+++ app/workers/linked_data_worker.rb
@@ -1,9 +1,13 @@
 class LinkedDataWorker
   include Sidekiq::Worker
+  attr_writer :triplestore
 
   def perform(uri)
-      @triplestore ||= TriplestoreAdapter::Triplestore.new(OregonDigital::Triplestore.triplestore_client)
       @triplestore.fetch(uri, from_remote: true)
       # TODO: Email user on success
+  end
+
+  def triplestore
+    @triplestore ||= TriplestoreAdapter::Triplestore.new(OregonDigital::Triplestore.triplestore_client)
   end
 end
--- spec/workers/linked_data_worker_spec.rb
+++ spec/workers/linked_data_worker_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 RSpec.describe LinkedDataWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:worker) { described_class.new }
+  let(:uri) { 'http://my.queryuri.com' }
+
+  describe '#triplestore' do
+    it { expect(worker.triplestore).to be_a(TriplestoreAdapter::Triplestore) }
+  end
+
+  describe '#perform' do
+    it 'attempts a fetch from the triplestore' do
+      triplestore = double()
+      worker.triplestore = triplestore
+
+      expect(triplestore).to receive(:fetch).with(:uri, from_remote: true)
+
+      worker.perform(:uri)
+    end
+  end
 end
