eb7fdde8f7a578e9a315d9cd007a5020b6be32eb
straleyb <straleyb@onid.oregonstate.edu>
2019-03-25 08:51:06 -0700

    require on autoloaded file

--- lib/oregon_digital/controlled_vocabularies.rb
+++ lib/oregon_digital/controlled_vocabularies.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
+Dir[File.dirname(__FILE__) + '/controlled_vocabularies/*.rb'].each {|file| require file }
+Dir[File.dirname(__FILE__) + '/controlled_vocabularies/vocabularies/*.rb'].each {|file| require file }
 
 module OregonDigital
   # Autoloads controlled vocabulary objects
   module ControlledVocabularies
     extend ActiveSupport::Autoload
-
-    eager_autoload do
-      autoload :MediaType
     end
   end
 end
