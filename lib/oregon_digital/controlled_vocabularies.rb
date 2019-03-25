94db064a207debc565599dc218bcfe2d3466cd3b
straleyb <straleyb@onid.oregonstate.edu>
2019-03-25 09:25:58 -0700

    check if code broke

--- lib/oregon_digital/controlled_vocabularies.rb
+++ lib/oregon_digital/controlled_vocabularies.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-Dir[File.dirname(__FILE__) + '/controlled_vocabularies/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/controlled_vocabularies/vocabularies/*.rb'].each {|file| require file }
+# Dir[File.dirname(__FILE__) + '/controlled_vocabularies/*.rb'].each {|file| require file }
+# Dir[File.dirname(__FILE__) + '/controlled_vocabularies/vocabularies/*.rb'].each {|file| require file }
 
 module OregonDigital
   # Autoloads controlled vocabulary objects
