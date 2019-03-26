b36f4b5bbc246e1138a72d79cf8b4132742321fa
Corey Gillen <gillenc@uoregon.edu>
2019-03-26 10:51:30 -0700

    Another spec

--- spec/oregon_digital/triplestore_spec.rb
+++ spec/oregon_digital/triplestore_spec.rb
@@ -38,6 +38,27 @@ RSpec.describe OregonDigital::Triplestore do
                                                               'http://www.w3.org/2004/02/skos/core#prefLabel' => [])
     end
   end
+
+  describe '#enqueue_fetch_failure' do
+    before do
+      allow(Hyrax.config.callback).to receive(:run)
+      allow(LinkedDataWorker).to receive(:perform_in)
+      stub_request(:post, 'http://ci-test:8080/bigdata/namespace/rw/sparql')
+        .to_return(status: 200, body: '', headers: {})
+    end
+    it 'emails the user' do
+      expect(Hyrax.config.callback).to receive(:run).with(:ld_fetch_error, :user, :uri)
+      triplestore.enqueue_fetch_failure(:uri, :user)
+    end
+    it 'enqueues a retry job' do
+      expect(LinkedDataWorker).to receive(:perform_in).with(15.minutes, :uri, :user)
+      triplestore.enqueue_fetch_failure(:uri, :user)
+    end
+    it 'stubs data in the triplestore' do
+      expect(triplestore.instance_variable_get(:@triplestore)).to receive(:store)
+      triplestore.enqueue_fetch_failure(:uri, :user)
+    end
+  end
 end
 
 def build_graph
