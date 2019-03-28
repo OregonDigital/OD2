ab5a78810284175d6f276c9e124a7c454bedd46e
Corey Gillen <gillenc@uoregon.edu>
2019-03-28 13:15:02 -0700

    Rubocop

--- spec/models/oregon_digital/contact_form_spec.rb
+++ spec/models/oregon_digital/contact_form_spec.rb
@@ -2,18 +2,17 @@
 
 RSpec.describe OregonDigital::ContactForm do
   let(:form) { described_class.new(form_data) }
-  let(:form_data) { {subject: 'test subject', email: 'test@test.com'} }
+  let(:form_data) { { subject: 'test subject', email: 'test@test.com' } }
 
   describe '#submitter_headers' do
     context 'with form data' do
       it 'provides email headers' do
-        expect(form.submitter_headers).to eq({
+        expect(form.submitter_headers).to eq(
           subject: 'Contact form: test subject',
           to: 'test@test.com',
           from: Hyrax.config.contact_email
-        })
+        )
       end
     end
   end
-
 end
