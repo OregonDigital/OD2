20f4c1ae47c65cb46c4adba565ea839353875f8c
straleyb <straleyb@onid.oregonstate.edu>
2019-04-01 08:16:35 -0700

    use semicolor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths += Rails.root.join('app/assets/html')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/html')
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
