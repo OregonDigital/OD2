ffe4ac483e388e2eb003733846eb26f4d86b792c
Corey Gillen <gillenc@uoregon.edu>
2019-04-02 09:42:16 -0700

    Remove primary set and set
    Set repository to multiple: false

--- app/models/concerns/oregon_digital/generic_metadata.rb
+++ app/models/concerns/oregon_digital/generic_metadata.rb
@@ -6,7 +6,7 @@ module OregonDigital
     extend ActiveSupport::Concern
     # Usage notes and expectations can be found in the Metadata Application Profile:
     # https://docs.google.com/spreadsheets/d/16xBFjmeSsaN0xQrbOpQ_jIOeFZk3ZM9kmB8CU3IhP2c/edit#gid=0
-    PROPERTIES = %w[abstract accepted_name_usage access_restrictions accession_number acquisition_date alternative arranger art_series artist author award award_date barcode biographical_information box box_name box_number based_near canzoniere_poems cartographer citation collected_date collector common_name compass_direction composer contents contributor conversion copy_location copyright_claimant cover_description coverage creator creator_display cultural_context current_repository_id date date_created date_digitized dedicatee description description_of_manifestation designer donor editor ethnographic_term event exhibit extent family file_size folder_name folder_number form_of_work format former_owner genus gps_latitude gps_longitude has_finding_aid has_part has_version higher_classification hydrologic_unit_code identification_verification_status identifier illustrator inscription institution interviewee interviewer isPartOf is_version_of issued item_locator keyword language layout legal_name license local_collection_id local_collection_name location location_copyshelf_location longitude_latitude_identification lyricist material measurements military_branch military_highest_rank military_occupation military_service_location mode_of_issuance mods_note object_orientation oembed_url order original_name_usage owner patron photographer phylum_or_division physical_extent place_of_production primary_set print_maker provenance publication_place publisher ranger_district recipient relation replaces_url repository resource_type rights_holder rights_statement scientific_name_authorship scribe series_name series_number set source source_condition species specimen_type sports_team state_or_edition street_address style_or_period subject taxon_class technique temporal tgn transcriber translator tribal_classes tribal_notes tribal_terms tribal_title use_restrictions view_date water_basin workType].freeze
+    PROPERTIES = %w[abstract accepted_name_usage access_restrictions accession_number acquisition_date alternative arranger art_series artist author award award_date barcode biographical_information box box_name box_number based_near canzoniere_poems cartographer citation collected_date collector common_name compass_direction composer contents contributor conversion copy_location copyright_claimant cover_description coverage creator creator_display cultural_context current_repository_id date date_created date_digitized dedicatee description description_of_manifestation designer donor editor ethnographic_term event exhibit extent family file_size folder_name folder_number form_of_work format former_owner genus gps_latitude gps_longitude has_finding_aid has_part has_version higher_classification hydrologic_unit_code identification_verification_status identifier illustrator inscription institution interviewee interviewer isPartOf is_version_of issued item_locator keyword language layout legal_name license local_collection_id local_collection_name location location_copyshelf_location longitude_latitude_identification lyricist material measurements military_branch military_highest_rank military_occupation military_service_location mode_of_issuance mods_note object_orientation oembed_url order original_name_usage owner patron photographer phylum_or_division physical_extent place_of_production print_maker provenance publication_place publisher ranger_district recipient relation replaces_url repository resource_type rights_holder rights_statement scientific_name_authorship scribe series_name series_number source source_condition species specimen_type sports_team state_or_edition street_address style_or_period subject taxon_class technique temporal tgn transcriber translator tribal_classes tribal_notes tribal_terms tribal_title use_restrictions view_date water_basin workType].freeze
     CONTROLLED = %w[arranger_label artist_label author_label based_near_label cartographer_label collector_label common_name_label composer_label contributor_label creator_label cultural_context_label dedicatee_label designer_label donor_label editor_label ethnographic_term_label family_label form_of_work_label format_label genus_label illustrator_label institution_label interviewee_label interviewer_label local_collection_name_label lyricist_label military_branch_label order_label owner_label patron_label photographer_label phylum_or_division_label print_maker_label publisher_label recipient_label repository_label scribe_label species_label style_or_period_label subject_label taxon_class_label tgn_label transcriber_label translator_label workType_label].freeze
 
     included do
@@ -295,14 +295,6 @@ module OregonDigital
         index.as :stored_searchable, :facetable
       end
 
-      property :primary_set, predicate: ::RDF::URI.new('http://opaquenamespace.org/ns/primarySet'), multiple: false do |index|
-        index.as :stored_searchable
-      end
-
-      property :set, predicate: ::RDF::URI.new('http://opaquenamespace.org/ns/set') do |index|
-        index.as :stored_searchable, :facetable
-      end
-
       property :conversion, predicate: ::RDF::URI.new('http://opaquenamespace.org/ns/conversionSpecifications') do |index|
         index.as :stored_searchable
       end
@@ -563,7 +555,7 @@ module OregonDigital
         index.as :stored_searchable, :facetable
       end
 
-      property :repository, predicate: ::RDF::Vocab::MARCRelators.rps,
+      property :repository, predicate: ::RDF::Vocab::MARCRelators.rps, multiple: false,
                             class_name: OregonDigital::ControlledVocabularies::Repository do |index|
         index.as :stored_searchable, :facetable
       end
