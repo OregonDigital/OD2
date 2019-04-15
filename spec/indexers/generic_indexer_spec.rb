242be13474893752c616701df7dc17f5947af8fa
straleyb <straleyb@onid.oregonstate.edu>
2019-04-15 13:50:19 -0700

    specs

--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe GenericIndexer do
   let(:work) { build(:generic, resource_type: ['MyType'], license: ['MyLicense'], rights_statement: ['MyRights'], language: ['MyLanguage']) }
 
   it { expect(solr_doc['resource_type_tesim']).to eq ['MyType'] }
-  it ( expect(solr_doc['license_tesim']).to eq ['MyLicense'] )
-  it ( expect(solr_doc['language_tesim']).to eq ['MyLanguage'] )
-  it ( expect(solr_doc['rights_statement_tesim']).to eq ['MyRights'] )
+  it { expect(solr_doc['license_tesim']).to eq ['MyLicense'] }
+  it { expect(solr_doc['language_tesim']).to eq ['MyLanguage'] }
+  it { expect(solr_doc['rights_statement_tesim']).to eq ['MyRights'] }
 end
