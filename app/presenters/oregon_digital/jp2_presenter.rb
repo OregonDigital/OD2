8004a5859cca21b6919f3e0eb279223b978e0e5c
Jeremy Echols <jechols@uoregon.edu>
2019-05-20 13:43:49 -0700

    JP2Presenter: pull image path build into a new fn

--- app/presenters/oregon_digital/jp2_presenter.rb
+++ app/presenters/oregon_digital/jp2_presenter.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal:true
 
+require 'iiif_manifest'
+
 module OregonDigital
   # JP2Presenter wraps a single JP2 derivative as if it were a fileset of its
   # own, strictly for the IIIF manifest / viewer.  This is adapted from
@@ -24,7 +26,7 @@ module OregonDigital
 
       # I have no idea why we have to specify height here; IIIF doesn't require
       # it.  But this is how the Hyrax code seems to work, sooo....
-      IIIFManifest::DisplayImage.new([iiif_url, 'full', '640,', '0', 'default.jpg'].join('/'),
+      IIIFManifest::DisplayImage.new(default_image_path,
                                      width: 640,
                                      height: 480,
                                      iiif_endpoint: iiif_endpoint)
@@ -55,6 +57,10 @@ module OregonDigital
     # Calculates the base URL for IIIF requests against this JP2
     def iiif_url
       [ENV.fetch('IIIF_SERVER_BASE_URL', request.base_url), iiif_id].join('/')
+    end
+
+    def default_image_path
+      [iiif_url, 'full', '640,', '0', 'default.jpg'].join('/')
     end
 
     def iiif_endpoint
