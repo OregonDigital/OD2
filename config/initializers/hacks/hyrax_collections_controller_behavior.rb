19b811fe47558910187ce96be6e25537f035afe5
Corey Gillen <gillenc@uoregon.edu>
2019-05-21 13:42:26 -0700

    Add Blacklight facets to collections search page

--- config/initializers/hacks/hyrax_collections_controller_behavior.rb
+++ config/initializers/hacks/hyrax_collections_controller_behavior.rb
@@ -5,4 +5,9 @@ Hyrax::CollectionsControllerBehavior.module_eval do
   def single_item_search_builder
     single_item_search_builder_class.new(self).with(params.except(:f, :q, :page))
   end
+
+  # Point Blacklight searching to collection route
+  def search_action_url options = {}
+    hyrax.collection_url(options.except(:controller, :action))
+  end
 end
