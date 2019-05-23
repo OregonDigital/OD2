ff42e65a8a2fb5a663b51c204164536186cb430b
Corey Gillen <gillenc@uoregon.edu>
2019-05-23 09:46:46 -0700

    Add some explicit OVERRIDE comments

--- config/initializers/hacks/hyrax_collections_controller_behavior.rb
+++ config/initializers/hacks/hyrax_collections_controller_behavior.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal:true
 
 Hyrax::CollectionsControllerBehavior.module_eval do
+  # OVERRIDE FROM HYRAX
   # Remove :f (facets) from single item search to allow collection to verify user access
   def single_item_search_builder
     single_item_search_builder_class.new(self).with(params.except(:f, :q, :page))
   end
 
+  # OVERRIDE FROM HYRAX
   def show
     @curation_concern ||= ActiveFedora::Base.find(params[:id])
+    # Set list of configured facets for the view to display
     configured_facets
     presenter
     query_collection_members
@@ -15,6 +18,7 @@ Hyrax::CollectionsControllerBehavior.module_eval do
 
   private
 
+    # OVERRIDE FROM HYRAX
     # Point Blacklight searching to collection route
     def search_action_url options = {}
       hyrax.collection_url(options.except(:controller, :action))
