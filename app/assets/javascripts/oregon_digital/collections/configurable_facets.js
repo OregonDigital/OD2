60e7ab11fa51dd1bb8bde130d665632593087608
Corey Gillen <gillenc@uoregon.edu>
2019-05-30 08:14:23 -0700

    Improve keyboard accessibility

--- app/assets/javascripts/oregon_digital/collections/configurable_facets.js
+++ app/assets/javascripts/oregon_digital/collections/configurable_facets.js
@@ -1,5 +1,30 @@
+// Add keyboard controls to jQuery.sortable()
+jQuery.fn.extend({
+  ksortable: function (options) {
+    this.sortable(options);
+    $('li', this).attr('tabindex', 0).bind('keydown', function (event) {
+      if (event.which == 37 || event.which == 38) { // left or up
+        $(this).insertBefore($(this).prev());
+        $(this).focus();
+      }
+      if (event.which == 39 || event.which == 40) { // right or down
+        $(this).insertAfter($(this).next());
+        $(this).focus();
+      }
+      if (event.which == 33) { // page-up
+        $(this).parent().prepend($(this));
+        $(this).focus();
+      }
+      if (event.which == 34) { // page-down
+        $(this).parent().append($(this));
+        $(this).focus();
+      }
+    });
+  }
+});
+
 $('#sortable_facets').ready(function () {
-  $('#sortable_facets').sortable({
+  $('#sortable_facets').ksortable({
     revert: 100,
     axis: 'y',
     containment: '#sortable_facets',
