429b83c133144e56b55e93dc3ed48743c77e4bd8
straleyb <straleyb@onid.oregonstate.edu>
2019-06-04 11:26:11 -0700

    fix spec?

--- spec/views/shared/_footer.html.erb_spec.rb
+++ spec/views/shared/_footer.html.erb_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe 'shared/_footer', type: :view do
   include Warden::Test::Helpers
   it 'renders the application version' do
+    request.env['warden']
     render
     expect(rendered).to have_text(OregonDigital::VERSION)
   end
