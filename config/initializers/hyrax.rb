cf8ef1c1589f317547ecd5171b29e60b73c1aacc
Corey Gillen <gillenc@uoregon.edu>
2019-06-06 11:07:10 -0700

    Hide share work button if user is logged out

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -125,7 +125,7 @@ Hyrax.config do |config|
   # config.work_requires_files = true
 
   # Should a button with "Share my work" show on the front page to all users (even those not logged in)?
-  # config.display_share_button_when_not_logged_in = true
+  config.display_share_button_when_not_logged_in = false
 
   # The user who runs batch jobs. Update this if you aren't using emails
   # config.batch_user_key = 'batchuser@example.com'
