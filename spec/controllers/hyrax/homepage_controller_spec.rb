cd3dd749b05f6cc352337e238da33df1d988dd95
Corey Gillen <gillenc@uoregon.edu>
2019-06-07 09:50:19 -0700

    more spec work

--- spec/controllers/hyrax/homepage_controller_spec.rb
+++ spec/controllers/hyrax/homepage_controller_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal:true
+
 RSpec.describe Hyrax::HomepageController, type: :controller do
   routes { Hyrax::Engine.routes }
 
-  describe "#index" do
+  describe '#index' do
     let(:user) { create(:user) }
 
     before do
@@ -11,49 +13,68 @@ RSpec.describe Hyrax::HomepageController, type: :controller do
     context 'with existing featured researcher' do
       let!(:frodo) { ContentBlock.create!(name: ContentBlock::NAME_REGISTRY[:researcher], value: 'Frodo Baggins', created_at: Time.zone.now) }
 
-      it 'finds the featured researcher' do
+      it 'finds a featured researcher' do
         get :index
         expect(response).to be_success
+      end
+      it 'finds the featured researcher' do
+        get :index
         expect(assigns(:featured_researcher)).to eq frodo
       end
     end
 
     context 'with no featured researcher' do
-      it "sets featured researcher" do
+      it 'responds' do
         get :index
         expect(response).to be_success
+      end
+
+      it 'creates a featured researcher' do
+        get :index
         assigns(:featured_researcher).tap do |researcher|
           expect(researcher).to be_kind_of ContentBlock
+        end
+      end
+      it 'sets featured researcher' do
+        get :index
+        assigns(:featured_researcher).tap do |researcher|
           expect(researcher.name).to eq 'featured_researcher'
         end
       end
     end
 
-    it "sets marketing text" do
+    it 'responds' do
       get :index
       expect(response).to be_success
+    end
+
+    it 'creates marketing text' do
+      get :index
       assigns(:marketing_text).tap do |marketing|
         expect(marketing).to be_kind_of ContentBlock
+      end
+    end
+    it 'sets marketing text' do
+      get :index
+      assigns(:marketing_text).tap do |marketing|
         expect(marketing.name).to eq 'marketing_text'
       end
     end
 
-    it "does not include other user's private documents in recent documents" do
+    it 'does not include other user\'s private documents in recent documents' do
       get :index
-      expect(response).to be_success
       titles = assigns(:recent_documents).map { |d| d['title_tesim'][0] }
       expect(titles).not_to include('Test Private Document')
     end
 
-    it "includes only Generic objects in recent documents" do
+    it 'includes only Generic objects in recent documents' do
       get :index
       assigns(:recent_documents).each do |doc|
-        byebug
-        expect(doc[Solrizer.solr_name("has_model", :symbol)]).to eql ["Generic"]
+        expect(doc[Solrizer.solr_name('has_model', :symbol)]).to eql ['Generic']
       end
     end
 
-    context "with a document not created this second", clean_repo: true do
+    context 'with a document not created this second', clean_repo: true do
       before do
         gw3 = Generic.new(title: ['Test 3 Document'], read_groups: ['public'])
         gw3.apply_depositor_metadata('mjg36')
@@ -68,16 +89,23 @@ RSpec.describe Hyrax::HomepageController, type: :controller do
         gw3.save
       end
 
-      it "sets recent documents in the right order" do
+      it 'responds' do
         get :index
         expect(response).to be_success
-        expect(assigns(:recent_documents).length).to be <= 4
+      end
+
+      it 'does not exceed the maximum recent documents' do
+        get :index
+        expect(assigns(:recent_documents).length).to be <= 6
+      end
+      it 'sets recent documents in the right order' do
+        get :index
         create_times = assigns(:recent_documents).map { |d| d['date_uploaded_dtsi'] }
         expect(create_times).to eq create_times.sort.reverse
       end
     end
 
-    context "with collections" do
+    context 'with collections' do
       let(:presenter) { double }
       let(:repository) { double }
       let(:collection_results) { double(documents: ['collection results']) }
@@ -89,49 +117,69 @@ RSpec.describe Hyrax::HomepageController, type: :controller do
                                                         .and_return(collection_results)
       end
 
-      it "initializes the presenter with ability and a list of collections" do
-        expect(Hyrax::HomepagePresenter).to receive(:new).with(Ability,
-                                                               ["collection results"])
-                                                         .and_return(presenter)
+      it 'responds' do
         get :index
         expect(response).to be_success
+      end
+
+      it 'initializes the presenter with ability and a list of collections' do
+        expect(Hyrax::HomepagePresenter).to receive(:new).with(Ability,
+                                                               ['collection results'])
+                                                         .and_return(presenter)
+        get :index
+      end
+      it 'assigns the presenter' do
+        allow(Hyrax::HomepagePresenter).to receive(:new).with(Ability,
+                                                               ['collection results'])
+                                                         .and_return(presenter)
+        get :index
         expect(assigns(:presenter)).to eq presenter
       end
     end
 
-    context "with featured works" do
+    context 'with featured works' do
       let!(:my_work) { create(:work, user: user) }
 
       before do
         FeaturedWork.create!(work_id: my_work.id)
       end
 
-      it "sets featured works" do
+      it 'responds' do
         get :index
         expect(response).to be_success
+      end
+
+      it 'sets featured works' do
+        get :index
         expect(assigns(:featured_work_list)).to be_kind_of FeaturedWorkList
       end
     end
 
-    it "sets announcement content block" do
+    it 'creates announcement content block' do
       get :index
-      expect(response).to be_success
       assigns(:announcement_text).tap do |announcement|
         expect(announcement).to be_kind_of ContentBlock
+      end
+    end
+    it 'sets announcement content block' do
+      get :index
+      assigns(:announcement_text).tap do |announcement|
         expect(announcement.name).to eq 'announcement_text'
       end
     end
 
-    context "without solr" do
+    context 'without solr' do
       before do
         allow(controller).to receive(:repository).and_return(instance_double(Blacklight::Solr::Repository))
         allow(controller.repository).to receive(:search).and_raise Blacklight::Exceptions::InvalidRequest
       end
 
-      it "errors gracefully" do
+      it 'errors admin_sets gracefully' do
         get :index
-        expect(response).to be_success
         expect(assigns(:admin_sets)).to be_blank
+      end
+      it 'errors recent_documents gracefully' do
+        get :index
         expect(assigns(:recent_documents)).to be_blank
       end
     end
