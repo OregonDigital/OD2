6db59073ea5e466086c89be7df85480a06f1f4bf
straleyb <straleyb@onid.oregonstate.edu>
2019-06-10 14:47:41 -0700

    more fixes

--- app/forms/hyrax/generic_form.rb
+++ app/forms/hyrax/generic_form.rb
@@ -30,7 +30,7 @@ module Hyrax
       Generic.controlled_property_labels.each do |prop|
         params << { prop.gsub('_label', '_attributes') => %i[id _destroy] }
       end
-      params << license
+      params << :license
       params
     end
   end
--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe GenericIndexer do
   let(:indexer) { described_class.new(work) }
   let(:solr_doc) { indexer.generate_solr_document }
-  let(:work) { build(:generic, resource_type: ['MyType'], license: ['MyLicense'], rights_statement: 'MyRights', language: ['MyLanguage']) }
+  let(:work) { build(:generic, resource_type: 'MyType', license: ['MyLicense'], rights_statement: ['MyRights'], language: ['MyLanguage']) }
 
   it { expect(solr_doc['resource_type_tesim']).to eq 'MyType' }
   it { expect(solr_doc['license_tesim']).to eq ['MyLicense'] }
