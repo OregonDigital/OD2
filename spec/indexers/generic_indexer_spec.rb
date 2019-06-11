3bcce44cbe9df774f0c391c4dfc6b10c2a0f3745
straleyb <straleyb@onid.oregonstate.edu>
2019-06-11 14:19:27 -0700

    fix spec

--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe GenericIndexer do
   let(:solr_doc) { indexer.generate_solr_document }
   let(:work) { Generic.new(resource_type: 'MyType') }
 
-  it { expect(solr_doc.type_label).to eq 'MyType' }
+  it { expect(solr_doc['type_label_tesim']).to eq 'MyType' }
   it { expect(solr_doc['license_tesim']).to eq ['MyLicense'] }
   it { expect(solr_doc['language_tesim']).to eq ['MyLanguage'] }
   it { expect(solr_doc['rights_statement_tesim']).to eq ['MyRights'] }
