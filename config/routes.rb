8a83aacc07e3ef84b6ff0d1c6f8a12fe70df07ed
Corey Gillen <gillenc@uoregon.edu>
2019-06-11 12:51:30 -0700

    Move routes overrides into controller overrides

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal:true
 
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :collection_types, except: :show, controller: 'oregon_digital/collection_types'
-  end
-
   mount BrowseEverything::Engine => '/browse'
   mount Blacklight::Oembed::Engine, at: 'oembed'
   mount Blacklight::Engine => '/'
@@ -34,19 +30,6 @@ Rails.application.routes.draw do
   root 'hyrax/homepage#index'
   curation_concerns_basic_routes
   concern :exportable, Blacklight::Routes::Exportable.new
-  namespace :dashboard do
-    resources :collections, controller: 'oregon_digital/collections' do # Dashboard -> All Collections and CRUD
-      member do
-        get 'page/:page', action: :index
-        get 'facet/:id', action: :facet, as: :dashboard_facet
-        get :files
-      end
-      collection do
-        put '', action: :update
-        put :remove_member
-      end
-    end
-  end
 
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
