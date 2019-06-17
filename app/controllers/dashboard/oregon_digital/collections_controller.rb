36af95de220674d06d8056d01ba72cd5edf58049
straleyb <straleyb@onid.oregonstate.edu>
2019-06-17 07:59:24 -0700

    more lint

--- app/controllers/dashboard/oregon_digital/collections_controller.rb
+++ app/controllers/dashboard/oregon_digital/collections_controller.rb
@@ -4,7 +4,6 @@ module Dashboard
   module OregonDigital
     ## Shows a list of all collections to the admins
     class CollectionsController < Hyrax::Dashboard::CollectionsController
-
       self.form_class = OregonDigital::Forms::CollectionForm
       self.presenter_class = OregonDigital::CollectionPresenter
 
