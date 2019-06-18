c4988eb1692fd60272ad405c4bebcac2dd6f222f
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2019-06-18 14:00:00 -0700

    begin work on configured collection metadata (#569)
    * begin work on configured collection metadata
    
    * process of updating collection metadata
    
    * Almost there, just need select2 stuff working
    
    * update to use select2 on collection page
    
    * update form to have all metadata
    
    * first refactor
    
    * remove input
    
    * refactor presenter
    
    * fix rubocop issues
    
    * more lint
    
    * typo
    
    * more updates
    
    * updates
    
    * exclude
    
    * updates collection form
    
    * specs
    
    * fixes
    
    * rubocop
    
    * more rspec
    
    * final fix
    
    * no collection_lw
    
    * fix spec

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,8 +6,8 @@ class Collection < ActiveFedora::Base
 
   include ::Hyrax::CollectionBehavior
   # You can replace these metadata if they're not suitable
-  include Hyrax::BasicMetadata
-  self.indexer = Hyrax::CollectionWithBasicMetadataIndexer
+  include OregonDigital::CollectionMetadata
+  self.indexer = Hyrax::CollectionIndexer
 
   delegate :facet_configurable?, to: :collection_type
 
---
+++ spec/forms/oregon_digital/forms/collection_form_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+RSpec.describe OregonDigital::Forms::CollectionForm do
+  let(:collection) { build(:collection) }
+  let(:ability) { Ability.new(create(:user)) }
+  let(:repository) { double }
+  let(:form) { described_class.new(collection, ability, repository) }
+
+  describe '#terms' do
+    let(:terms) { described_class.terms }
+
+    it do
+      expect(terms).to eq %i[resource_type
+                             title
+                             creator
+                             contributor
+                             description
+                             license
+                             publisher
+                             date_created
+                             subject
+                             language
+                             representative_id
+                             thumbnail_id
+                             identifier
+                             related_url
+                             visibility
+                             collection_type_gid
+                             institution
+                             date
+                             repository]
+    end
+  end
+
+  describe '#primary_terms' do
+    let(:primary_terms) { form.primary_terms }
+
+    it {
+      expect(primary_terms).to eq(%i[title description
+                                     creator contributor
+                                     license publisher
+                                     date_created subject
+                                     language identifier
+                                     related_url resource_type
+                                     institution date
+                                     repository])
+    }
+  end
+
+  describe '#secondary_terms' do
+    let(:secondary_terms) { form.secondary_terms }
+
+    it { expect(secondary_terms).to eq [] }
+  end
+
+  describe '#[]' do
+    it 'has one element' do
+      expect(form['description']).to eq ['']
+    end
+  end
+
+  describe 'initialized fields' do
+    context 'with :description' do
+      let(:description) { form[:description] }
+
+      it { expect(description).to eq [''] }
+    end
+  end
+end
