5c0215ea3fd3100b5bee83ffd29a2e17b7760406
Corey Gillen <gillenc@uoregon.edu>
2019-07-16 13:21:30 -0700

    Add and fix spec for homepage accessibility

--- spec/system/homepage_accessibility_spec.rb
+++ spec/system/homepage_accessibility_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal:true
 
 RSpec.describe 'Homepage',  js: true, type: :system, clean_repo: true do
+  before do
+    create(:content_block, name: 'marketing_text', value: '<p>Title</p>')
+  end
   context 'with an annonymous user' do
     it 'is accessible' do
       visit '/'
