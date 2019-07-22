5a6e94667c268fe1c0c9e6156843a40053e07dc9
straleyb <straleyb@onid.oregonstate.edu>
2019-07-22 10:47:00 -0700

    move solrize deeper to work for all vocabs

--- lib/oregon_digital/controlled_vocabularies/resource.rb
+++ lib/oregon_digital/controlled_vocabularies/resource.rb
@@ -37,9 +37,9 @@ module OregonDigital
 
       # Return a tuple of url & label
       def solrize
-        return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label_uri_same?
+        return [rdf_subject.to_s] if rdf_label.first.to_s.blank? || rdf_label.first.to_s == rdf_subject.to_s
 
-        [rdf_subject.to_s, { label: "#{rdf_label.first}$#{rdf_subject}" }]
+        [rdf_subject.to_s, { label: "#{rdf_label.select { |label| label.language == :en }.first}$#{rdf_subject}" }]
       end
 
       # Sanity check for valid rdf_subject. Subject should never be blank but in the event,
