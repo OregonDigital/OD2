a610abfc164b909dc66b9366caab7ef02832a4b9
Corey Gillen <gillenc@uoregon.edu>
2019-07-22 16:30:01 -0700

    rspec

--- app/helpers/oregon_digital/citations_behaviors/name_behavior.rb
+++ app/helpers/oregon_digital/citations_behaviors/name_behavior.rb
@@ -10,7 +10,7 @@ module OregonDigital
       # return all unique authors of a work or nil if none
       def all_authors(work, &block)
         # OVERRIDE FROM HYRAX to grab creator_label as author
-        authors = work.creator_label.uniq.compact
+        authors = work.creator_label.empty? ? [] : work.creator_label.uniq.compact
         # END OVERRIDE
         block_given? ? authors.map(&block) : authors
       end
