5ab39166cd280e45bc5d51df3334f43c45aab748
straleyb <straleyb@onid.oregonstate.edu>
2019-07-24 07:57:33 -0700

    configure loadpath

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module OD2
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths += %W[#{config.root}/lib]
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
