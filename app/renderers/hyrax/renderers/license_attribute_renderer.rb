06a973b7e02444c373ed18789fec982efc3e75a1
Corey Gillen <gillenc@uoregon.edu>
2019-07-31 09:10:13 -0700

    rubocop

--- app/renderers/hyrax/renderers/license_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/license_attribute_renderer.rb
@@ -7,22 +7,22 @@ module Hyrax
     class LicenseAttributeRenderer < AttributeRenderer
       private
 
-        ##
-        # Special treatment for license/rights.  A URL from the Hyrax gem's config/hyrax.rb is stored in the descMetadata of the
-        # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
-        def attribute_value_to_html(value)
-          begin
-            parsed_uri = URI.parse(value)
-          rescue URI::InvalidURIError
-            nil
-          end
-          if parsed_uri.nil?
-            ERB::Util.h(value)
-          else
-            # OVERRIDE FROM HYRAX to remove open-in-new-tab functionality
-            %(<a href=#{ERB::Util.h(value)}>#{Hyrax.config.license_service_class.new.label(value)}</a>)
-          end
+      ##
+      # Special treatment for license/rights.  A URL from the Hyrax gem's config/hyrax.rb is stored in the descMetadata of the
+      # curation_concern.  If that URL is valid in form, then it is used as a link.  If it is not valid, it is used as plain text.
+      def attribute_value_to_html(value)
+        begin
+          parsed_uri = URI.parse(value)
+        rescue URI::InvalidURIError
+          nil
         end
+        if parsed_uri.nil?
+          ERB::Util.h(value)
+        else
+          # OVERRIDE FROM HYRAX to remove open-in-new-tab functionality
+          %(<a href=#{ERB::Util.h(value)}>#{Hyrax.config.license_service_class.new.label(value)}</a>)
+        end
+      end
     end
   end
 end
