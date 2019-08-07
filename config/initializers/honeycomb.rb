da014a690a195c8687f5c2b32c69cdd020cf5cb6
straleyb <straleyb@onid.oregonstate.edu>
2019-08-07 11:56:49 -0700

    move keys around

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,6 +1,6 @@
 Honeycomb.configure do |config|
-  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'd5529d94d4e989bb1fc1b1cfc559ebba')
-  config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'od2-rails-staging')
+  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'hereisareallylonglookingkey')
+  config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'od2-something-something')
   config.notification_events = %w[
     sql.active_record
     render_template.action_view
