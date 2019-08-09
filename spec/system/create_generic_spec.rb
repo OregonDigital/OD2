1b11f6a4b987aa8b39bf1d9edddf166a08a9e69b
lsat12357 <lsato@uoregon.edu>
2019-08-09 16:13:35 -0700

    rubocop multiple expectations (#700)
    * rubocop multiple expectations
    
    * remove spec disable
    
    * remove expectation length
    
    * disable example length

--- spec/system/create_generic_spec.rb
+++ spec/system/create_generic_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe 'Create a Generic', js: true, type: :system, clean_repo: true do
       sign_in_as user
     end
 
-    # rubocop:disable RSpec/MultipleExpectations, RSpec/ExampleLength
     it 'Creates an Generic' do
       visit new_hyrax_generic_path
 
@@ -66,6 +65,5 @@ RSpec.describe 'Create a Generic', js: true, type: :system, clean_repo: true do
       save_screenshot if ENV.fetch('CI', nil)
       # rubocop:enable Lint/Debugger
     end
-    # rubocop:enable RSpec/MultipleExpectations, RSpec/ExampleLength
   end
 end
