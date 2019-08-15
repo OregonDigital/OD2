698c79880fd830a23f3be47d36ab49ff6501e922
Corey Gillen <gillenc@uoregon.edu>
2019-08-15 12:51:51 -0700

    Add options to max_values and max_length pipelines based on BL view mode

--- app/presenters/blacklight/rendering/max_length.rb
+++ app/presenters/blacklight/rendering/max_length.rb
@@ -7,8 +7,20 @@ module Blacklight
       def render
         return next_step(values) unless config.truncate
 
-        truncate_length = config.truncate.is_a?(Integer) ? config.truncate : 20
         next_step(values.map { |x| x.truncate_words(truncate_length) })
+      end
+
+      private
+
+      def truncate_length
+        default = 20
+        if config.truncate.is_a?(Hash)
+          config.truncate[context.document_index_view_type] || default
+        elsif config.truncate.is_a?(Integer)
+          config.truncate
+        else
+          default
+        end
       end
     end
   end
