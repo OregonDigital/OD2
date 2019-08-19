4a0a102578cbe6ebe98125948e67f03673f2377d
Corey Gillen <gillenc@uoregon.edu>
2019-08-19 10:30:09 -0700

    Limit search shown fields (#727)
    Break up max_value and max_value_label pipeline steps
    
    Limit description to 1 value

--- config/initializers/blacklight.rb
+++ config/initializers/blacklight.rb
@@ -1,6 +1,7 @@
 Blacklight::Rendering::Pipeline.operations = [Blacklight::Rendering::MaxLength,
+                                              Blacklight::Rendering::MaxValues,
                                               Blacklight::Rendering::HelperMethod,
                                               Blacklight::Rendering::LinkToFacet,
                                               Blacklight::Rendering::Microdata,
-                                              Blacklight::Rendering::MaxValues,
+                                              Blacklight::Rendering::MaxValuesLabel,
                                               Blacklight::Rendering::Join]
