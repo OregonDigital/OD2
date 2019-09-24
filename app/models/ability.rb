a02dc5fb22cfecbbad34d09dd2d16eec3ed40c1c
straleyb <straleyb@onid.oregonstate.edu>
2019-09-24 13:13:15 -0700

    update to remove create from base permission

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,7 +17,7 @@ class Ability
   def work_classes
     [SolrDocument, ActiveFedora::Base]
     # TODO: THIS WILL BE REMOVED ONCE WE NAIL DOWN THE ROLES AND PERMISSIONS MORE
-    can(%i[create show add_user remove_user index edit update destroy], Role) if current_user.admin?
+    can(%i[show add_user remove_user index edit update destroy], Role) if current_user.admin?
     can(:create, ActiveFedora::Base) if current_user.roles.map(&:name).include?(create_permissions)
   end
 
