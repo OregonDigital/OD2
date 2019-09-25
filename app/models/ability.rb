b578839dafdef8112fc45cb52826f69e933fea64
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2019-09-25 12:37:14 -0700

    update ability for create and spec (#765)
    * update ability for create and spec
    
    * fix linting
    
    * more specs
    
    * map over roles
    
    * proc
    
    * update spec
    
    * update lint
    
    * update specs
    
    * update to remove create from base permission
    
    * refine test better
    
    * more granular check for perms
    
    * lint
    
    * fix self issue
    
    * update system specs
    
    * change button danger contrast
    
    * update to background color
    
    * updates
    
    * rebase
    
    * lint

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,32 +3,27 @@
 # Sets the abilities of the user and authorizes actions
 class Ability
   include Hydra::Ability
-
   include Hyrax::Ability
-
+  include OregonDigital::Ability::WorkCreateAbility
   include OregonDigital::Ability::WorkEditAbility
 
-  # Define any customized permissions here.
   def custom_permissions
-    # Limits deleting objects to a the admin user
-    #
-    # if current_user.admin?
-    #   can [:destroy], ActiveFedora::Base
-    # end
-
-    # Limits creating new objects to a specific group
-    #
-    # if user_groups.include? 'special_group'
-    #   can [:create], ActiveFedora::Base
-    # end
-
-    can(%i[create show add_user remove_user index edit update destroy], Role) if current_user.admin?
+    can(%i[show add_user remove_user index edit update destroy], Role) if current_user.admin?
 
     # Apply works edit permissions
     work_edit_ability
+    work_create_ability
   end
 
   def work_classes
     [SolrDocument, ActiveFedora::Base]
+  end
+
+  def admin_permission_roles
+    %w[admin collection_curator]
+  end
+
+  def manager_permission_roles
+    admin_permission_roles << 'depositor'
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -3,14 +3,18 @@
 describe Ability do
   let(:ability) { described_class.new(user) }
 
-  context 'when a user is an admin' do
-    let(:user) { create(:user) }
+  context 'when a user as an admin' do
+    let(:ability) { described_class.new(user) }
+    let(:user) do
+      u = create(:user)
+      r = role
+      r.users << u
+      r.save
+      u
+    end
     let(:role) { Role.create(name: 'admin') }
 
-    before do
-      user.roles << role
-      user.save
-    end
+    it { expect(ability).to be_able_to(:create, ActiveFedora::Base) }
 
     it 'can edit works' do
       Hyrax.config.curation_concerns.each do |type|
