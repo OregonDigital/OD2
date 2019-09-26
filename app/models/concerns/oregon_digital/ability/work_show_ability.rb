f2a42bedd8f7f4e23a849d5579c2754061c89e0a
Corey Gillen <gillenc@uoregon.edu>
2019-09-26 09:17:43 -0700

    Add UO & OSU restriction boilerplate

--- app/models/concerns/oregon_digital/ability/work_show_ability.rb
+++ app/models/concerns/oregon_digital/ability/work_show_ability.rb
@@ -8,8 +8,11 @@ module OregonDigital
 
       included do
         def work_show_ability
-          can(%i[show], ActiveFedora::Base) if current_user.role?(manager_permission_roles + community_roles)
+          can(%i[show], ActiveFedora::Base) if current_user.role?(manager_permission_roles)
           # TODO: Configure UO & OSU rules based on how we determine work restriction
+          cannot(%i[show], ActiveFedora::Base) if current_user.role?(uo_roles)
+          cannot(%i[show], ActiveFedora::Base) if current_user.role?(osu_roles)
+          cannot(%i[show], ActiveFedora::Base) if current_user.id.nil?
         end
       end
     end
