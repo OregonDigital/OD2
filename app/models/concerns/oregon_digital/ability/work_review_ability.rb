80acf6f0c12c30442c9be9dcacd2ca98fdc273c4
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2019-10-03 09:47:05 -0700

    work on setting up initial ability for review (#784)
    * work on setting up initial ability for review
    
    * Disallows collection curator from reviewing own works
    
    * update suppressed
    
    * updates lint
    
    * ignore abc size
    
    * refine review stuff
    
    * lint updates
    
    * more lint
    
    * update methods

---
+++ app/models/concerns/oregon_digital/ability/work_review_ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal:true
+
+module OregonDigital
+  module Ability
+    # Sets the abilities for work displaying
+    module WorkReviewAbility
+      extend ActiveSupport::Concern
+
+      included do
+        def work_review_ability
+          can :review, SolrDocument do |solr_doc|
+            can_review_solr_doc?(solr_doc.depositor) && can_review_submissions? && is_in_depositors_collection?(solr_doc.edit_people)
+          end
+        end
+
+        def can_review_solr_doc?(depositor)
+          current_user.admin? || depositor != current_user.email
+        end
+      end
+    end
+  end
+end
