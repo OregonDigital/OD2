231bfb0ee4336376df9f5a38a414341be5d7996b
Jeremy Echols <jechols@uoregon.edu>
2019-10-08 14:01:18 -0700

    First run: make sure the admin role exists (#806)

--- build/firstrun.sh
+++ build/firstrun.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+echo "Creating default admin set..."
 bundle exec rails hyrax:default_admin_set:create
+
+echo
+echo "Creating default collection types..."
 bundle exec rails hyrax:default_collection_types:create
+
+echo
+echo "Loading workflow stuff..."
 bundle exec rails hyrax:workflow:load
-bundle exec rails runner 'User.create(email: "admin@example.org", encrypted_password: "$2a$11$AzzYy9sCWJh6JCKu0qjvdeiSDp0e6DS2rdTOtDEHV2UZw/cQ0ltnG", preferred_locale: "en")'
-bundle exec rails runner 'User.last.roles << Role.find_by_name("admin")'
+
+echo
+echo "Creating the admin role"
+bundle exec rails runner "Role.create(name: 'admin')"
+
+echo
+echo "Creating a default admin user"
+bundle exec rails runner 'User.create(email: "admin@example.org", encrypted_password: "$2a$11$AzzYy9sCWJh6JCKu0qjvdeiSDp0e6DS2rdTOtDEHV2UZw/cQ0ltnG", preferred_locale: "en", roles: [Role.last])'
