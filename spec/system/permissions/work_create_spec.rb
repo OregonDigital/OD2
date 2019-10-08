fba2b2661138ce8d2e0f9fc12a8b6f77b415aedb
Corey Gillen <gillenc@uoregon.edu>
2019-10-08 12:39:49 -0700

    Add spec for work creation

---
+++ spec/system/permissions/work_create_spec.rb
@@ -0,0 +1,306 @@
+# frozen_string_literal:true
+
+RSpec.describe 'Create a work', js: true, type: :system, clean_repo: true do
+  let(:user) { create(:user) }
+  let!(:ability) { ::Ability.new(user) }
+  let(:upload_file_path) { "#{Rails.root}/spec/fixtures/test.jpg" }
+
+  context 'Without any roles' do
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+      sign_in_as user
+    end
+
+    it 'Blocks work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'You are not authorized'
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With community affiliate role' do
+    let(:role) { Role.new(name: 'community_affiliate') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Blocks work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'You are not authorized'
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With osu role' do
+    let(:role) { Role.new(name: 'osu_user') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Blocks work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'You are not authorized'
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With uo role' do
+    let(:role) { Role.new(name: 'uo_user') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Blocks work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'You are not authorized'
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With depositor role' do
+    let(:role) { Role.new(name: 'depositor') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Allows work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'Add New Generic'
+      click_link 'Files' # switch tab
+      expect(page).to have_content 'Add files'
+      expect(page).to have_content 'Add folder'
+      within('span#addfiles') do
+        page.execute_script("$('input[type=file]').css('opacity','1')")
+        page.execute_script("$('input[type=file]').css('position','inherit')")
+        attach_file('files[]', upload_file_path)
+      end
+      click_link 'Descriptions' # switch tab
+      within('div.generic_title') do
+        fill_in('Title', with: 'Test Title')
+      end
+      within('div.generic_identifier') do
+        fill_in('Identifier', with: 'Test ID')
+      end
+      within('div.generic_resource_type') do
+        select('Dataset', from: 'Type')
+      end
+      select('In Copyright', from: 'Rights')
+
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      choose('generic_visibility_open')
+      expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      check('agreement', visible: false)
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      click_on 'Save'
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content 'Your files are being processed by Oregon Digital (Development) in the background.'
+      expect(page).to be_accessible.skipping('aria-allowed-role').excluding('.label-success')
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With curation curator role' do
+    let(:role) { Role.new(name: 'collection_curator') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Allows work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'Add New Generic'
+      click_link 'Files' # switch tab
+      expect(page).to have_content 'Add files'
+      expect(page).to have_content 'Add folder'
+      within('span#addfiles') do
+        page.execute_script("$('input[type=file]').css('opacity','1')")
+        page.execute_script("$('input[type=file]').css('position','inherit')")
+        attach_file('files[]', upload_file_path)
+      end
+      click_link 'Descriptions' # switch tab
+      within('div.generic_title') do
+        fill_in('Title', with: 'Test Title')
+      end
+      within('div.generic_identifier') do
+        fill_in('Identifier', with: 'Test ID')
+      end
+      within('div.generic_resource_type') do
+        select('Dataset', from: 'Type')
+      end
+      select('In Copyright', from: 'Rights')
+
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      choose('generic_visibility_open')
+      expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      check('agreement', visible: false)
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      click_on 'Save'
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content 'Your files are being processed by Oregon Digital (Development) in the background.'
+      expect(page).to be_accessible.skipping('aria-allowed-role').excluding('.label-success')
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+
+  context 'With admin role' do
+    let(:role) { Role.new(name: 'admin') }
+
+    before do
+      create(:permission_template_access,
+             :deposit,
+             permission_template: create(:permission_template, with_admin_set: true, with_active_workflow: true),
+             agent_type: 'user',
+             agent_id: user.user_key)
+      allow(CharacterizeJob).to receive(:perform_later)
+
+      user.roles = [ role ]
+      user.save
+      sign_in_as user
+    end
+
+    it 'Allows work creation' do
+      visit new_hyrax_generic_path
+
+      expect(page).to have_content 'Add New Generic'
+      click_link 'Files' # switch tab
+      expect(page).to have_content 'Add files'
+      expect(page).to have_content 'Add folder'
+      within('span#addfiles') do
+        page.execute_script("$('input[type=file]').css('opacity','1')")
+        page.execute_script("$('input[type=file]').css('position','inherit')")
+        attach_file('files[]', upload_file_path)
+      end
+      click_link 'Descriptions' # switch tab
+      within('div.generic_title') do
+        fill_in('Title', with: 'Test Title')
+      end
+      within('div.generic_identifier') do
+        fill_in('Identifier', with: 'Test ID')
+      end
+      within('div.generic_resource_type') do
+        select('Dataset', from: 'Type')
+      end
+      select('In Copyright', from: 'Rights')
+
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      choose('generic_visibility_open')
+      expect(page).to have_content('Please note, making something visible to the world (i.e. marking this as Public) may be viewed as publishing which could impact your ability to')
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      check('agreement', visible: false)
+      # Selenium/chrome on CircleCI requires the focus to change after the previous method
+      find('body').click
+
+      click_on 'Save'
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content 'Your files are being processed by Oregon Digital (Development) in the background.'
+      expect(page).to be_accessible.skipping('aria-allowed-role').excluding('.label-success')
+
+      # save a successful screenshot if running in CI for build artifacts
+      # rubocop:disable Lint/Debugger
+      save_screenshot if ENV.fetch('CI', nil)
+      # rubocop:enable Lint/Debugger
+    end
+  end
+end
