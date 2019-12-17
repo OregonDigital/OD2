83ab74b50e7ee55f9bc164dc920d6ee9e52caf25
Corey Gillen <gillenc@uoregon.edu>
2019-12-17 11:03:28 -0800

    Restrict password change to non-institutional users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,31 @@
 # frozen_string_literal: true
 
-require 'mail'
-
 # This class handles registering users.
 # When it receives an email from a university it redirects
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :redirect_if_university, only: [:create]
+  before_action :redirect_create_if_university, only: [:create]
+  before_action :redirect_edit_if_university, only: [:edit]
 
   protected
 
-  def redirect_if_university
-    service = OregonDigital::UserAttributeService.new(params[:user])
-    redirect_path = service.email_redirect_path
+  def redirect_create_if_university
+    redirect_path = insitutional_redirect_page(params[:user])
     return if redirect_path.nil?
 
     flash[:error] = 'Please sign in through your university below.'
     redirect_to redirect_path
+  end
+
+  def redirect_edit_if_university
+    redirect_path = insitutional_redirect_page(current_user)
+    return if redirect_path.nil?
+
+    flash[:error] = 'University members are unable to change their password here. Please refer to instructions provided by your university to change your password.'
+    redirect_to hyrax.dashboard_path
+  end
+
+  def insitutional_redirect_page(user)
+    service = OregonDigital::UserAttributeService.new(user)
+    service.email_redirect_path
   end
 end
