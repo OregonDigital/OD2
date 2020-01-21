ab82c2227f5f8042ea9c3fb530c64a849075ae0d
Corey Gillen <gillenc@uoregon.edu>
2020-01-21 10:53:07 -0800

    Fix indexer logging about cache & authorative source

--- app/indexers/oregon_digital/deep_indexing_service.rb
+++ app/indexers/oregon_digital/deep_indexing_service.rb
@@ -14,7 +14,6 @@ module OregonDigital
 
     # OVERRIDEN FROM HYRAX TO ADD GRAPH FETCH RETRY QUEUE
     def fetch_value(value)
-      Rails.logger.info "Fetching #{value.rdf_subject} from the authorative source. (this is slow)"
       value.fetch(headers: { 'Accept' => default_accept_header })
     rescue Net::ReadTimeout, IOError, SocketError, TriplestoreAdapter::TriplestoreException => e
       # IOError could result from a 500 error on the remote server
