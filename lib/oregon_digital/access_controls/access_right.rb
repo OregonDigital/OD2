9366797ebe594a1cd66aa1adddc81e170cfc51a8
straleyb <straleyb@onid.oregonstate.edu>
2020-02-05 13:28:10 -0800

    unfreeze the frozen

--- lib/oregon_digital/access_controls/access_right.rb
+++ lib/oregon_digital/access_controls/access_right.rb
@@ -4,8 +4,8 @@ module OregonDigital
   module AccessControls
     # Contains the constants for OSU and UO visibility
     class AccessRight
-      PERMISSION_TEXT_VALUE_OSU = 'osu'.freeze
-      PERMISSION_TEXT_VALUE_UO = 'uo'.freeze
+      PERMISSION_TEXT_VALUE_OSU = 'osu'
+      PERMISSION_TEXT_VALUE_UO = 'uo'
     end
   end
 end
