4da409ae7d594cfef06578b5cbb71796a37484f8
straleyb <straleyb@onid.oregonstate.edu>
2020-02-10 13:34:40 -0800

    update edit

--- spec/controllers/oregon_digital/oembeds_controller_spec.rb
+++ spec/controllers/oregon_digital/oembeds_controller_spec.rb
@@ -38,20 +38,34 @@ RSpec.describe OregonDigital::OembedsController do
     context 'when I do not have edit permissions for the object' do
       it 'redirects' do
         get :edit, params: { id: not_my_work }
-        expect(response.status).to eq 302
         expect(flash[:alert]).to eq 'You are not authorized to access this page.'
       end
     end
 
     context 'when I have permission to edit the object' do
-      it 'shows me the page' do
+      it 'renders the dashboard' do
+        get :edit, params: { id: a_work }
+        expect(response).to render_template('dashboard')
+      end
+
+      it "sets up home breadcrumbs" do
         expect(controller).to receive(:add_breadcrumb).with('Home', controller.root_path)
+        get :edit, params: { id: a_work }
+      end
+
+      it "sets up dashboard breadcrumbs" do
         expect(controller).to receive(:add_breadcrumb).with('Dashboard', controller.hyrax.dashboard_path)
+        get :edit, params: { id: a_work }
+      end 
+
+      it "sets up manage oembeds breadcrumbs" do
         expect(controller).to receive(:add_breadcrumb).with('Manage oEmbeds', Rails.application.routes.url_helpers.oembeds_path)
+        get :edit, params: { id: a_work }
+      end
+
+      it "sets up update oembeds breadcrumbs" do
         expect(controller).to receive(:add_breadcrumb).with('Update oEmbed', '#')
         get :edit, params: { id: a_work }
-        expect(response).to be_success
-        expect(response).to render_template('dashboard')
       end
     end
   end
