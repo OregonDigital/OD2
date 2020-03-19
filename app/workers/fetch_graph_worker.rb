0757f58b6c202cdfc7147ed4618d51184d7ef646
straleyb <straleyb@onid.oregonstate.edu>
2020-03-19 10:24:06 -0700

    rubocop

--- app/workers/fetch_graph_worker.rb
+++ app/workers/fetch_graph_worker.rb
@@ -8,6 +8,8 @@ class FetchGraphWorker
   # JOBS TEND TOWARD BEING LARGE. DISABLED BECAUSE FETCHING IS HEAVY HANDED.
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def perform(pid, _user_key)
     # Fetch Work and SolrDoc
     work = ActiveFedora::Base.find(pid)
@@ -40,11 +42,11 @@ class FetchGraphWorker
           # Insert into SolrDocument
           if val.is_a?(String)
             Solrizer.insert_field(solr_doc, "#{controlled_prop}_label", val, behavior)
-            Solrizer.insert_field(solr_doc, "location_combined_facet", val, behavior) if location_combined_facet?(controlled_prop)
+            Solrizer.insert_field(solr_doc, 'location_combined_facet', val, behavior) if location_combined_facet?(controlled_prop)
           else
             extractred_val = val.solrize.last.is_a?(String) ? val.solrize.last : val.solrize.last[:label].split('$').first
             Solrizer.insert_field(solr_doc, "#{controlled_prop}_label", [extractred_val], behavior)
-            Solrizer.insert_field(solr_doc, "location_combined_facet", [extractred_val], behavior) if location_combined_facet?(controlled_prop)
+            Solrizer.insert_field(solr_doc, 'location_combined_facet', [extractred_val], behavior) if location_combined_facet?(controlled_prop)
           end
         end
       end
@@ -56,6 +58,8 @@ class FetchGraphWorker
   end
   # rubocop:enable Metrics/MethodLength
   # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
 
   # TODO: WILL INTEGRATE THIS WHEN REDOING EMAILING FOR THESE JOBS
   # def fetch_failed_callback(user, val)
@@ -71,6 +75,6 @@ class FetchGraphWorker
   end
 
   def location_combined_facet?(controlled_prop)
-    [:ranger_district, :water_basin, :location].include? controlled_prop
+    %i[ranger_district water_basin location].include? controlled_prop
   end
 end
