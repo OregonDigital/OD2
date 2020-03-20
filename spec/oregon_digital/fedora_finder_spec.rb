79f1909dd0d73275f223013e01bc73cc8bd2cf13
Jeremy Echols <jechols@uoregon.edu>
2020-03-20 13:17:41 -0700

    Test more aspects of the FedoraFinder

--- spec/oregon_digital/fedora_finder_spec.rb
+++ spec/oregon_digital/fedora_finder_spec.rb
@@ -9,40 +9,49 @@ RSpec.describe OregonDigital::FedoraFinder do
     end
   end
 
-  let(:objects) do
-    o = nil
-    VCR.use_cassette('fedora-fetch', record: :none) do
-      o = described_class.new(ActiveFedora.fedora.base_uri).fetch_all
-    end
-
-    o
-  end
-
   describe '.fetch_all' do
-    let(:collections) { objects.select { |o| o.model == 'Collection' } }
-    let(:images) { objects.select { |o| o.model == 'Image' } }
-    let(:admin_sets) { objects.select { |o| o.model == 'AdminSet' } }
-
-    it 'has two collections' do
-      expect(collections.length).to eq(2)
+    before do
+      VCR.use_cassette('fedora-fetch', record: :none) do
+        finder.fetch_all
+      end
     end
+
+    let(:finder) { described_class.new(ActiveFedora.fedora.base_uri) }
+    let(:raw_collections) { finder.all_objects.select { |o| o.model == 'Collection' } }
+    let(:raw_images) { finder.all_objects.select { |o| o.model == 'Image' } }
+    let(:raw_filesets) { finder.all_objects.select { |o| o.model == 'FileSet' } }
+    let(:raw_admin_sets) { finder.all_objects.select { |o| o.model == 'AdminSet' } }
 
     it 'has the Gifford and OSU SCARC collections' do
-      expect(collections.collect { |c| c.pid }.sort.join(' ')).to eq('gifford osu-scarc')
+      expect(raw_collections.collect { |c| c.pid }.sort.join(' ')).to eq('gifford osu-scarc')
     end
 
     it 'returns nine images' do
-      expect(images.length).to eq(9)
+      expect(raw_images.length).to eq(9)
     end
 
     it 'returns all our admin sets' do
       expected = 'admin_set/default nwcu oac ohs osu osu-scarc-admin uo uo-jsma uo-mc uo-scua'
-      expect(admin_sets.collect { |as| as.pid }.sort.join(' ')).to eq(expected)
+      expect(raw_admin_sets.collect { |as| as.pid }.sort.join(' ')).to eq(expected)
     end
 
     it 'returns one file per TIFF' do
-      files = objects.select { |o| o.model == '<blob>' }
-      expect(files.length).to eq(images.length)
+      files = finder.all_objects.select { |o| o.model == '<blob>' }
+      expect(files.length).to eq(raw_images.length)
+    end
+
+    it 'categorizes assets properly' do
+      pids = finder.assets.collect {|o| o.pid }.sort.join(' ')
+      assets = raw_images + raw_filesets
+      raw_asset_pids = assets.collect {|o| o.pid }.sort.join(' ')
+      expect(pids).to eq(raw_asset_pids)
+    end
+
+    it 'categorizes all admin sets and collections as "collections"' do
+      pids = finder.collections.collect {|c| c.pid}.sort.join(' ')
+      colls = raw_admin_sets + raw_collections
+      raw_pids = colls.collect {|c| c.pid}.sort.join(' ')
+      expect(pids).to eq(raw_pids)
     end
   end
 end
