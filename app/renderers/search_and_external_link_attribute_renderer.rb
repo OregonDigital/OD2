114de84563ffd3e707384a1ffd0a5c7942a4ecc9
straleyb <straleyb@onid.oregonstate.edu>
2020-03-30 12:26:00 -0700

    First pass to generalize external link fields

--- app/renderers/search_and_external_link_attribute_renderer.rb
+++ app/renderers/search_and_external_link_attribute_renderer.rb
@@ -11,4 +11,36 @@ class SearchAndExternalLinkAttributeRenderer < Hyrax::Renderers::LinkedAttribute
     links << link_to('<span class="glyphicon glyphicon-new-window"></span>'.html_safe, external_link, 'aria-label' => 'Open link in new window', class: 'btn', target: '_blank') unless external_link.nil?
     links.join('')
   end
+
+  def search_field
+    search_link_field = :blank
+    if creator_fields.include? field.to_sym
+      search_link_field = :creator_combined_label
+    elsif scientific_fields.include? field.to_sym 
+      search_link_field = :scientific_combined_label
+    elsif topic_fields.include? field.to_sym 
+      search_link_field = :topic_combined_label
+    elsif location_fields.include? field.to_sym 
+      search_link_field = :location_combined_label
+    else
+      search_link_field = options.fetch(:search_field, field)
+    end
+    search_link_field
+  end
+
+  def creator_fields
+    %i[arranger_label artist_label author_label cartographer_label collector_label composer_label creator_label contributor_label dedicatee_label donor_label designer_label editor_label illustrator_label interviewee_label interviewer_label lyricist_label owner_label patron_label photographer_label print_maker_label recipient_label transcriber_label translator_label]
+  end
+
+  def topic_fields
+    %i[keyword_label subject_label]
+  end
+
+  def location_fields
+    %i[ranger_district_label water_basin_label location_label] 
+  end
+
+  def scientific_fields
+    %i[taxon_class_label family_label genus_label order_label species_label phylum_or_division_label]
+  end
 end
