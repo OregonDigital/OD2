1a31bbfae1dae040965bdd31b0a9ca0594a5a693
straleyb <straleyb@onid.oregonstate.edu>
2020-04-16 14:04:24 -0700

    rework to add URI in and update class names

---
+++ app/inputs/label_and_uri_controlled_vocabulary_input.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# OVERRIDE to fetch value so rdf_label actually works
+class LabelAndUriControlledVocabularyInput < ControlledVocabularyInput
+  private
+
+  def build_options_for_existing_row(attribute_name, index, value, options)
+    solr_document_label = object.solr_document.send("#{attribute_name}_label")[index] 
+    value_uri = value.rdf_subject
+    options[:value] = "#{solr_document_label} (#{value_uri})" || "Unable to fetch label for #{value.rdf_subject}"
+    options[:readonly] = true
+  end
+end
