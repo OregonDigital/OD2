ed37142f3e4a498ed4fd8373d4517c7f627d7b0a
straleyb <straleyb@onid.oregonstate.edu>
2020-04-16 12:07:00 -0700

    add override note and remove unused variables

--- app/inputs/fetched_controlled_vocabulary_input.rb
+++ app/inputs/fetched_controlled_vocabulary_input.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+# OVERRIDE to fetch value so rdf_label actually works
 class FetchedControlledVocabularyInput < ControlledVocabularyInput
   private
 
-  def build_options_for_existing_row(attribute_name, index, value, options)
+  def build_options_for_existing_row(_attribute_name, _index, value, options)
     value.fetch(RDF::Util::File::HttpAdapter.default_accept_header.sub(%r{, \*\/\*;q=0\.1\Z}, ''))
     options[:value] = value.rdf_label.first || "Unable to fetch label for #{value.rdf_subject}"
     options[:readonly] = true
