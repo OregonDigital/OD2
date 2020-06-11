e5868f87c2a26b5d9a395217fb0939a71e82b5c7
straleyb <straleyb@onid.oregonstate.edu>
2020-06-11 11:22:14 -0700

    update to remove alerts

--- app/assets/javascripts/oregon_digital/collections/update_save_checkboxes.js
+++ app/assets/javascripts/oregon_digital/collections/update_save_checkboxes.js
@@ -1,8 +1,6 @@
 function updateSaveCheckboxes (button) {
   $('input[type=checkbox]').each(function() {
-    alert(this.id)
     if (this.id.includes(button.dataset.id)) {
-      alert('checked')
       this.checked = true;
     } else {
       this.checked = false;
