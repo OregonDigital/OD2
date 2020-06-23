914fc2e3c04392530d890411e733ea5a23d22a2a
straleyb <straleyb@onid.oregonstate.edu>
2020-06-23 08:19:20 -0700

    update spec with new factory

--- spec/factories/collection_type.rb
+++ spec/factories/collection_type.rb
@@ -3,5 +3,11 @@
 FactoryBot.define do
   factory :collection_type, class: Hyrax::CollectionType do
     sequence(:title) { |n| "title-#{n}" }
+
+    
+  end
+
+  factory :user_collection_type, class: Hyrax::CollectionType do
+    initialize_with { Hyrax::CollectionType.find_or_create_default_collection_type }
   end
 end
