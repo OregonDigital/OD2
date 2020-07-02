7b0fbae0fd3e988e6aae777a8f27241b3367b355
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2020-07-01 17:56:50 -0700

    remove sidekiq workaround

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,6 @@
 require 'sidekiq/web'
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
 Sidekiq::Web.set :sessions,       Rails.application.config.session_options
-Sidekiq::Web.class_eval do
-  use Rack::Protection, origin_whitelist: ENV.fetch('SIDEKIQ_ADMIN_SAFE_URLS', '').split(',')
-end
 
 config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
