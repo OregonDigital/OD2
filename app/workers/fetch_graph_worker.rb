5fec2a603e477895a600434753d42b9520100a3c
Corey Gillen <corey.gillen@oregonstate.edu>
2020-08-03 07:31:11 -0700

    Make sure keyword_tesim is an array

--- app/workers/fetch_graph_worker.rb
+++ app/workers/fetch_graph_worker.rb
@@ -42,7 +42,7 @@ class FetchGraphWorker
         solr_doc["#{controlled_prop}_label_tesim"] = val
         solr_doc['creator_combined_label_tesim'] = val if creator_combined_facet?(controlled_prop)
         solr_doc['location_combined_label_tesim'] = val if location_combined_facet?(controlled_prop)
-        solr_doc['topic_combined_label_tesim'] = val + solr_doc['keyword_tesim'] if topic_combined_facet?(controlled_prop)
+        solr_doc['topic_combined_label_tesim'] = val + solr_doc['keyword_tesim'].to_a if topic_combined_facet?(controlled_prop)
         solr_doc['scientific_combined_label_tesim'] = val if scientific_combined_facet?(controlled_prop)
         ActiveFedora::SolrService.add(solr_doc)
         ActiveFedora::SolrService.commit
