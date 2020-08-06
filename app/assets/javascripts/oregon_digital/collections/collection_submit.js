d4eae8e59428430e1cffe5c072bcea658ccfabe4
Corey Gillen <corey.gillen@oregonstate.edu>
2020-08-06 14:40:16 -0700

    Disable collection form default event and continue using Hyrax rewrite of form action

--- app/assets/javascripts/oregon_digital/collections/collection_submit.js
+++ app/assets/javascripts/oregon_digital/collections/collection_submit.js
@@ -1,19 +1,14 @@
-function post_collection_update() {
-  let collection_id = $('#member_of_collection_ids')[0].value
-  let batch_ids = []
-  $('input[type=checkbox]').each(function() {
-    if (this.checked) {
-      batch_ids.push(this.id.split("_")[2])
-    }
-  });
+function post_collection_update(event) {
+  event.preventDefault();
 
   $.ajax({
     method: "POST",
-    async: false,
+    async: true,
     timeout: 100,
     url: "/dashboard/collections/" + collection_id,
-    data: {"collection": {"member_of_collection_ids": collection_id, "members": "add"}, "commit": "Save", "batch_document_ids": batch_ids, "id": collection_id}
-  })
-    .done(function( msg ) {
-    });
+    data: {"collection": {"member_of_collection_ids": collection_id, "members": "add"}, "commit": "Save", "batch_document_ids": batch_ids, "id": collection_id},
+    complete: function(jqXHR, status) {
+      alert('Your work(s) are being added to the collection. Please give us time to process this request');
+    }
+  });
 };
--- config/initializers/hacks/hyrax_collections_members_controller.rb
+++ config/initializers/hacks/hyrax_collections_members_controller.rb
@@ -2,7 +2,19 @@
 
 Hyrax::Dashboard::CollectionMembersController.class_eval do
   # Override after update method to redirect users back to where they updated the collection from
-  def after_update;end
+  def after_update
+    respond_to do |format|
+      format.html do
+        case URI(request.referer).path.split('/')[1]
+        when 'dashboard'
+          redirect_to success_return_path, notice: t('hyrax.dashboard.my.action.collection_update_success')
+        else
+          redirect_back fallback_location: '/'
+        end
+      end
+      format.json { render json: @collection, status: :updated, location: dashboard_collection_path(@collection) }
+    end
+  end
 
   def update_members
     err_msg = validate
