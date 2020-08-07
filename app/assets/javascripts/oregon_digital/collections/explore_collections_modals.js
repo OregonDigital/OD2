46d744d30c02c7eb7ea54a7f7a61345b210deed3
Corey Gillen <corey.gillen@oregonstate.edu>
2020-08-07 14:55:16 -0700

    Use ajax submission for user collection creation modal

--- app/assets/javascripts/oregon_digital/collections/explore_collections_modals.js
+++ app/assets/javascripts/oregon_digital/collections/explore_collections_modals.js
@@ -92,3 +92,38 @@ function submit_delete_my_collection_modal(event) {
     }
   });
 }
+
+// Create modal submission callback
+function submit_create_my_collection_modal(event) {
+  // Prevent regular submission
+  event.preventDefault();
+  console.log(event);
+
+  let type = event.target.elements['type'].value;
+  let collection_type_gid = event.target.elements['collection_type_gid'].value;
+  let collection_title = [];
+  let collection_description = [];
+  let batch_ids = [];
+
+  $(event.target.elements['collection[title][]']).each(function() {
+    collection_title.push(this.value)
+  });
+  $(event.target.elements['collection[description][]']).each(function() {
+    collection_description.push(this.value)
+  });
+  $(event.target.elements['batch_document_ids[]']).each(function() {
+    batch_ids.push(this.value)
+  });
+
+  // Perform async call and alert user this may take some time
+  $.ajax({
+    method: "POST",
+    async: true,
+    timeout: 100,
+    url: event.target.action,
+    data: {"collection": {"title": collection_title, "description": collection_description}, "batch_document_ids": batch_ids, "collection_type_gid": collection_type_gid, "type": type},
+    complete: function(jqXHR, status) {
+      alert('Your collection is being created. Please give us time to process this request');
+    }
+  });
+}
