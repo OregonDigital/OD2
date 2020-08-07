3498ed343c6d244a8cc1a0056e0f742f67560d53
Corey Gillen <corey.gillen@oregonstate.edu>
2020-08-07 12:38:28 -0700

    Start using Hyrax pub-sub for fetch graph workers

---
+++ app/services/oregon_digital/listeners/metadata_fetch_listener.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module OregonDigital
+  module Listeners
+    # Listens for metadata update events and enqueues metadata fetch jobs
+    class MetadataFetchListener
+
+      ##
+      # @param event [Dry::Event]
+      def on_object_metadata_updated(event)
+        FetchGraphWorker.perform_async(event[:object].id, event[:object].depositor)
+      end
+    end
+  end
+end
