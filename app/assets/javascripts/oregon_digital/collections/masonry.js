1b3f216ef4b43fd6dbd1e066107c5db9d2353e15
Corey Gillen <corey.gillen@oregonstate.edu>
2020-08-20 15:09:02 -0700

    Masonry actions styles

--- app/assets/javascripts/oregon_digital/collections/masonry.js
+++ app/assets/javascripts/oregon_digital/collections/masonry.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   $('a[data-toggle="pill"]').on('shown.bs.tab', function(e) {
     // Reload masonry view for tabbed content and remove transition animation
     $("[data-behavior='masonry-gallery']").masonry({
-      transitionDuration: 0
+      transitionDuration: 0,
+      gutter: 35
     });
   });
 });
