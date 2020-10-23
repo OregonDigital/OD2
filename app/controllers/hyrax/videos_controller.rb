d0a126c3ac1f38d13005843d7479551ed6b69ba1
Corey Gillen <corey.gillen@oregonstate.edu>
2020-10-23 13:14:23 -0700

    Fix actor environment missing oembed_urls param

--- app/controllers/hyrax/videos_controller.rb
+++ app/controllers/hyrax/videos_controller.rb
@@ -9,6 +9,7 @@ module Hyrax
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
     include OregonDigital::DownloadControllerBehavior
+    prepend OregonDigital::WorksControllerBehavior
     self.curation_concern_type = ::Video
 
     # Override the way Hyrax's works present iiif manifests
