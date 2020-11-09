661a1197cc502fefaedd2392776c890ff9287d3b
straleyb <straleyb@onid.oregonstate.edu>
2020-11-09 14:56:42 -0800

    adds object to convert work to presenter and refine sibling view

---
+++ lib/oregon_digital/work_to_presenter_converter.rb
@@ -0,0 +1,22 @@
+module OregonDigital
+  class WorkToPresenterConverter
+    def self.convert_to_presenter(work, current_ability)
+      work_type = work.class
+      solr_doc = SolrDocument.find(work.id)
+      @work_presenter = ""
+      case work_type.to_s
+      when "Generic"
+        @work_presenter = Hyrax::GenericPresenter.new(solr_doc, current_ability)
+      when "Image"
+        @work_presenter = Hyrax::ImagePresenter.new(solr_doc, current_ability)
+      when "Video"
+        @work_presenter = Hyrax::VideoPresenter.new(solr_doc, current_ability)
+      when "Document"
+        @work_presenter = Hyrax::DocumentPresenter.new(solr_doc, current_ability)
+      when "Audio"
+        @work_presenter = Hyrax::AudioPresenter.new(solr_doc, current_ability)
+      end
+      return @work_presenter
+    end
+  end
+end
