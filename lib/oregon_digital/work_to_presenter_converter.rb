b2b543522d487fac8743e924ae337bb3fb424048
straleyb <straleyb@onid.oregonstate.edu>
2020-11-10 11:13:59 -0800

    update lint

--- lib/oregon_digital/work_to_presenter_converter.rb
+++ lib/oregon_digital/work_to_presenter_converter.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal:true
+
 module OregonDigital
+  # Converts a work to a work type presenter
   class WorkToPresenterConverter
     def self.convert_to_presenter(work, current_ability)
       work_type = work.class
       solr_doc = SolrDocument.find(work.id)
-      @work_presenter = ""
+      @work_presenter = ''
       case work_type.to_s
-      when "Generic"
+      when 'Generic'
         @work_presenter = Hyrax::GenericPresenter.new(solr_doc, current_ability)
-      when "Image"
+      when 'Image'
         @work_presenter = Hyrax::ImagePresenter.new(solr_doc, current_ability)
-      when "Video"
+      when 'Video'
         @work_presenter = Hyrax::VideoPresenter.new(solr_doc, current_ability)
-      when "Document"
+      when 'Document'
         @work_presenter = Hyrax::DocumentPresenter.new(solr_doc, current_ability)
-      when "Audio"
+      when 'Audio'
         @work_presenter = Hyrax::AudioPresenter.new(solr_doc, current_ability)
       end
       return @work_presenter
