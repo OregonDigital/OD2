f359d82642b34051d4007b09098237eebb70c8f4
straleyb <straleyb@onid.oregonstate.edu>
2021-01-06 13:53:12 -0800

    remove space

--- app/search_builders/oregon_digital/uo_collections_search_builder.rb
+++ app/search_builders/oregon_digital/uo_collections_search_builder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal:true
 
-# Added to allow for the My controller to show only UO items 
+# Added to allow for the My controller to show only UO items
 class OregonDigital::UoCollectionsSearchBuilder < OregonDigital::NonUserCollectionsSearchBuilder
   include OregonDigital::OnlyUoCollectionsBehavior
   self.default_processor_chain += [:show_only_uo_collections]
