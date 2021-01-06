62fc75d760cdc6550a482d22afeec1ca7ecd3681
straleyb <straleyb@onid.oregonstate.edu>
2021-01-06 13:17:35 -0800

    remove spaces

--- app/search_builders/oregon_digital/osu_collections_search_builder.rb
+++ app/search_builders/oregon_digital/osu_collections_search_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal:true
 
 # Added to allow for the My controller to show only things I have edit access to
-class OregonDigital::OsuCollectionsSearchBuilder < OregonDigital::NonUserCollectionsSearchBuilder 
+class OregonDigital::OsuCollectionsSearchBuilder < OregonDigital::NonUserCollectionsSearchBuilder
   include OregonDigital::OnlyOsuCollectionsBehavior
 
   self.default_processor_chain += [:show_only_osu_collections]
--- app/search_builders/oregon_digital/uo_collections_search_builder.rb
+++ app/search_builders/oregon_digital/uo_collections_search_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal:true
 
 # Added to allow for the My controller to show only things I have edit access to
-class OregonDigital::UoCollectionsSearchBuilder < OregonDigital::NonUserCollectionsSearchBuilder 
+class OregonDigital::UoCollectionsSearchBuilder < OregonDigital::NonUserCollectionsSearchBuilder
   include OregonDigital::OnlyUoCollectionsBehavior
   self.default_processor_chain += [:show_only_uo_collections]
 end
