aab0d5a537410a1444d1da893d7af0fdbc2dab95
Corey Gillen <corey.gillen@oregonstate.edu>
2021-01-07 11:20:00 -0800

    single quote lint

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -285,10 +285,10 @@ class CatalogController < ApplicationController
     # except in the relevancy case).
     # label is key, solr field is value
     config.add_sort_field "score desc, #{uploaded_field} desc", label: 'relevance'
-    config.add_sort_field "#{uploaded_field} desc", label: "date uploaded (desc)"
-    config.add_sort_field "#{uploaded_field} asc", label: "date uploaded (asc)"
-    config.add_sort_field "#{modified_field} desc", label: "date modified (desc)"
-    config.add_sort_field "#{modified_field} asc", label: "date modified (asc)"
+    config.add_sort_field "#{uploaded_field} desc", label: 'date uploaded (desc)'
+    config.add_sort_field "#{uploaded_field} asc", label: 'date uploaded (asc)'
+    config.add_sort_field "#{modified_field} desc", label: 'date modified (desc)'
+    config.add_sort_field "#{modified_field} asc", label: 'date modified (asc)'
 
     # If there are more than this many search results, no spelling ('did you
     # mean') suggestion is offered.
