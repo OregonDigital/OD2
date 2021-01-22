30fa6b69228ddeb8678faa17084e09b3c1be5a76
Corey Gillen <corey.gillen@oregonstate.edu>
2021-01-22 11:56:33 -0800

    Remove rows assignment from OnlyNonUserCollectionsBehavior

--- app/search_builders/oregon_digital/only_non_user_collections_behavior.rb
+++ app/search_builders/oregon_digital/only_non_user_collections_behavior.rb
@@ -12,7 +12,6 @@ module OregonDigital
         ]
         solr_parameters[:fq] ||= []
         solr_parameters[:fq] += ["!(#{clauses.join(' OR ')})"]
-        solr_parameters[:rows] = 1000
       end
     end
   end
