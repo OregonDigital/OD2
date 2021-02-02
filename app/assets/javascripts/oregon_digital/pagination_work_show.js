f5402af245ced82b923ab2488d292aea8883147a
straleyb <straleyb@onid.oregonstate.edu>
2021-02-02 12:30:41 -0800

    cleanup and formatting

--- app/assets/javascripts/oregon_digital/pagination_work_show.js
+++ app/assets/javascripts/oregon_digital/pagination_work_show.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
   $('#show-page-table').DataTable({
     pageLength: 2,
     lengthMenu: [[ 2 ],[ "2" ]]
