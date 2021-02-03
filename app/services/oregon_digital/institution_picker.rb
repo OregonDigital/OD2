21e68cd1023ec24b65621791819866d86a6d9ff6
straleyb <straleyb@onid.oregonstate.edu>
2021-02-03 13:58:09 -0800

    make methods work as class methods

--- app/services/oregon_digital/institution_picker.rb
+++ app/services/oregon_digital/institution_picker.rb
@@ -1,28 +1,28 @@
 module OregonDigital
   # A class that returns acronym or full name of two main institutions. Returns empty string for anything else
-  class CollectionInstitutionPicker
+  class InstitutionPicker
     def self.institution_acronym(collection)
       return "" unless contains_university?(collection)
-      insitution = selected_institution(collection) 
+      institution = selected_institution(collection) 
       translate_institution(institution, :acronym)
     end
 
     def self.institution_full_name(collection)
       return "" unless contains_university?(collection)
-      insitution = selected_institution(collection) 
+      institution = selected_institution(collection) 
       translate_institution(institution, :full)
     end
 
     private
 
-    def selected_institution(collection)
+    def self.selected_institution(collection)
       institution = ""
       institution = collection.institution.select { |val| val == osu }.first
       institution = collection.institution.select { |val| val == uo }.first if institution.blank?
       institution
     end
 
-    def translate_institution(institution, translation_type)
+    def self.translate_institution(institution, translation_type)
       case translation_type
       when :acronym
         return institution == osu ? "OSU" : "UO"
@@ -33,15 +33,15 @@ module OregonDigital
       end
     end
 
-    def contains_university?(collection)
+    def self.contains_university?(collection)
       collection.institution.include?(uo) || collection.institution.include?(osu) 
     end
 
-    def osu
+    def self.osu
       "http://id.loc.gov/authorities/names/n80017721"
     end
 
-    def uo
+    def self.uo
       "http://id.loc.gov/authorities/names/n80126183"
     end
   end
