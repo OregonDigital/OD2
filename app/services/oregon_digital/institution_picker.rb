a7a1e3f59fd66f4546ba9fce93caa8022c7933e4
straleyb <straleyb@onid.oregonstate.edu>
2021-02-03 14:08:31 -0800

    remove double quotes and private

--- app/services/oregon_digital/institution_picker.rb
+++ app/services/oregon_digital/institution_picker.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 module OregonDigital
   # A class that returns acronym or full name of two main institutions. Returns empty string for anything else
   class InstitutionPicker
     def self.institution_acronym(collection)
-      return "" unless contains_university?(collection)
+      return '' unless contains_university?(collection)
 
-      institution = selected_institution(collection) 
+      institution = selected_institution(collection)
       translate_institution(institution, :acronym)
     end
 
     def self.institution_full_name(collection)
-      return "" unless contains_university?(collection)
+      return '' unless contains_university?(collection)
 
-      institution = selected_institution(collection) 
+      institution = selected_institution(collection)
       translate_institution(institution, :full)
     end
 
-    private
-
     def self.selected_institution(collection)
-      institution = ""
       institution = collection.institution.select { |val| val == osu }.first
       institution = collection.institution.select { |val| val == uo }.first if institution.blank?
       institution
@@ -27,24 +26,24 @@ module OregonDigital
     def self.translate_institution(institution, translation_type)
       case translation_type
       when :acronym
-        institution == osu ? "OSU" : "UO"
+        institution == osu ? 'OSU' : 'UO'
       when :full
-        institution == osu ? "Oregon State University" : "University of Oregon"
+        institution == osu ? 'Oregon State University' : 'University of Oregon'
       else
-        ""
+        ''
       end
     end
 
     def self.contains_university?(collection)
-      collection.institution.include?(uo) || collection.institution.include?(osu) 
+      collection.institution.include?(uo) || collection.institution.include?(osu)
     end
 
     def self.osu
-      "http://id.loc.gov/authorities/names/n80017721"
+      'http://id.loc.gov/authorities/names/n80017721'
     end
 
     def self.uo
-      "http://id.loc.gov/authorities/names/n80126183"
+      'http://id.loc.gov/authorities/names/n80126183'
     end
   end
 end
