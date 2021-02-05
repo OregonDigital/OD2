a381a83c929362c0b6eae4d3b1af0ce37e6a7756
Corey Gillen <corey.gillen@oregonstate.edu>
2021-02-05 11:31:23 -0800

    Port SA specs for date-range/decade indexing to OD2

--- spec/indexers/generic_indexer_spec.rb
+++ spec/indexers/generic_indexer_spec.rb
@@ -9,4 +9,109 @@ RSpec.describe GenericIndexer do
   it { expect(solr_doc['license_tesim']).to eq ['MyLicense'] }
   it { expect(solr_doc['language_tesim']).to eq ['MyLanguage'] }
   it { expect(solr_doc['rights_statement_tesim']).to eq ['MyRights'] }
+
+  describe 'date facet yearly' do
+    let(:field) { solr_doc['date_combined_year_label_ssim'] }
+
+    context 'when given date is empty' do
+      before { work.date_created = [] }
+
+      it 'should be empty array' do
+        expect(field).to eq []
+      end
+    end
+    context 'when given an invalid date' do
+      before { work.date_created = ['typo2011-01-01'] }
+      before { work.date_created = ['typo2011-01-01'] }
+
+      it 'should be empty array' do
+        expect(field).to eq []
+      end
+    end
+    context 'when given a date' do
+      before { work.date_created = ['2022-01-01'] }
+
+      it 'should be that year' do
+        expect(field).to eq [2022]
+      end
+    end
+    context 'when given just a year' do
+      before { work.date_created = ['2011'] }
+
+      it 'should pull the year' do
+        expect(field).to eq [2011]
+      end
+    end
+    context 'when given a date range' do
+      context 'when is a range of dates YYYY-mm-dd/YYY-mm-dd' do
+        before { work.date_created = ['1925-12-01/1927-01-01'] }
+
+        it 'should have years in that range' do
+          expect(field).to eq [1925, 1926, 1927]
+        end
+      end
+      context 'when is a range of years only YYYY/YYYY' do
+        before { work.date_created = ['2014/2017'] }
+
+        it 'should have years in that range' do
+          expect(field).to eq [2014, 2015, 2016, 2017]
+        end
+      end
+      context 'when is a range of year and month only YYYY-mm/YYY-mm' do
+        before { work.date_created = ['2017-12/2018-01'] }
+
+        it 'should have years in that range' do
+          expect(field).to eq [2017, 2018]
+        end
+      end
+    end
+  end
+  describe 'decade facets' do
+    let(:field) { solr_doc['date_combined_decade_label_ssim'] }
+
+    context 'when given date is empty' do
+      before { work.date_created = [] }
+
+      it 'should be empty array' do
+        expect(field).to eq []
+      end
+    end
+    context 'when given a date' do
+      before { work.date_created = ['2022-01-01'] }
+
+      it 'should be that decade' do
+        expect(field).to eq ['2020-2029']
+      end
+    end
+    context 'when given just a year' do
+      before { work.date_created = ['2011'] }
+
+      it 'should pull the date' do
+        expect(field).to eq ['2010-2019']
+      end
+    end
+    context 'when given a date range' do
+      context 'when is in a single decade' do
+        before { work.date_created = ['1910-1915'] }
+
+        it 'should have only one entry' do
+          expect(field).to eq ['1910-1919']
+        end
+      end
+      context 'which spans decades' do
+        before { work.date_created = ['1910-1920'] }
+
+        it 'should have two entries' do
+          expect(field).to eq %w[1910-1919 1920-1929]
+        end
+        context 'which is more than 30 years' do
+          before { work.date_created = ['1900-1940'] }
+
+          it 'should be empty array' do
+            expect(field).to eq []
+          end
+        end
+      end
+    end
+  end
 end
