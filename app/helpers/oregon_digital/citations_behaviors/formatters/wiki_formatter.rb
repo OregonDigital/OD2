11c5437dab517b23ac0a50974ca293385ea545af
straleyb <straleyb@onid.oregonstate.edu>
2021-04-09 15:08:43 -0700

    fix some linty stuff

--- app/helpers/oregon_digital/citations_behaviors/formatters/wiki_formatter.rb
+++ app/helpers/oregon_digital/citations_behaviors/formatters/wiki_formatter.rb
@@ -3,16 +3,14 @@
 module OregonDigital
   module CitationsBehaviors
     module Formatters
-      # MLA Citation Formatter
+      # Wiki Citation Formatter
       class WikiFormatter < Hyrax::CitationsBehaviors::Formatters::MlaFormatter
         include OregonDigital::CitationsBehaviors::NameBehavior
         include OregonDigital::CitationsBehaviors::PublicationBehavior
         include Hyrax::CitationsBehaviors::TitleBehavior
 
         def format(work)
-          text = super
-          text << "<ref name=Oregon Digital>{{cite web | url= #{view_context.controller.request.original_url.split('?').first if view_context.respond_to?(:controller)}| title= #{work.title.first} |author=#{work.author.first} |accessdate=#{Date.today} |publisher=#{work.publisher.first.to_s}}}</ref>"
-          text.html_safe
+          "<ref name=Oregon Digital>{{cite web | url= #{view_context.controller.request.original_url.split('?').first if view_context.respond_to?(:controller)}| title= #{work.title.first} |author=#{work.author.first} |accessdate=#{Date.today} |publisher=#{work.publisher.first}}}</ref>".html_safe
         end
       end
     end
