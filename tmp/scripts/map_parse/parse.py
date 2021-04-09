30d6304fb1671d4766d7a8a390a154b57fc8adce
Corey Gillen <corey.gillen@oregonstate.edu>
2021-04-09 13:55:52 -0700

    Comments and remove unused import

--- tmp/scripts/map_parse/parse.py
+++ tmp/scripts/map_parse/parse.py
@@ -2,35 +2,49 @@
 # Leave map.csv in this directory and this script will generate a file with `property` definitions for each worktype
 
 import csv
-import pprint
 
 generic, document, image, video, audio = ([] for i in range(5))
 with open('map.csv') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         prop = row[1]
+
         predicate = row[4]
+
         facetable = ('', ', :facetable')[row[14] == 'yes']
+
         multiple = str(row[10] == '{0,n}' or row[10] == '{1,n}').lower()
+
         workType = row[9]
+
         controlledClass = row[23]
         skippedClasses = ['Hyrax Collection', 'Language', 'License', 'Types', 'RightsStatement']
 
 
+        # skip this property if prop or predicate isn't in MAP
         if len(prop) == 0 or len(predicate) <= 1:
             continue
+
+        # Craft our property statements in this format:
+        # property :prop_name, predicate: RDF::Vocab::Vocabulary.predicate, multiple: true/false, class_name: OregonDigtal::ControlledVocabularies::Vocab do |index|
+        #   index.as :stored_searchable, :facetable
+        # end
         md = "property %s, predicate: %s, multiple: %s"%(prop, predicate, multiple)
 
+        # Add optional class_name if this is a controlled vocab
         if controlledClass in skippedClasses:
           pass
         elif controlledClass == 'Location':
             md += ", class_name: Hyrax::ControlledVocabularies::%s"%(controlledClass)
         elif not controlledClass == 'N/A':
             md += ", class_name: OregonDigital::ControlledVocabularies::%s"%(controlledClass)
+
+        # Add indexing information
         md += " do |index|"
         md += "\n  index.as :stored_searchable%s"%(facetable)
         md += "\nend"
 
+        # Gather all these definitions up to write them out later
         if workType == 'Generic':
             generic.append(md)
         elif workType == 'Document':
@@ -42,6 +56,7 @@ with open('map.csv') as csvfile:
         elif workType == 'Audio':
             audio.append(md)
 
+# Write out our property definitions to separate files
 with open('generic', 'w') as f:
     for g in generic:
         f.write(g + "\n\n")
