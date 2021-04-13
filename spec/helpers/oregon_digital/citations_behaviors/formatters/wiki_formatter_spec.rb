3f7a1e764f82f774d5a04a51f7abeb8b1fd66a11
straleyb <straleyb@onid.oregonstate.edu>
2021-04-13 11:27:21 -0700

    empty lint trap

--- spec/helpers/oregon_digital/citations_behaviors/formatters/wiki_formatter_spec.rb
+++ spec/helpers/oregon_digital/citations_behaviors/formatters/wiki_formatter_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe OregonDigital::CitationsBehaviors::Formatters::WikiFormatter do
   subject(:formatter) { described_class.new(:no_context) }
 
   let(:presenter) { Hyrax::GenericPresenter.new(SolrDocument.new(work.to_solr), :no_ability) }
-  let(:work) { build(:work, title: ['My Title'], author: ["My Author"], publisher: ["My Publisher"]) }
-  let(:view_context) { double("View Context") }
-  let(:controller) { double("Controller") }
-  let(:original_url) { "http://test.oregondigital.org/blah/blah/12345678?q=blah" }
+  let(:work) { build(:work, title: ['My Title'], author: ['My Author'], publisher: ['My Publisher']) }
+  let(:view_context) { double('View Context') }
+  let(:controller) { double('Controller') }
+  let(:original_url) { 'http://test.oregondigital.org/blah/blah/12345678?q=blah' }
 
   before do
     allow(presenter).to receive(:creator_label).and_return(['last name, first name'])
