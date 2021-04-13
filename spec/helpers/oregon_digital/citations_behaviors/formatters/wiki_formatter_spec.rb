665dc6728f422830e57ca228fc05028c5ad9761c
straleyb <straleyb@onid.oregonstate.edu>
2021-04-13 11:20:04 -0700

    add spec for formatter

---
+++ spec/helpers/oregon_digital/citations_behaviors/formatters/wiki_formatter_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe OregonDigital::CitationsBehaviors::Formatters::WikiFormatter do
+  subject(:formatter) { described_class.new(:no_context) }
+
+  let(:presenter) { Hyrax::GenericPresenter.new(SolrDocument.new(work.to_solr), :no_ability) }
+  let(:work) { build(:work, title: ['My Title'], author: ["My Author"], publisher: ["My Publisher"]) }
+  let(:view_context) { double("View Context") }
+  let(:controller) { double("Controller") }
+  let(:original_url) { "http://test.oregondigital.org/blah/blah/12345678?q=blah" }
+
+  before do
+    allow(presenter).to receive(:creator_label).and_return(['last name, first name'])
+    allow(formatter).to receive(:view_context).and_return(view_context)
+    allow(view_context).to receive(:controller).and_return(controller)
+    allow(controller).to receive(:original_url).and_return(original_url)
+  end
+
+  it 'displays the citation' do
+    expect(formatter.format(presenter)).to eq "<ref name=Oregon Digital>{{cite web | url= http://test.oregondigital.org/blah/blah/12345678?q=blah | title= My Title |author= My Author |accessdate= #{Date.today} |publisher= My Publisher}}</ref>"
+  end
+end
