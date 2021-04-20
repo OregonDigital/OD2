c0c45e200e5c53600dd5fdce9dbbd53015a3b05b
Corey Gillen <corey.gillen@oregonstate.edu>
2021-04-20 14:37:25 -0700

    lint

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,6 +2,7 @@
 
 # Controller for user email confirmation
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  # rubocop:disable Metrics/AbcSize
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     yield resource if block_given?
@@ -10,9 +11,10 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
       set_flash_message!(:notice, :confirmed)
       # OVERRIDE FROM DEVISE to sign user in after confirming email
       sign_in(resource)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
     end
   end
+  # rubocop:enable Metrics/AbcSize
 end
