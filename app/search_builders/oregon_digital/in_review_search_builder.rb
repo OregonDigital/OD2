086e493bec7384eeedf6fb3d9b50fba9fbad3a2f
Corey Gillen <corey.gillen@oregonstate.edu>
2021-04-20 09:26:14 -0700

    Get IDs for the review queue the Hyrax way

--- app/search_builders/oregon_digital/in_review_search_builder.rb
+++ app/search_builders/oregon_digital/in_review_search_builder.rb
@@ -4,32 +4,14 @@ module OregonDigital
   # Finds works that are in review and the user can action on
   # Based on Hyrax::Workflow::StatusListService
   class InReviewSearchBuilder < ::SearchBuilder
-    self.default_processor_chain += %i[actionable not_deposited]
+    self.default_processor_chain += %i[in_review_ids]
     self.default_processor_chain -= %i[only_active_works]
 
-    def actionable(solr_params)
+    # Get all the ids for the works in review from Hyrax and pipe into this search builder
+    def in_review_ids(solr_params)
+      solr_documents = Hyrax::Workflow::StatusListService.new(@scope, '-workflow_state_name_ssim:deposited').each
       solr_params[:fq] ||= []
-      solr_params[:fq] << "{!terms f=actionable_workflow_roles_ssim}#{actionable_roles.join(',')}"
-    end
-
-    def not_deposited(solr_params)
-      solr_params[:fq] ||= []
-      solr_params[:fq] << '-workflow_state_name_ssim:deposited'
-    end
-
-    # @return [Array<String>] the list of workflow-role combinations this user has
-    def actionable_roles
-      Sipity::Workflow.all.flat_map do |wf|
-        workflow_roles_for_user_and_workflow(wf).map do |wf_role|
-          "#{wf.permission_template.source_id}-#{wf.name}-#{wf_role.role.name}"
-        end
-      end
-    end
-
-    # @param workflow [Sipity::Workflow]
-    # @return [ActiveRecord::Relation<Sipity::WorkflowRole>]
-    def workflow_roles_for_user_and_workflow(workflow)
-      Hyrax::Workflow::PermissionQuery.scope_processing_workflow_roles_for_user_and_workflow(user: @scope.current_user, workflow: workflow)
+      solr_params[:fq] << "id:(#{solr_documents.map(&:id).join(' OR ')})"
     end
   end
 end
