23b14234a2823ddf235765be91c4f9ffbcd9ce98
straleyb <straleyb@onid.oregonstate.edu>
2021-04-21 14:23:41 -0700

    update style and rendering of headers

--- app/helpers/oregon_digital/citations_behaviors/formatters/chicago_formatter.rb
+++ app/helpers/oregon_digital/citations_behaviors/formatters/chicago_formatter.rb
@@ -9,16 +9,12 @@ module OregonDigital
         include OregonDigital::CitationsBehaviors::PublicationBehavior
         include Hyrax::CitationsBehaviors::TitleBehavior
 
-        # rubocop:disable Metrics/AbcSize
-        # rubocop:disable Metrics/MethodLength
         def format(work)
           text = super(work)
           # setup formatted author list
           text += " #{view_context.controller.request.original_url.split('?').first if view_context.respond_to?(:controller)}"
           text.html_safe
         end
-        # rubocop:enable Metrics/AbcSize
-        # rubocop:enable Metrics/MethodLength
       end
     end
   end
