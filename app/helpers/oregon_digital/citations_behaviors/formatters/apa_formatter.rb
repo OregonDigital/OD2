030e57848a8868a786dfc01aac77e3820fbf980c
straleyb <straleyb@onid.oregonstate.edu>
2021-04-21 12:40:31 -0700

    lint updates

--- app/helpers/oregon_digital/citations_behaviors/formatters/apa_formatter.rb
+++ app/helpers/oregon_digital/citations_behaviors/formatters/apa_formatter.rb
@@ -9,6 +9,8 @@ module OregonDigital
         include OregonDigital::CitationsBehaviors::PublicationBehavior
         include Hyrax::CitationsBehaviors::TitleBehavior
 
+        # rubocop:disable Metrics/AbcSize
+        # rubocop:disable Metrics/MethodLength
         def format(work)
           text = ''
           # set authors
@@ -26,7 +28,8 @@ module OregonDigital
           text += " #{view_context.controller.request.original_url.split('?').first if view_context.respond_to?(:controller)}"
           text.html_safe
         end
-
+        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/MethodLength
         private
 
         def chicago_author_list(work)
