342e3aeeb469e7eba946df6d62303bdf24d2b950
straleyb <straleyb@onid.oregonstate.edu>
2021-04-21 12:53:52 -0700

    fix author_label mistake

--- app/helpers/oregon_digital/citations_behaviors/name_behavior.rb
+++ app/helpers/oregon_digital/citations_behaviors/name_behavior.rb
@@ -10,7 +10,7 @@ module OregonDigital
       # return all unique authors of a work or nil if none
       def all_authors(work, &block)
         # OVERRIDE FROM HYRAX to grab creator_label as author
-        authors = work.author_label.empty? ? [] : work.author.uniq.compact
+        authors = work.author_label.empty? ? [] : work.author_label.uniq.compact
         # END OVERRIDE
         block_given? ? authors.map(&block) : authors
       end
