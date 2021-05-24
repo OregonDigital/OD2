4c511e2ab8cc74dc6331e582e997c6d16d517fef
straleyb <straleyb@onid.oregonstate.edu>
2021-05-24 13:19:50 -0700

    lint

--- app/controllers/oregon_digital/ntriples_controller.rb
+++ app/controllers/oregon_digital/ntriples_controller.rb
@@ -4,7 +4,7 @@ module OregonDigital
   # Displays ntriples for a given work
   class NtriplesController < ApplicationController
     def show
-      @ntriples = ActiveFedora::Base.find(params[:id]).resource.dump(:ntriples) 
+      @ntriples = ActiveFedora::Base.find(params[:id]).resource.dump(:ntriples)
     end
   end
 end
