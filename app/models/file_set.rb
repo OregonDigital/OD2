1ad0c776422fa1eb3fb6630adddfc6c49ba18ad1
Corey Gillen <corey.gillen@oregonstate.edu>
2021-05-25 11:51:24 -0700

    Get filesets to index characterization metadata into solr
    And get file set show page to show all characterization metadata

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -16,17 +16,18 @@ class FileSet < ActiveFedora::Base
     !oembed_url.nil? && !oembed_url.empty?
   end
 
-  def characterization_terms
+  def self.characterization_terms
     %i[
-      format_label file_size well_formed valid date_created hasCreatingApplicationVersion
+      format_label file_size well_formed valid date_created fits_version
       exif_version original_checksum byte_order compression height width color_space
       profile_name profile_version orientation color_map image_producer capture_device
       scanning_software gps_timestamp latitude longitude file_title creator page_count
       language word_count character_count line_count character_set markup_basis markup_language
       paragraph_count table_count graphics_count bit_depth channels data_format frame_rate
-      bit_rate duration sample_rate offset aspect_ratio oembed_url
+      bit_rate duration sample_rate offset aspect_ratio
     ]
   end
+  delegate(*characterization_terms, to: :characterization_proxy)
 
   def ocr_content
     @ocr_content ||= SolrDocument.find(id).to_h.dig('all_text_tsimv')
