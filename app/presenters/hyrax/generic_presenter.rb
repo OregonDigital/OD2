d40e1a4fcdd4c1ee450cb7b77311f87cf4d88949
Corey Gillen <corey.gillen@oregonstate.edu>
2021-05-25 15:36:23 -0700

    Display work history to content creators

--- app/presenters/hyrax/generic_presenter.rb
+++ app/presenters/hyrax/generic_presenter.rb
@@ -4,6 +4,7 @@ module Hyrax
   # Display config for generic object
   class GenericPresenter < Hyrax::WorkShowPresenter
     include OregonDigital::PresentsAttributes
+    include Hyrax::WithEvents
     delegate(*Generic.generic_properties.map(&:to_sym), to: :solr_document)
     delegate(*Generic.controlled_properties.map(&:to_sym), to: :solr_document)
     delegate(*Generic.controlled_property_labels.map(&:to_sym), to: :solr_document)
