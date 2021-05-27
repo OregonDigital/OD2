2a5542775a335e0e6d6d25bb7e82f52ebbfd19e8
Corey Gillen <corey.gillen@oregonstate.edu>
2021-05-27 09:45:04 -0700

    Add validation to generic model for date edtf format

--- app/models/generic.rb
+++ app/models/generic.rb
@@ -9,9 +9,23 @@ class Generic < ActiveFedora::Base
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
+  validate :dates_edtf_format
 
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::OregonDigital::GenericMetadata
   include ::OregonDigital::ControlledPropertiesBehavior
+
+  private
+    def dates_edtf_format
+      edtf_fields.each do |field|
+        send(field).each do |value|
+          errors.add(field, "'#{value}' is not in EDTF format") unless EDTF.parse(value).present?
+        end
+      end
+    end
+
+    def edtf_fields
+      %i[ date acquisition_date award_date collected_date issued view_date ]
+    end
 end
