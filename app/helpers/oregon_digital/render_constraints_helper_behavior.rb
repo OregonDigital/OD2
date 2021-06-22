07beaa5b04aa3d3f8aa6d68c0eb23a36d2e42f72
Corey Gillen <corey.gillen@oregonstate.edu>
2021-06-22 09:33:11 -0700

    Hook up blacklight to the explore collections search header

---
+++ app/helpers/oregon_digital/render_constraints_helper_behavior.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal:true
+
+# Behaviors to add to Blacklight's RenderConstraintsHelper
+module OregonDigital::RenderConstraintsHelperBehavior
+
+  ##
+  # Provide a url for removing a particular constraint. This can be overriden
+  # in the case that you want parameters other than the defaults to be removed
+  # (e.g. :search_field)
+  #
+  # @param [ActionController::Parameters] localized_params query parameters
+  # @return [String]
+  def remove_constraint_url(localized_params)
+    scope = localized_params.delete(:route_set) || self
+
+    unless localized_params.is_a? ActionController::Parameters
+      localized_params = ActionController::Parameters.new(localized_params)
+    end
+
+    # OVERRIDE FROM BLACKLIGHT: Don't remove `action` parameter
+    options = localized_params.merge(q: nil)
+    options.permit!
+    scope.url_for(options)
+  end
+end
