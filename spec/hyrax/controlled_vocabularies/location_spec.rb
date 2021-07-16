f999bded403e147fab1a72f4b5336aceae8632cb
Corey Gillen <corey.gillen@oregonstate.edu>
2021-07-16 08:09:40 -0700

    Fix stub on location spec

--- spec/hyrax/controlled_vocabularies/location_spec.rb
+++ spec/hyrax/controlled_vocabularies/location_spec.rb
@@ -1,12 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe Hyrax::ControlledVocabularies::Location do
-  let(:location) { described_class.new('https://id.loc.gov/authorities/names/n80126183') }
-  let(:parent_adm1) { described_class.new('https://id.loc.gov/authorities/names/n80126183') }
+  let(:location) { described_class.new('https://sws.geonames.org/3469034/') }
+  let(:parent_adm1) { described_class.new('https://sws.geonames.org/3469034/') }
 
   before do
-    stub_request(:get, 'https://id.loc.gov/authorities/names/n80126183')
-      .to_return(status: 200, body: '', headers: {})
+    stub_request(:get, 'https://sws.geonames.org/3469034/')
+      .to_return(status: 200, body: '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+      <rdf:RDF xmlns:cc="http://creativecommons.org/ns#" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:gn="http://www.geonames.org/ontology#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:wgs84_pos="http://www.w3.org/2003/01/geo/wgs84_pos#">
+          <gn:Feature rdf:about="https://sws.geonames.org/3469034/">
+              <rdfs:isDefinedBy rdf:resource="https://sws.geonames.org/3469034/about.rdf"/>
+              <gn:name>Brazil</gn:name>
+              <gn:alternateName xml:lang="en">Federative Republic of Brazil</gn:alternateName>
+              <gn:officialName xml:lang="en">Brazil</gn:officialName>
+              <gn:featureClass rdf:resource="https://www.geonames.org/ontology#A"/>
+              <gn:featureCode rdf:resource="https://www.geonames.org/ontology#A.PCLI"/>
+              <gn:countryCode>BR</gn:countryCode>
+              <gn:population>209469333</gn:population>
+              <wgs84_pos:lat>-10</wgs84_pos:lat>
+              <wgs84_pos:long>-55</wgs84_pos:long>
+              <gn:parentFeature rdf:resource="https://sws.geonames.org/11812364/"/>
+              <gn:parentFeature rdf:resource="https://sws.geonames.org/6255150/"/>
+              <gn:childrenFeatures rdf:resource="https://sws.geonames.org/3469034/contains.rdf"/>
+              <gn:neighbouringFeatures rdf:resource="https://sws.geonames.org/3469034/neighbours.rdf"/>
+              <gn:locationMap rdf:resource="https://www.geonames.org/3469034/federative-republic-of-brazil.html"/>
+              <gn:wikipediaArticle rdf:resource="https://en.wikipedia.org/wiki/Brazil"/>
+              <rdfs:seeAlso rdf:resource="https://dbpedia.org/resource/Brazil"/>
+              <gn:wikipediaArticle rdf:resource="https://ru.wikipedia.org/wiki/%D0%91%D1%80%D0%B0%D0%B7%D0%B8%D0%BB%D0%B8%D1%8F"/>
+          </gn:Feature>
+          <foaf:Document rdf:about="https://sws.geonames.org/3469034/about.rdf">
+              <foaf:primaryTopic rdf:resource="https://sws.geonames.org/3469034/"/>
+              <cc:license rdf:resource="https://creativecommons.org/licenses/by/4.0/"/>
+              <cc:attributionURL rdf:resource="https://www.geonames.org"/>
+              <cc:attributionName rdf:datatype="https://www.w3.org/2001/XMLSchema#string">GeoNames</cc:attributionName>
+              <dcterms:created rdf:datatype="https://www.w3.org/2001/XMLSchema#date">2006-01-15</dcterms:created>
+              <dcterms:modified rdf:datatype="https://www.w3.org/2001/XMLSchema#date">2020-03-29</dcterms:modified>
+          </foaf:Document>
+      </rdf:RDF>', headers: {})
   end
 
   describe '#solrize' do
@@ -92,7 +122,7 @@ RSpec.describe Hyrax::ControlledVocabularies::Location do
         allow(location).to receive(:parent_hierarchy).and_return([])
       end
 
-      it { expect(location.rdf_label).to eq ['https://id.loc.gov/authorities/names/n80126183'] }
+      it { expect(location.rdf_label).to eq ['https://sws.geonames.org/3469034/'] }
     end
 
     context 'when a parent administrative hierarchy exists' do
@@ -101,7 +131,7 @@ RSpec.describe Hyrax::ControlledVocabularies::Location do
         allow(location).to receive(:top_level_element?).and_return(true)
       end
 
-      it { expect(location.rdf_label).to eq ['https://id.loc.gov/authorities/names/n80126183'] }
+      it { expect(location.rdf_label).to eq ['https://sws.geonames.org/3469034/'] }
     end
 
     context 'when a parent administrative hierarchy exists and isnt a top level element' do
@@ -110,7 +140,7 @@ RSpec.describe Hyrax::ControlledVocabularies::Location do
         allow(location).to receive(:top_level_element?).and_return(false)
       end
 
-      it { expect(location.rdf_label).to eq ['https://id.loc.gov/authorities/names/n80126183'] }
+      it { expect(location.rdf_label).to eq ['https://sws.geonames.org/3469034/'] }
     end
   end
 end
