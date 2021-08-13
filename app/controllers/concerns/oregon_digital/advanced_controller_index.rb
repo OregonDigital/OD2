0bac194d686cee831e967722201e2dfddaf68848
lsat12357 <lsato@uoregon.edu>
2021-08-13 14:57:22 -0700

    add comment

--- app/controllers/concerns/oregon_digital/advanced_controller_index.rb
+++ app/controllers/concerns/oregon_digital/advanced_controller_index.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal:true
 
-module OregonDigital::AdvancedControllerIndex
-  # Prepend to BlacklightAdvancedSearch::AdvancedController to get breadcrumb
-  def index
-    @response = get_advanced_search_facets unless request.method == :post
-    add_breadcrumb t('hyrax.controls.advanced'), 'advanced'
+module OregonDigital
+  # Modules for OregonDigital
+  module AdvancedControllerIndex
+    # Prepend to BlacklightAdvancedSearch::AdvancedController to get breadcrumb
+    def index
+      @response = get_advanced_search_facets unless request.method == :post
+      add_breadcrumb t('hyrax.controls.advanced'), 'advanced'
+    end
   end
 end
