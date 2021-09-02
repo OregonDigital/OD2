1f901ffd4b1a4e3547fa6ce72dce5864791a7de8
Katelynn Thorn <katelynnthorn@10-248-54-207.wireless.oregonstate.edu>
2021-09-02 13:35:46 -0700

    Ticket #1746: Commit to fix rubocop errors (fixes #1746)

--- app/builders/hyrax/bootstrap_breadcrumbs_builder.rb
+++ app/builders/hyrax/bootstrap_breadcrumbs_builder.rb
@@ -1,6 +1,9 @@
-# OVERRIDE FROM HYRAX to allow breadcrumb of current page to be recognized
 # frozen_string_literal: true
+
+# OVERRIDE FROM HYRAX to allow breadcrumb of current page to be recognized
+#
 # The BootstrapBreadcrumbsBuilder is a Bootstrap compatible breadcrumb builder.
+#
 # It provides basic functionalities to render a breadcrumb navigation according to Bootstrap's conventions.
 #
 # BootstrapBreadcrumbsBuilder accepts a limited set of options:
@@ -9,28 +12,27 @@
 #     <%= render_breadcrumbs builder: Hyrax::BootstrapBreadcrumbsBuilder %>
 #
 class Hyrax::BootstrapBreadcrumbsBuilder < BreadcrumbsOnRails::Breadcrumbs::Builder
-    include ActionView::Helpers::OutputSafetyHelper
-    def render
-      return "" if @elements.blank?
-  
-      @context.tag.nav(breadcrumbs_options) do
-        @context.tag.ol do
-          safe_join(@elements.uniq.collect { |e| render_element(e) })
-        end
+  include ActionView::Helpers::OutputSafetyHelper
+  def render
+    return '' if @elements.blank?
+
+    @context.tag.nav(breadcrumbs_options) do
+      @context.tag.ol do
+        safe_join(@elements.uniq.collect { |e| render_element(e) })
       end
     end
-  
-    def render_element(element)
-      html_class = 'active' if @context.current_page?(compute_path(element)) || element.options["aria-current"] == "page"
-      element.options['aria-current'] = 'page' if html_class
+  end
 
-      @context.tag.li(class: html_class) do
-        @context.link_to(@context.truncate(compute_name(element), length: 30, separator: ' '), compute_path(element), element.options)
-      end
+  def render_element(element)
+    html_class = 'active' if @context.current_page?(compute_path(element)) || element.options['aria-current'] == 'page'
+    element.options['aria-current'] = 'page' if html_class
 
+    @context.tag.li(class: html_class) do
+      @context.link_to(@context.truncate(compute_name(element), length: 30, separator: ' '), compute_path(element), element.options)
     end
-  
-    def breadcrumbs_options
-      { class: 'breadcrumb', role: "navigation", "aria-label" => "Breadcrumb" }
-    end
+  end
+
+  def breadcrumbs_options
+    { class: 'breadcrumb', role: 'navigation', 'aria-label' => 'Breadcrumb' }
+  end
 end
