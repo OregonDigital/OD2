8e90d4ef1ca2a34bf2640270f6d3eb00a472f6f5
straleyb <straleyb@onid.oregonstate.edu>
2021-09-08 09:06:49 -0700

    fix last lint

--- app/renderers/hyrax/renderers/faceted_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/faceted_attribute_renderer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+
 module Hyrax
   module Renderers
+    # Renders faceted attributes
     class FacetedAttributeRenderer < AttributeRenderer
       private
 
@@ -10,7 +12,7 @@ module Hyrax
           link_to(ERB::Util.h(value), search_path(value))
         end
       end
- 
+
       def search_path(value)
         Rails.application.routes.url_helpers.search_catalog_path("f[#{search_field}][]": value, locale: I18n.locale)
       end
