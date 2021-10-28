13254903534d3cf81f50bc602b0c88425cd21f64
straleyb <straleyb@onid.oregonstate.edu>
2021-10-28 12:16:52 -0700

    switch over to ajax to submit

--- app/assets/javascripts/oregon_digital/bulk_review.js
+++ app/assets/javascripts/oregon_digital/bulk_review.js
@@ -1,9 +1,19 @@
-function updateRouteForWorkflows(button) {
+function bulkReview(event) {
+  event.preventDefault();
   clicked_ids = []
   $('input[type=checkbox]').each(function() {
     if (this.checked == true) {
       clicked_ids.push($(`#${this.id}`).val())
     }
+  });    
+  $.ajax({
+    method: "POST",
+    async: true,
+    timeout: 100,
+    url: `/bulk_review/${clicked_ids}?locale=en`,
+    complete: function(jqXHR, status) {
+      alert('Your work(s) are being added to the collection. Please give us time to process this request');
+    }
   });
-  $('#bulk_review_button').parent().attr('action', `/bulk_review/${clicked_ids}?locale=en`)
-}
+};
+      
