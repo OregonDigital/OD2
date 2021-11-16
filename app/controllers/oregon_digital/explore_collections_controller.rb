008e8330d723f2b65c1a341887a0f837359ed755
straleyb <straleyb@onid.oregonstate.edu>
2021-11-15 17:19:57 -0800

    ruby 1.9 syntax

--- app/controllers/oregon_digital/explore_collections_controller.rb
+++ app/controllers/oregon_digital/explore_collections_controller.rb
@@ -75,11 +75,11 @@ module OregonDigital
     end
 
     def osu_restricted?(id)
-      !osu_items(id).empty? && current_ability.cannot?(:show, {:any => osu_items(id)}, visibility: 'osu')
+      !osu_items(id).empty? && current_ability.cannot?(:show, { any: osu_items(id) }, visibility: 'osu')
     end
 
     def uo_restricted?(id)
-      !uo_items(id).empty? && current_ability.cannot?(:show, {:any => uo_items(id)}, visibility: 'uo')
+      !uo_items(id).empty? && current_ability.cannot?(:show, { any: uo_items(id) }, visibility: 'uo')
     end
 
     def institution_restricted?(id)
