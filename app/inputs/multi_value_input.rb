52fad27031e6a2a68d17d79f444c2779a1c1ce0c
Katelynn Thorn <katelynnthorn@katelynns-mbp.lan>
2022-01-12 15:17:59 -0800

    Ticket #1951: Commit to fix lint errors (fixes #1951)

--- app/inputs/multi_value_input.rb
+++ app/inputs/multi_value_input.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Class for multi-value input forms
 class MultiValueInput < SimpleForm::Inputs::CollectionInput
-  def input(wrapper_options)
+  def input(_wrapper_options)
     @rendered_first_element = false
     input_html_classes.unshift('string')
     input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
@@ -15,71 +18,73 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
 
   protected
 
-    def buffer_each(collection)
-      collection.each_with_object('').with_index do |(value, buffer), index|
-        buffer << yield(value, index)
-      end
+  def buffer_each(collection)
+    collection.each_with_object('').with_index do |(value, buffer), index|
+      buffer << yield(value, index)
     end
+  end
 
-    def outer_wrapper
-      "    <ul class=\"listing\">\n        #{yield}\n      </ul>\n"
-    end
+  def outer_wrapper
+    "    <ul class=\"listing\">\n        #{yield}\n      </ul>\n"
+  end
 
-    def inner_wrapper
-      <<-HTML
+  def inner_wrapper
+    <<-HTML
           <li class="field-wrapper">
             #{yield}
           </li>
-        HTML
-    end
+    HTML
+  end
 
   private
 
-    # Although the 'index' parameter is not used in this implementation it is useful in an
-    # an overridden version of this method, especially when the field is a complex object and
-    # the override defines nested fields.
-    def build_field_options(value, index)
-      options = input_html_options.dup
+  # Although the 'index' parameter is not used in this implementation it is useful in an
+  # an overridden version of this method, especially when the field is a complex object and
+  # the override defines nested fields.
+  def build_field_options(value, _index)
+    options = input_html_options.dup
 
-      options[:value] = value
-      if @rendered_first_element
-        options[:id] = nil
-        options[:required] = nil
-      else
-        options[:id] ||= input_dom_id
-      end
-      options[:class] ||= []
-      options[:class] += ["#{input_dom_id} form-control multi-text-field"]
-      # OVERRIDE FROM HYDRA-EDITOR to correct aria labeling
-      @rendered_first_element = true
-
-      options
+    options[:value] = value
+    if @rendered_first_element
+      options[:id] = nil
+      options[:required] = nil
+    else
+      options[:id] ||= input_dom_id
     end
+    options[:class] ||= []
+    options[:class] += ["#{input_dom_id} form-control multi-text-field"]
+    # OVERRIDE FROM HYDRA-EDITOR to correct aria labeling
+    @rendered_first_element = true
 
-    def build_field(value, index)
-      options = build_field_options(value, index)
-      if options.delete(:type) == 'textarea'.freeze
-        @builder.text_area(attribute_name, options)
-      else
-        @builder.text_field(attribute_name, options)
-      end
+    options
+  end
+
+  def build_field(value, index)
+    options = build_field_options(value, index)
+    if options.delete(:type) == 'textarea'
+      @builder.text_area(attribute_name, options)
+    else
+      @builder.text_field(attribute_name, options)
     end
+  end
 
-    def label_id
-      input_dom_id + '_label'
+  def label_id
+    input_dom_id + '_label'
+  end
+
+  def input_dom_id
+    input_html_options[:id] || "#{object_name}_#{attribute_name}"
+  end
+
+  def collection
+    @collection ||= begin
+      val = object.send(attribute_name)
+      col = val.respond_to?(:to_ary) ? val.to_ary : val
+      col.reject { |value| value.to_s.strip.blank? } + ['']
     end
+  end
 
-    def input_dom_id
-      input_html_options[:id] || "#{object_name}_#{attribute_name}"
-    end
-
-    def collection
-      @collection ||= begin
-                        val = object.send(attribute_name)
-                        col = val.respond_to?(:to_ary) ? val.to_ary : val
-                        col.reject { |value| value.to_s.strip.blank? } + ['']
-                      end
-    end
-
-    def multiple?; true; end
+  def multiple?
+    true
+  end
 end
