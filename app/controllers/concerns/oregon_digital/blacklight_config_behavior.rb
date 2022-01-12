2e16bc6e63c1e3597afafd39282e0158a43f6371
Katelynn Thorn <katelynnthorn@katelynns-mbp.lan>
2022-01-12 10:32:51 -0800

    Ticket #1951: Commit to correct errors from NU Checker on Search page (fixes #1951)

--- app/controllers/concerns/oregon_digital/blacklight_config_behavior.rb
+++ app/controllers/concerns/oregon_digital/blacklight_config_behavior.rb
@@ -70,7 +70,7 @@ module OregonDigital
         #   The ordering of the field names is the order of the display
         config.add_index_field 'title_tesim', label: 'Title', itemprop: 'name', if: false, highlight: true
         config.add_index_field 'creator_label_sim', itemprop: 'creator', link_to_search: 'creator_label_sim', max_values: 3, max_values_label: 'others'
-        config.add_index_field 'date_tesim', itemprop: 'date'
+        config.add_index_field 'date_tesim'
         config.add_index_field 'description_tesim', itemprop: 'description', helper_method: :iconify_auto_link_with_highlight, truncate: { list: 20, masonry: 10 }, max_values: 1, highlight: true, if: lambda { |_context, _field_config, document|
           # Only display description if a highlight is hit
           !document.response.dig('highlighting', document.id, 'description_tesim').nil?
