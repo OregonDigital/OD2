5a7ab739bbee15703eb52cb80ab265ab19c772ba
Katelynn Thorn <katelynnthorn@katelynns-mbp.lan>
2022-01-27 13:57:11 -0800

    Ticket #1790: Commit to edit toggleFooter function and fix focus (fixes

--- app/assets/javascripts/oregon_digital/toggle_login_footer.js
+++ app/assets/javascripts/oregon_digital/toggle_login_footer.js
@@ -3,4 +3,7 @@ function toggleFooter (footer) {
   $('#login-modal-footer-UO-footer').hide();
   $('#login-modal-footer-local-footer').hide();
   $('#login-modal-footer-' + footer).toggle();
+
+  var e = $.Event('shown.bs.modal', {});
+  $('#user-login-modal').trigger(e); 
 }
