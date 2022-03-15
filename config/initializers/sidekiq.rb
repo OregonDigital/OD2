1a3ee908d2daa267ee087a91a4a2c0afc3aeaabb
Ryan Ordway <ryan.ordway@oregonstate.edu>
2022-03-14 19:26:01 -0700

    remove sidekiq_queue_metrics require

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal:true
 
 require 'sidekiq/web'
-require 'sidekiq_queue_metrics'
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
 Sidekiq::Web.set :sessions,       Rails.application.config.session_options
 
