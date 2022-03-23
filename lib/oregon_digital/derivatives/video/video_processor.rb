1c58afd9c78046889c30a7879ad8c19935193aa1
straleyb <straleyb@onid.oregonstate.edu>
2022-03-23 12:29:30 -0700

    update command to work

--- lib/oregon_digital/derivatives/video/video_processor.rb
+++ lib/oregon_digital/derivatives/video/video_processor.rb
@@ -6,7 +6,7 @@ module OregonDigital::Derivatives
       
       def options_for(format)
         input_options = ""
-        output_options = "-c:v libx264 -filter:v fps=fps=30 -b:v 3500K"
+        output_options = "-c:v libx264 -filter:v fps=fps=30 -filter:v \"scale='min(1280,iw)':min'(720,ih)':force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2\" -b:v 3500K-c:v libx264 -filter:v fps=fps=30 -b:v 3500K"
       
         { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
       end
