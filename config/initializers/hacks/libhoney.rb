da69d557218241df5049a2e40ba745c8f4ca4c0f
Corey Gillen <corey.gillen@oregonstate.edu>
2022-04-05 13:07:12 -0700

    Make hacks run every page load in dev
    Based on this answer: https://stackoverflow.com/questions/8895103/how-can-i-keep-my-initializer-configuration-from-being-lost-in-development-mode

--- config/initializers/hacks/libhoney.rb
+++ config/initializers/hacks/libhoney.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal:true
 
-Libhoney::LogTransmissionClient.class_eval do
-  def add(event)
-    if @verbose
-      data = "Honeycomb dataset '#{event.dataset}' | #{event.timestamp.iso8601}"
-      data << " (sample rate: #{event.sample_rate})" if event.sample_rate != 1
-      @output.print("#{data} | ")
+Rails.application.config.to_prepare do
+  Libhoney::LogTransmissionClient.class_eval do
+    def add(event)
+      if @verbose
+        data = "Honeycomb dataset '#{event.dataset}' | #{event.timestamp.iso8601}"
+        data << " (sample rate: #{event.sample_rate})" if event.sample_rate != 1
+        @output.print("#{data} | ")
+      end
+      @output.puts(event.data.to_s.to_json)
     end
-    @output.puts(event.data.to_s.to_json)
   end
 end
