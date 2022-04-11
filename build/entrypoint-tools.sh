1628defd1107a4a83989ab4cb09577929d747c46
Ryan Ordway <ryan.ordway@oregonstate.edu>
2022-04-11 15:28:16 -0700

    instead of sleeping forever, exec /sbin/init

--- build/entrypoint-tools.sh
+++ build/entrypoint-tools.sh
@@ -3,9 +3,14 @@
 echo "Starting tools container ${RAILS_ENV}"
 
 # Get setup to be able to do stuff
+echo "Installing gems..."
 ./build/install_gems.sh
 
+# Start init
+echo "Starting init"
+exec /sbin/init
+
 # Sleep forever, sweet container
-while `true`; do
-   sleep 600
-done
+#while `true`; do
+#   sleep 600
+#done
