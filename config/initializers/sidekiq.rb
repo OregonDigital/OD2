247c2bcd3c773c2c28c3a4a5ee6305ee5be4c6a5
Ryan Ordway <ryan.ordway@oregonstate.edu>
2022-04-11 15:46:31 -0700

    add queue_as for specific job types

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -16,3 +16,11 @@ end
 Sidekiq.configure_client do |s|
   s.redis = redis_conn
 end
+
+# Override specific job queues
+CreateDerivativeJob.queue_as :ingest
+CreateWorkJob.queue_as :ingest
+AttachFilesToWorkJob.queue_as :ingest
+EventJob.queue_as :events
+CharacterizeJob.queue_as :ingest
+StreamNotificationsJob.queue_as :notify
