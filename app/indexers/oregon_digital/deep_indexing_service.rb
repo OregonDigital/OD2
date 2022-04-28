f07baad320b88d30eaf0fb7704676c47a7adeee4
lsat12357 <lsato@uoregon.edu>
2022-04-28 11:10:48 -0700

    bring combined from FGW, block fetching at first

--- app/indexers/oregon_digital/deep_indexing_service.rb
+++ app/indexers/oregon_digital/deep_indexing_service.rb
@@ -10,12 +10,33 @@ module OregonDigital
     self.stored_fields = %i[]
     self.symbol_fields = %i[]
 
+    protected
+
+    # override Hyrax/ActiveFedora add_assertions to insert combined labels
+    # rubocop:disable Metrics/MethodLength
+    def add_assertions(prefix_method, solr_doc = {})
+      fetch_external
+      fields.each do |field_key, field_info|
+        solr_field_key = solr_document_field_name(field_key, prefix_method)
+        field_info.values.each do |val|
+          append_to_solr_doc(solr_doc, solr_field_key, field_info, val)
+          combined_prop = combined_properties[field_key]
+          next if combined_prop.blank?
+
+          label = labelize(val)
+          append_combined_label(solr_doc, "#{combined_prop}_combined_label", field_info, label)
+        end
+      end
+      solr_doc
+    end
+
     private
 
-    # rubocop:disable Metrics/CyclomaticComplexity
-    # rubocop:disable Metrics/AbcSize
-    # OVERRIDEN FROM HYRAX TO BYPASS THE FETCHING OF CONTROLLED VOCABULARIES
+    # block this until asset has reached a later stage in its workflow
+    # potentially remove block post migration
     def fetch_external
+      return if object.representative_id.nil?
+
       object.controlled_properties.each do |property|
         object[property].each do |value|
           resource = value.respond_to?(:resource) ? value.resource : value
@@ -23,12 +44,54 @@ module OregonDigital
 
           next if value.is_a?(ActiveFedora::Base)
 
-          # Fetch if the vocab is cached since this is fast and can be displayed quicker.
-          fetch_with_persistence(resource) if !resource.class.to_s.include?('Location') && resource.in_triplestore?
+          # assuming the FetchGraphWorker has run
+          # potentially redo using OregonDigital::Triplestore.fetch_cached_term
+          fetch_with_persistence(resource)
         end
       end
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
-    # rubocop:enable Metrics/AbcSize
+
+    # override Hyrax fetch_value to get rid of misleading logging
+    def fetch_value(value)
+      value.fetch(headers: { 'Accept' => default_accept_header })
+    rescue IOError, SocketError => e
+      # IOError could result from a 500 error on the remote server
+      # SocketError results if there is no server to connect to
+      Rails.logger.error "Unable to fetch #{value.rdf_subject}.\n#{e.message}"
+    end
+
+    # modelled after Hyrax::DeepIndexingService methods
+    def append_combined_label(solr_doc, solr_field_key, field_info, val)
+      ActiveFedora::Indexing::Inserter.create_and_insert_terms(solr_field_key, val, field_info.behaviors, solr_doc)
+    end
+
+    # adapted from FetchGraphWorker
+    def labelize(val)
+      return val if val.first.is_a?(String)
+
+      val.solrize.last.is_a?(String) ? val.solrize.last : val.solrize.last[:label].split('$').first
+    end
+
+    def combined_properties
+      @combined_properties ||= combined_property_map
+    end
+
+    def combined_property_map
+      cpm = {}
+      %w[ranger_district water_basin location].each do |prop|
+        cpm[prop] = 'location'
+      end
+      %w[arranger artist author cartographer collector composer creator contributor dedicatee donor designer editor illustrator interviewee interviewer lyricist owner patron photographer print_maker recipient transcriber translator].each do |prop|
+        cpm[prop] = 'creator'
+      end
+      %w[keyword subject].each do |prop|
+        cpm[prop] = 'topic'
+      end
+      %w[taxon_class family genus order species phylum_or_division].each do |prop|
+        cpm[prop] = 'scientific'
+      end
+      cpm
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
