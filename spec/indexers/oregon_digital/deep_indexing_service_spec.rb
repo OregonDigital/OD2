b9207f720433b9a5c5f0c9e670dea18c0a6944a5
lsat12357 <lsato@uoregon.edu>
2022-05-05 07:54:29 -0700

    mock triplestore

--- spec/indexers/oregon_digital/deep_indexing_service_spec.rb
+++ spec/indexers/oregon_digital/deep_indexing_service_spec.rb
@@ -4,13 +4,20 @@ RSpec.describe OregonDigital::DeepIndexingService do
   subject(:service) { described_class.new(work) }
 
   let(:user) { build(:user) }
-  let(:file_set) { FactoryBot.create(:file_set, user: user, title: ['Shark'], content: file) }
   let(:file) { File.open(fixture_path + '/test.jpg') }
   let(:work) { create(:image, title: ['Sharks on a plane'], location: [location], depositor: user.email, id: 'abcde1234', ordered_members: [file_set], representative_id: file_set.id) }
+  let(:file_set) { create(:file_set, user: user, title: ['Shark'], content: file) }
 
   describe '#add_assertions' do
     context 'when there is a label to fetch' do
       let(:location) { Hyrax::ControlledVocabularies::Location.new('https://sws.geonames.org/5037649/') }
+      let(:geo_subject) { RDF::URI('https://sws.geonames.org/5037649/') }
+      let(:graph) do
+        g = RDF::Graph.new
+        g << RDF::Statement.new(geo_subject, RDF::URI('http://www.geonames.org/ontology#name'), 'Newberg')
+        g << RDF::Statement.new(geo_subject, RDF::URI('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), RDF::URI('http://www.geonames.org/ontology#Feature'))
+        g
+      end
 
       before do
         newberg = <<RDFXML.strip_heredoc
@@ -26,9 +33,7 @@ RDFXML
         stub_request(:get, 'https://sws.geonames.org/5037649/')
           .to_return(status: 200, body: newberg,
                      headers: { 'Content-Type' => 'application/rdf+xml;charset=UTF-8' })
-        stub_request(:get, 'http://ci-test:8080/bigdata/namespace/rw/sparql?GETSTMTS&includeInferred=false&s=%3Chttps://sws.geonames.org/5037649/%3E')
-          .to_return(status: 200, body: newberg,
-                     headers: { 'Content-Type' => 'application/rdf+xml;charset=UTF-8' })
+        allow(OregonDigital::Triplestore).to receive(:fetch_cached_term).and_return(graph)
       end
 
       it 'adds both property label and combined label' do
