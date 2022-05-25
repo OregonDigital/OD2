3a83cbf1f37381fa5ebfc3321af8043d681c3256
Corey Gillen <corey.gillen@oregonstate.edu>
2022-05-25 13:01:20 -0700

    Tune A/V derivatives to ensure smaller size than original

--- lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
+++ lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
@@ -4,11 +4,12 @@ module OregonDigital::Derivatives::Audio
   # GraphicsMagick processor for derivatives
   class FfmpegProcessor < Hydra::Derivatives::Processors::Audio
     # returns a hash of options that the specific processors use
-    def options_for(_format)
-      {
-        Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => '',
-        Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => '-ar 22050 -b:a 96K'
-      }
+    def options_for(format)
+      input_options = ''
+      # pass mp3s through, all else get a standard sampling rate and bitrate
+      output_options = '-ar 22050 -b:a 96K' if format != 'mp3'
+
+      { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
     end
   end
 end
--- lib/oregon_digital/derivatives/video/video_processor.rb
+++ lib/oregon_digital/derivatives/video/video_processor.rb
@@ -8,7 +8,7 @@ module OregonDigital::Derivatives
 
       def options_for(_format)
         input_options = ''
-        output_options = "-c:v libx264 -c:a copy -filter:v \"scale='min(1280,iw)':min'(720,ih)':force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,fps=30\" -b:v 3500K -b:a 3500K"
+        output_options = "-c:v libx264 -c:a copy -filter:v \"scale='min(1280,iw)':min'(720,ih)':force_original_aspect_ratio=decrease\" -fpsmax 30 -crf 25"
 
         { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
       end
