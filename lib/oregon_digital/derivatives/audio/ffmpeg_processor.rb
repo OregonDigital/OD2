748f9eeaa26f6c62e4309a859a4d193428ef9176
Corey Gillen <corey.gillen@oregonstate.edu>
2022-05-26 15:57:19 -0700

    Use streamio-ffmpeg to determine A/V attributes and clamp derivative specs

--- lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
+++ lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
@@ -5,9 +5,11 @@ module OregonDigital::Derivatives::Audio
   class FfmpegProcessor < Hydra::Derivatives::Processors::Audio
     # returns a hash of options that the specific processors use
     def options_for(format)
+      audio = FFMPEG::Movie.new(source_path)
+      audio_bitrate = [audio.audio_bitrate, 96000].min
       input_options = ''
       # pass mp3s through, all else get a standard sampling rate and bitrate
-      output_options = '-ar 22050 -b:a 96K' if format != 'mp3'
+      output_options = "-ar 22050 -b:a #{audio_bitrate}" if format != 'mp3'
 
       { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
     end
--- lib/oregon_digital/derivatives/video/video_processor.rb
+++ lib/oregon_digital/derivatives/video/video_processor.rb
@@ -7,8 +7,11 @@ module OregonDigital::Derivatives
       protected
 
       def options_for(_format)
+        video = FFMPEG::Movie.new(source_path)
+        video_bitrate = [video.video_bitrate, 3500000].min
+        audio_bitrate = [video.audio_bitrate, 3500000].min
         input_options = ''
-        output_options = "-c:v libx264 -c:a copy -filter:v \"scale='min(1280,iw)':min'(720,ih)':force_original_aspect_ratio=decrease\" -fpsmax 30 -crf 25"
+        output_options = "-c:v libx264 -c:a copy -b:v #{video_bitrate} -b:a #{audio_bitrate} -filter:v \"scale='min(1280,iw)':min'(720,ih)':force_original_aspect_ratio=decrease\" -fpsmax 30"
 
         { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
       end
