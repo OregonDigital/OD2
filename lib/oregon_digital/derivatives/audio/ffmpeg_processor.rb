f38dc74ef3038c998d14967500da2f6f33309e07
Corey Gillen <corey.gillen@oregonstate.edu>
2022-05-27 12:55:22 -0700

    Remove ogg derivative and check incoming audio codec not output format

--- lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
+++ lib/oregon_digital/derivatives/audio/ffmpeg_processor.rb
@@ -9,7 +9,7 @@ module OregonDigital::Derivatives::Audio
       audio_bitrate = [audio.audio_bitrate, 96000].min
       input_options = ''
       # pass mp3s through, all else get a standard sampling rate and bitrate
-      output_options = "-ar 22050 -b:a #{audio_bitrate}" if format != 'mp3'
+      output_options = "-ar 44100 -b:a #{audio_bitrate}" if audio.audio_codec != 'mp3'
 
       { Hydra::Derivatives::Processors::Ffmpeg::OUTPUT_OPTIONS => output_options, Hydra::Derivatives::Processors::Ffmpeg::INPUT_OPTIONS => input_options }
     end
