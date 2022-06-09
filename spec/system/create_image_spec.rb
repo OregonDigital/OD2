b25954096190946333aef55b2486d20590bbc9f7
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2022-06-09 10:03:58 -0700

    Update create_image_spec.rb

--- spec/system/create_image_spec.rb
+++ spec/system/create_image_spec.rb
@@ -39,7 +39,8 @@ RSpec.describe 'Create a Image',  js: true, type: :system, clean_repo: true do
       within('ul.nav-tabs') do
         click_link 'Relationships'
       end
-      find("#generic_admin_set_id option[value='1234']").select_option
+      first_element = find('#image_admin_set_id > option:nth-child(2)').text
+      select(first_element, from: 'image_admin_set_id')
       within('ul.nav-tabs') do
         click_link 'Descriptions' # switch tab
       end
