3a24e01fdffc2d7d0c43799df381919e9d7071f3
lsat12357 <lsato@uoregon.edu>
2022-06-22 17:10:02 -0700

    temp disable FGW

--- app/services/oregon_digital/listeners/metadata_fetch_listener.rb
+++ app/services/oregon_digital/listeners/metadata_fetch_listener.rb
@@ -3,17 +3,19 @@
 module OregonDigital
   module Listeners
     # Listens for metadata update events and enqueues metadata fetch jobs
+    # FetchGraphWorker provides redundancy in case DeepIndexingService fails
+    # However, for duration of migration, disable FGW
     class MetadataFetchListener
       ##
       # @param event [Dry::Event]
       def on_object_metadata_updated(event)
-        FetchGraphWorker.perform_at(2.seconds, event[:object].id, event[:object].depositor)
+        #FetchGraphWorker.perform_at(2.seconds, event[:object].id, event[:object].depositor)
       end
 
       ##
       # @param event [Dry::Event]
       def on_file_set_attached(event)
-        FetchGraphWorker.perform_at(2.seconds, event[:file_set].parent.id, event[:file_set].parent.depositor)
+        #FetchGraphWorker.perform_at(2.seconds, event[:file_set].parent.id, event[:file_set].parent.depositor)
       end
     end
   end
