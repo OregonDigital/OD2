9422ceec7e458824a6527c0059f8cf5d7d340d71
Ryan Ordway <ryan.ordway@oregonstate.edu>
2022-06-29 13:44:08 -0700

    remove comments

--- build/entrypoint-sidekiq.sh
+++ build/entrypoint-sidekiq.sh
@@ -7,22 +7,9 @@ echo "[$timestamp] Initializing Sidekiq (${RAILS_ENV})"
 mkdir -p /data/tmp/pids
 rm -f /data/tmp/pids/sidekiq.pid
 
-#timestamp=`date +'%Y-%m-%d %H:%M:%S'`
-#echo "[$timestamp] Validating database migrations"
-#/data/build/validate_migrated.sh
 timestamp=`date +'%Y-%m-%d %H:%M:%S'`
 echo "[$timestamp] Rebuild gems if necessary"
 /data/build/install_gems.sh
-
-# Create default roles
-#timestamp=`date +'%Y-%m-%d %H:%M:%S'`
-#echo "[$timestamp] Create OD Roles if necessary"
-#bundle exec rails oregon_digital:create_roles
-
-# Submit a marker to honeycomb marking the time the application starts booting
-#if [ "${RAILS_ENV}" = 'production' ]; then
-#  curl https://api.honeycomb.io/1/markers/${HONEYCOMB_DATASET} -X POST -H "X-Honeycomb-Team: ${HONEYCOMB_WRITEKEY}" -d "{\"message\":\"Sidekiq (${RAILS_ENV}) - ${DEPLOYED_VERSION} - booting\", \"type\":\"deploy\"}"
-#fi
 
 timestamp=`date +'%Y-%m-%d %H:%M:%S'`
 echo "[$timestamp] Starting Sidekiq ($RAILS_ENV)"
