059a77d295a7b739ee5bcbd19b7aa41f905e8225
Corey Gillen <corey.j.gillen@gmail.com>
2022-07-01 12:08:44 -0700

    Add child work metadata to collection export

--- app/services/oregon_digital/collection_streamer.rb
+++ app/services/oregon_digital/collection_streamer.rb
@@ -17,26 +17,32 @@ module OregonDigital
       writer = ZipTricks::BlockWrite.new(&chunks)
 
       ZipTricks::Streamer.open(writer, auto_rename_duplicate_filenames: true) do |zip|
-        stream_collection(collection, '/', zip)
+        stream_collection(collection, '', zip)
       end
     end
 
     private
 
     def stream_collection(collection, folder, zip)
+      # Init our metadata CSV
+      metadata = [collection.csv_metadata]
+
       # Recursively drill down into sub-collections
       collection.child_collections.each do |col|
         stream_collection(col, "#{folder}#{col.id}/", zip)
       end
 
-      # Add collection metadata
-      zip.write_deflated_file("#{folder}metadata.csv") do |file_writer|
-        file_writer << collection.csv_metadata
-      end
-
-      # Add low quality works from collection
+      # Add low quality works from collection and append metadata
       collection.child_works.each do |work|
         stream_works_low(work, zip, folder)
+        metadata << work.csv_metadata
+      end
+
+      # Add all metadata
+      zip.write_deflated_file("#{folder}metadata.csv") do |file_writer|
+        metadata.each do |data|
+          file_writer << data 
+        end
       end
     end
   end
