e180c10ec7577c1503849e12d42ac8ad65c9b2e8
Corey Gillen <corey.gillen@oregonstate.edu>
2022-08-09 09:16:09 -0700

    Update PermissionChangePropagationJob to use valkyrie style

--- app/jobs/oregon_digital/permission_change_propagation_job.rb
+++ app/jobs/oregon_digital/permission_change_propagation_job.rb
@@ -4,8 +4,9 @@ module OregonDigital
   # Log content permissions update to each fileset's activity streams
   class PermissionChangePropagationJob < Hyrax::ApplicationJob
     def perform(work)
-      work.file_sets.each do |file_set|
-        OregonDigital::PermissionChangePropagationEventJob.perform_later(file_set, work)
+      # Tried updating to valkyrie style, needs tested
+      Hyrax.custom_queries.find_child_filesets(resource: work, use_valkyrie: false).each do |file_set|
+        OregonDigital::PermissionChangePropagationEventJob.perform_later(file_set, nil)
       end
     end
   end
