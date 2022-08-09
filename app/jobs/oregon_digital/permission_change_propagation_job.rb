ff128a0ce7e0e253ba7a0973eb8a345d7e03ba10
Corey Gillen <corey.gillen@oregonstate.edu>
2022-08-09 11:55:52 -0700

    Revert fancy valkyrie queries for PermissionChangePropagationEventJob

--- app/jobs/oregon_digital/permission_change_propagation_job.rb
+++ app/jobs/oregon_digital/permission_change_propagation_job.rb
@@ -5,8 +5,8 @@ module OregonDigital
   class PermissionChangePropagationJob < Hyrax::ApplicationJob
     def perform(work)
       # Tried updating to valkyrie style, needs tested
-      Hyrax.custom_queries.find_child_filesets(resource: work, use_valkyrie: false).each do |file_set|
-        OregonDigital::PermissionChangePropagationEventJob.perform_later(file_set, nil)
+      work.file_sets.each do |file_set|
+        OregonDigital::PermissionChangePropagationEventJob.perform_later(file_set, work)
       end
     end
   end
