4d6fbce2c8df8a2044cd7b6f2fb9f8a3b6d7ac52
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2022-08-12 10:18:08 -0700

    Update user_edit_profile_event_job.rb

--- app/jobs/user_edit_profile_event_job.rb
+++ app/jobs/user_edit_profile_event_job.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 # Log user profile edits to activity streams
 class UserEditProfileEventJob < EventJob
-def perform(user)
-@user = user
-super
-end
+	def perform(user)
+		@user = user
+		super
+	end
 
-def action
-"User profile has been updated by #{@user}"
-end
+	def action
+		"User profile has been updated by #{@user}"
+	end
 end
