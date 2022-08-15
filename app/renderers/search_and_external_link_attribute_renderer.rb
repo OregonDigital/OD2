4a1bfd20e688c9af637091f208b4c87054593c8e
Corey Gillen <corey.gillen@oregonstate.edu>
2022-08-15 10:18:34 -0700

    Use CV fetched labels for data source accuracy

--- app/renderers/search_and_external_link_attribute_renderer.rb
+++ app/renderers/search_and_external_link_attribute_renderer.rb
@@ -5,10 +5,8 @@ class SearchAndExternalLinkAttributeRenderer < Hyrax::Renderers::FacetedAttribut
   private
 
   def li_value(value)
-    uri = options[:links][value] unless options[:links].nil?
-
     links = Array(super(value))
-    links << link_to("<sup>#{options[:indices][uri]}</sup>".html_safe, '#data_sources', 'aria-label' => 'learn more about this taxonomy term', class: 'metadata-superscript', title: 'learn more') unless uri.nil?
+    links << link_to("<sup>#{options[:indices][value]}</sup>".html_safe, '#data_sources', 'aria-label' => 'learn more about this taxonomy term', class: 'metadata-superscript', title: 'learn more') unless value.nil?
     links.join('')
   end
 end
