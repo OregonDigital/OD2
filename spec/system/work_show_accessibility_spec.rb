c57a24f6dac6d620351ed99ed097f7bdb3417528
Corey Gillen <corey.gillen@oregonstate.edu>
2022-09-07 10:29:58 -0700

    Remove RDF and use JSONLD

--- spec/system/work_show_accessibility_spec.rb
+++ spec/system/work_show_accessibility_spec.rb
@@ -4,24 +4,10 @@ RSpec.describe 'Work show page', js: true, type: :system, clean_repo: true do
   let(:work) { create(:work, with_admin_set: true, visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC, rights_statement: ['http://rightsstatements.org/vocab/InC/1.0/'], creator: ['http://opaquenamespace.org/ns/creator/UniversityofOregonstudents'], description: ['description']) }
 
   before do
-    stub_request(:get, 'http://ci-test:8080/bigdata/namespace/rw/sparql?GETSTMTS&includeInferred=false&s=%3Chttp://opaquenamespace.org/ns/creator/UniversityofOregonstudents%3E').with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Connection' => 'keep-alive', 'Host' => 'ci-test:8080', 'Keep-Alive' => '30', 'User-Agent' => 'Ruby' }).to_return(status: 200, body: '<?xml version="1.0" encoding="UTF-8"?>
-      <rdf:RDF
-        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-
-      <rdf:Description rdf:about="http://opaquenamespace.org/ns/creator/UniversityofOregonstudents">
-        <rdf:type rdf:resource="http://www.w3.org/2004/02/skos/core#CorporateName"/>
-        <rdf:type rdf:resource="http://www.w3.org/2000/01/rdf-schema#Resource"/>
-        <comment xmlns="http://www.w3.org/2000/01/rdf-schema#" xml:lang="en">Professional contributors.</comment>
-        <isDefinedBy xmlns="http://www.w3.org/2000/01/rdf-schema#" rdf:resource="http://opaquenamespace.org/VOCAB_PLACEHOLDER.nt"/>
-        <label xmlns="http://www.w3.org/2000/01/rdf-schema#" xml:lang="en">University of Oregon students</label>
-        <seeAlso xmlns="http://www.w3.org/2000/01/rdf-schema#" rdf:resource="http://opaquenamespace.org/VOCAB_PLACEHOLDER.nt"/>
-        <issued xmlns="http://purl.org/dc/terms/" rdf:datatype="http://www.w3.org/2001/XMLSchema#date">2015-07-16</issued>
-        <issued xmlns="http://purl.org/dc/terms/" rdf:datatype="http://www.w3.org/2001/XMLSchema#date">2015-08-25</issued>
-        <modified xmlns="http://purl.org/dc/terms/" rdf:datatype="http://www.w3.org/2001/XMLSchema#date">2015-07-16</modified>
-        <modified xmlns="http://purl.org/dc/terms/" rdf:datatype="http://www.w3.org/2001/XMLSchema#date">2015-08-25</modified>
-      </rdf:Description>
-
-      </rdf:RDF>', headers: {})
+    stub_request(:get, 'http://opaquenamespace.org/ns/creator/UniversityofOregonstudents')
+      .to_return(status: 200, body: '', headers: {})
+    stub_request(:get, 'http://ci-test:8080/bigdata/namespace/rw/sparql?GETSTMTS&includeInferred=false&s=%3Chttp://opaquenamespace.org/ns/creator/UniversityofOregonstudents%3E')
+      .to_return(status: 200, body: '', headers: {})
   end
 
   context 'with an annonymous user' do
