9f378a1b203b3a347274065a6684d1df82180e69
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2022-09-27 14:06:01 -0700

    more fixes (#2511)

--- app/mailers/oregon_digital/delete_user_mailer.rb
+++ app/mailers/oregon_digital/delete_user_mailer.rb
@@ -3,9 +3,8 @@
 module OregonDigital
   # Emails users that have reviews to do
   class DeleteUserMailer < ApplicationMailer
-    def delete_user
-      @user = params[:user]
-      mail(to: @user.email, subject: 'Oregon Digital account removal confirmation')
+    def delete_user(user)
+      mail(to: user.email, subject: 'Oregon Digital account removal confirmation')
     end
   end
 end
