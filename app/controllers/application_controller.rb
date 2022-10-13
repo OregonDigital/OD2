5be6e8b0e659f14e0fffc9cd4a5858e473e4d38a
Corey Gillen <corey.gillen@oregonstate.edu>
2022-10-13 14:34:12 -0700

    Redirect non-institution users too (#2543)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 # This is the base controller of the application. Basic setup for the rest of the app occurs here.
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
@@ -10,6 +11,7 @@ class ApplicationController < ActionController::Base
   # Adds Hyrax behaviors into the application controller
   include Hyrax::Controller
   include Hyrax::ThemedLayoutController
+
   with_themed_layout '1_column'
 
   # Hyrax 2.1 migration
@@ -23,5 +25,21 @@ class ApplicationController < ActionController::Base
       Honeycomb.add_field('version', ENV.fetch('DEPLOYED_VERSION', OregonDigital::VERSION))
       Honeycomb.add_field('classname', self.class.name)
     end
+  end
+
+  private
+
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+  #    infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end
