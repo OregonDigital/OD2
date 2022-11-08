b2be76ca2599cc57bb92b1096bb13799a5f1a531
Corey Gillen <corey.gillen@oregonstate.edu>
2022-11-08 13:10:36 -0800

    Send users to 403 for access denied works

--- app/controllers/concerns/oregon_digital/works_controller_behavior.rb
+++ app/controllers/concerns/oregon_digital/works_controller_behavior.rb
@@ -3,6 +3,15 @@
 module OregonDigital
   # Behavior for each work type controller
   module WorksControllerBehavior
+
+    # Send user to 403 page rather than forward with flash message
+    rescue_from CanCan::AccessDenied do |exception|
+      respond_to do |wants|
+        wants.json { head :forbidden }
+        wants.html { render(file: File.join('public/403.html'), status: 403, layout: false) }
+      end
+    end
+
     # We can use Hyrax::WorksControllerBehavior definition and add on additional params we want
     def attributes_for_actor
       attributes = super
