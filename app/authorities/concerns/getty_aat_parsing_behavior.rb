be434aba4048815ff195636d84a929c8da5f3752
Corey Gillen <corey.gillen@oregonstate.edu>
2022-11-18 15:02:56 -0800

    Lint

--- app/authorities/concerns/getty_aat_parsing_behavior.rb
+++ app/authorities/concerns/getty_aat_parsing_behavior.rb
@@ -2,12 +2,11 @@
 
 # Getty AAT places the ID in an unusual spot in the response
 module GettyAatParsingBehavior
-
   private
 
   def parse_authority_response(term, vocabulary)
-    id = (term.first['id'] || term.first['@id'].to_s) unless !term.first.is_a?(Hash)
+    id = (term.first['id'] || term.first['@id'].to_s) if term.first.is_a?(Hash)
     [{ 'id' => id,
-        'label' => label.call(term, vocabulary) }]
+       'label' => label.call(term, vocabulary) }]
   end
 end
