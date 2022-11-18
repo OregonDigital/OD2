9f116cdf22658e5f605b0a673e21fccf35a843d8
Corey Gillen <corey.gillen@oregonstate.edu>
2022-11-18 09:35:05 -0800

    Advanced Search wireframe implementation (#2592)
    * Add very-light-grey lines around advanced search header
    
    * Move advanced facets to sidebar
    
    * Change matching select box to match other select boxes
    
    * Match advanced search boxes to wireframe
    
    * Match advanced search search tips to wireframe
    
    * Add search button to bottom and fix facet searching
    
    * Advanced facet styling
    
    * Facet value css
    
    * Fixes for mobile
    
    * Collection search
    
    * Css for chosen dropdown

--- app/assets/javascripts/oregon_digital/advanced_facets.js
+++ app/assets/javascripts/oregon_digital/advanced_facets.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.advanced-search-facet-select').chosen({
     allow_single_deselect: true,
     max_shown_results: 10000,
-    no_results_text: 'No results matched'
-    });
-}); 
+    no_results_text: 'No results matched',
+    width: '100%'
+  });
+});
