9b36affb0e3f7ec221fb780669cbc0492bee33a5
straleyb <straleyb@onid.oregonstate.edu>
2022-11-21 13:25:39 -0800

    update lint

--- app/controllers/concerns/hyrax/dashboard_authorization.rb
+++ app/controllers/concerns/hyrax/dashboard_authorization.rb
@@ -15,11 +15,13 @@ module Hyrax
     end
 
     # rubocop:disable Metrics/CyclomaticComplexity
+    # rubocob:disable Metrics/PerceivedComplexity
     def dashboard_allowed
       # current user has a managing role || current user has a workflow responsibility || if user is trying to create user_collection
       current_user&.role?(::Ability.manager_permission_roles) || current_user&.sipity_agent&.workflow_responsibilities&.count&.positive? || user_is_creating_collection? || user_is_updating_collection?
     end
     # rubocop:enable Metrics/CyclomaticComplexity
+    # rubocob:enable Metrics/PerceivedComplexity
 
     def user_is_creating_collection?
       (allowed_controllers.include? controller_name.to_s) && (params[:collection_type_gid] == Hyrax::CollectionType.find_by(machine_id: :user_collection).gid.to_s)
