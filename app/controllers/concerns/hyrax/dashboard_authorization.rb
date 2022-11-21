c65a9ae052beacdf27fad3474f003bf8e9b6a1f0
straleyb <straleyb@onid.oregonstate.edu>
2022-11-21 13:12:25 -0800

    updates code to allow adding works and updating collections

--- app/controllers/concerns/hyrax/dashboard_authorization.rb
+++ app/controllers/concerns/hyrax/dashboard_authorization.rb
@@ -17,12 +17,20 @@ module Hyrax
     # rubocop:disable Metrics/CyclomaticComplexity
     def dashboard_allowed
       # current user has a managing role || current user has a workflow responsibility || if user is trying to create user_collection
-      current_user&.role?(::Ability.manager_permission_roles) || current_user&.sipity_agent&.workflow_responsibilities&.count&.positive? || user_is_creating_collection?
+      current_user&.role?(::Ability.manager_permission_roles) || current_user&.sipity_agent&.workflow_responsibilities&.count&.positive? || user_is_creating_collection? || user_is_updating_collection?
     end
     # rubocop:enable Metrics/CyclomaticComplexity
 
     def user_is_creating_collection?
-      controller_name.to_s.include?('collections') && params[:collection_type_gid] == Hyrax::CollectionType.find_by(machine_id: :user_collection).gid
+      (allowed_controllers.include? controller_name.to_s) && (params[:collection_type_gid] == Hyrax::CollectionType.find_by(machine_id: :user_collection).gid.to_s)
+    end
+
+    def user_is_updating_collection?
+      (controller_name.to_s == "collection_members") && (params[:collection][:members] == "add")
+    end
+
+    def allowed_controllers
+      ["collections", "works"]
     end
   end
 end
