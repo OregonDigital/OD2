f2da9ea9ac88ad595bda9e61a15c6ead8b1fbde6
straleyb <straleyb@onid.oregonstate.edu>
2022-11-22 07:45:19 -0800

    perceived complexity

--- app/controllers/concerns/hyrax/dashboard_authorization.rb
+++ app/controllers/concerns/hyrax/dashboard_authorization.rb
@@ -14,15 +14,14 @@ module Hyrax
       render(file: File.join("public/#{response_code}.html"), status: response_code, layout: false) unless dashboard_allowed
     end
 
-    # rubocop:disable Metrics/CyclomaticComplexity
     # rubocob:disable Metrics/PerceivedComplexity
-
+    # rubocop:disable Metrics/CyclomaticComplexity
     def dashboard_allowed
       # current user has a managing role || current user has a workflow responsibility || if user is trying to create user_collection
       current_user&.role?(::Ability.manager_permission_roles) || current_user&.sipity_agent&.workflow_responsibilities&.count&.positive? || user_is_creating_collection? || user_is_updating_collection?
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
     # rubocob:enable Metrics/PerceivedComplexity
+    # rubocop:enable Metrics/CyclomaticComplexity
 
     def user_is_creating_collection?
       (allowed_controllers.include? controller_name.to_s) && (params[:collection_type_gid] == Hyrax::CollectionType.find_by(machine_id: :user_collection).gid.to_s)
