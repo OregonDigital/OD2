69b221fc096baae110d72fd8da6bf3083f9e5d0d
Corey Gillen <corey.gillen@oregonstate.edu>
2022-11-23 15:36:29 -0800

    Use .json for getty requests in specs

--- spec/authorities/qa/authorities/creator_spec.rb
+++ spec/authorities/qa/authorities/creator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Qa::Authorities::Creator do
   let(:ons_creator_request) { 'http://opaquenamespace.org/ns/creator/my_id.jsonld' }
   let(:ons_people_request) { 'http://opaquenamespace.org/ns/people/my_id.jsonld' }
   let(:ons_osu_academic_units_request) { 'http://opaquenamespace.org/ns/osuAcademicUnits/my_id.jsonld' }
-  let(:ulan_request) { 'http://vocab.getty.edu/ulan/my_id.jsonld' }
+  let(:ulan_request) { 'http://vocab.getty.edu/ulan/my_id.json' }
   let(:loc_names_request) { 'http://id.loc.gov/authorities/names/my_id.jsonld' }
   let(:wikidata_request) { 'http://www.wikidata.org/entity/my_id' }
   let(:ons_creator_response) { [{ 'rdfs:label': { '@value': 'mylabel' }.with_indifferent_access, '@id': 'http://opaquenamespace.org/ns/creator/my_id' }.with_indifferent_access] }
