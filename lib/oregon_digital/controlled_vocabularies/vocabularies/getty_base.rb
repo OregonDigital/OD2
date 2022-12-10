1dd3419bfb03dfb8f93fdfbc946dd18666c32187
Corey Gillen <corey.gillen@oregonstate.edu>
2022-12-09 16:00:43 -0800

    Improve getty label fetching for QA

--- lib/oregon_digital/controlled_vocabularies/vocabularies/getty_base.rb
+++ lib/oregon_digital/controlled_vocabularies/vocabularies/getty_base.rb
@@ -5,11 +5,18 @@ module OregonDigital
     # Receives information pulled from the endpoint and can parse and generate queries
     class GettyBase
       def self.label(data)
+        # Find all possible labels
         term = data.first['identified_by'].select do |identifier|
           identifier['classified_as'].any? do |classification|
             classification['id'] == 'http://vocab.getty.edu/term/type/Descriptor'
           end
         end
+        # Find an english label
+        term = term.select { |trm|
+          trm['language'].any? { |t|
+            t['id'].in? %i[http://vocab.getty.edu/language/en http://vocab.getty.edu/language/en-us]  == 'http://vocab.getty.edu/language/en-us'
+          }
+        }
         term.count.positive? ? term.first['content'] : data.first['identified_by'][0]['content']
       end
 
