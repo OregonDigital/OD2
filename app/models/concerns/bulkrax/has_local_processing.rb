e5d31f89cbc62e2806215f91c05f674f5bb8e415
lsat12357 <lsato@uoregon.edu>
2023-01-05 14:56:13 -0800

    update bulkrax overrides

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -6,7 +6,7 @@ module Bulkrax
   module HasLocalProcessing
     # Override the factory to use a customized version
     def factory
-      @factory ||= Bulkrax::OregonDigitalObjectFactory.new(parsed_metadata, identifier, replace_files, user, factory_class, update_files)
+      @factory ||= Bulkrax::OregonDigitalObjectFactory.new(attributes: parsed_metadata, source_identifier_value: identifier, work_identifier: work_identifier, replace_files: replace_files, user: user, klass: factory_class, update_files: update_files)
     end
 
     # This method is called during build_metadata
