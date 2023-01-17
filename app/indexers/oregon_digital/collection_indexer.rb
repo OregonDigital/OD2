80db55d5a5e4de8b2c7e1d7c377a0fb250961cae
Corey Gillen <corey.gillen@oregonstate.edu>
2023-01-17 08:31:01 -0800

    Add sorting to explore collections page (#2691)

--- app/indexers/oregon_digital/collection_indexer.rb
+++ app/indexers/oregon_digital/collection_indexer.rb
@@ -5,5 +5,13 @@ module OregonDigital
   class CollectionIndexer < Hyrax::CollectionIndexer
     include OregonDigital::IndexesLinkedMetadata
     include OregonDigital::IndexingDatesBehavior
+
+    # Custom collection indexing options
+    def generate_solr_document
+      super.tap do |solr_doc|
+        # Sortable collection title
+        solr_doc['title_ssort'] = object.title.first
+      end
+    end
   end
 end
