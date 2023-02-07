aae4cbd5ac251dfcfaa984d8eefc9cc2889a5524
Corey Gillen <corey.gillen@oregonstate.edu>
2023-02-07 13:54:28 -0800

    Strip solr stopwords from title_ssort before sending to solr

--- app/indexers/oregon_digital/collection_indexer.rb
+++ app/indexers/oregon_digital/collection_indexer.rb
@@ -5,12 +5,13 @@ module OregonDigital
   class CollectionIndexer < Hyrax::CollectionIndexer
     include OregonDigital::IndexesLinkedMetadata
     include OregonDigital::IndexingDatesBehavior
+    include OregonDigital::StripsStopwords
 
     # Custom collection indexing options
     def generate_solr_document
       super.tap do |solr_doc|
         # Sortable collection title
-        solr_doc['title_ssort'] = object.title.first
+        solr_doc['title_ssort'] = strip_stopwords(object.title.first.downcase) if object.title.first
       end
     end
   end
