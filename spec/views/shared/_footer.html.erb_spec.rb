19b67fc2b5ec56144dde8c5b0dc66253738c3a91
Corey Gillen <corey.gillen@oregonstate.edu>
2023-02-10 12:51:57 -0800

    Remove footer version spec

--- spec/views/shared/_footer.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal:true
-
-RSpec.describe 'shared/_footer', type: :view do
-  include Warden::Test::Helpers
-  it 'renders the application version' do
-    render
-    expect(rendered).to have_text(OregonDigital::VERSION)
-  end
-end
