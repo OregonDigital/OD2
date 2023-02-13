b114b4c59631e409371ca2ea9527bfa0ee80b048
straleyb <straleyb@onid.oregonstate.edu>
2023-02-13 09:16:40 -0800

    updates GA

--- config/initializers/_hyrax.rb
+++ config/initializers/_hyrax.rb
@@ -66,15 +66,15 @@ Hyrax.config do |config|
   # Enable displaying usage statistics in the UI
   # Defaults to false
   # Requires a Google Analytics id and OAuth2 keyfile.  See README for more info
-  # config.analytics = false
+  config.analytics = true
 
   # Google Analytics tracking ID to gather usage statistics
-  # config.google_analytics_id = 'UA-99999999-1'
+  config.google_analytics_id = 'UA-55356404-1'
 
   # Date you wish to start collecting Google Analytic statistics for
   # Leaving it blank will set the start date to when ever the file was uploaded by
   # NOTE: if you have always sent analytics to GA for downloads and page views leave this commented out
-  # config.analytic_start_date = DateTime.new(2014, 9, 10)
+  config.analytic_start_date = DateTime.new(2023, 2, 13)
 
   # Enables a link to the citations page for a work
   # Default is false
