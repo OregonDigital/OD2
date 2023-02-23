5de76726674511be86204dfe4f4b346d993f71b4
Corey Gillen <corey.gillen@oregonstate.edu>
2023-02-23 08:48:09 -0800

    lint

--- app/controllers/oregon_digital/robots_controller.rb
+++ app/controllers/oregon_digital/robots_controller.rb
@@ -3,11 +3,10 @@
 module OregonDigital
   # This controller chooses which robots.txt to use based on an env var
   class RobotsController < ApplicationController
-
     def robots
-      robot_type = ENV["DISABLE_ROBOTS"] == "true" ? "staging" : "production"
+      robot_type = ENV.fetch('DISABLE_ROBOTS', 'true') == 'true' ? 'staging' : 'production'
       robots = File.read(Rails.root + "config/robots.#{robot_type}.txt")
-      render :plain => robots, :content_type => "text/plain"
+      render plain: robots, content_type: 'text/plain'
     end
   end
 end
