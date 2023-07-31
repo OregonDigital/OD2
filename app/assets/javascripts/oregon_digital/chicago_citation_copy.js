7aed2102787ddbc77525c21a8a81d27fcbcc824c
megwalt <mwalter2@uoregon.edu>
2023-07-31 08:15:04 -0700

    Add copy citation buttons next to citation titles

---
+++ app/assets/javascripts/oregon_digital/chicago_citation_copy.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    // Init clipboard-rails button for all copy URL buttons
+    var clipboard = new Clipboard('#chicago-copy-button');
+    // Init Bootstrap tooltips for all copy URL buttons
+    $('#copy-button').tooltip({
+      title: 'Copy text to clipboard'
+    });
+  
+    // On clipboard copy success
+    clipboard.on('success', function (e) {
+      // Set the tool tip to 'Copied' and enable it
+      $(e.trigger)
+        .attr('data-original-title', 'Copied')
+        .tooltip('show');
+  
+      // Set a timeout to hide and restore tooltip text after 1sec
+      setTimeout(function () {
+        $(e.trigger).tooltip('hide')
+          .attr('data-original-title', 'Copy text to clipboard');
+      }, 1000);
+    });
+  });
