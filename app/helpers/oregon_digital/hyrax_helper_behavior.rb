03d5b029f655c078bff9afcd0827e77cc7fd31ce
Corey Gillen <corey.gillen@oregonstate.edu>
2023-08-02 15:52:48 -0700

    Revert collection titles from id

--- app/helpers/oregon_digital/hyrax_helper_behavior.rb
+++ app/helpers/oregon_digital/hyrax_helper_behavior.rb
@@ -22,6 +22,10 @@ module OregonDigital::HyraxHelperBehavior
     sanitize combine_hits_with_links(html_content, show_link)
   end
 
+  def collection_title_from_id(field, _show_link = true)
+    SolrDocument.find(field).title_or_label
+  end
+
   # @return [String] the appropriate action url for our search form (depending on our current page)
   def search_form_action
     anchor = '#content'
