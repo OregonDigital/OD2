44b3889248fe2bc16490fc2f78a4c63cbdeec460
Corey Gillen <corey.gillen@oregonstate.edu>
2023-10-03 12:50:07 -0700

    Indexing update and some spec stuff

--- app/workers/reindex_one_lite.rb
+++ app/workers/reindex_one_lite.rb
@@ -10,6 +10,6 @@ class ReindexOneLite
   # A single pid is expected
   def perform(pid)
     w = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: pid)
-    w.update_index
+    Hyrax.index_adapter.save(resource: w)
   end
 end
