dee0c239e4731be6699b3066af7b4893278ca300
lsat12357 <lsato@uoregon.edu>
2023-12-15 09:18:48 -0800

    rubocop entry factory

--- spec/factories/bulkrax_entries.rb
+++ spec/factories/bulkrax_entries.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :bulkrax_csv_entry_work, class: 'Bulkrax::CsvEntry' do
-    identifier { "entry_work" }
+    identifier { 'entry_work' }
     type { 'Bulkrax::CsvEntry' }
     importerexporter { FactoryBot.build(:bulkrax_importer) }
     raw_metadata { {} }
@@ -10,15 +10,15 @@ FactoryBot.define do
   end
 
   factory :bulkrax_csv_entry_export, class: 'Bulkrax::CsvEntry' do
-    identifier { "csv_entry" }
+    identifier { 'csv_entry' }
     type { 'Bulkrax::CsvEntry' }
     importerexporter { FactoryBot.build(:bulkrax_exporter) }
     raw_metadata { {} }
     parsed_metadata { {
-      "title" => "Hiphopopotamus",
-      "rights_statement" => "http://rightsstatements.org/vocab/InC/1.0/",
-      "resource_type" => "http://purl.org/dc/dcmitype/Image",
-      "identifier": "abcde1234"
+      'title' => 'Hiphopopotamus',
+      'rights_statement' => 'http://rightsstatements.org/vocab/InC/1.0/',
+      'resource_type' => 'http://purl.org/dc/dcmitype/Image',
+      'identifier': 'abcde1234'
     } }
   end
 end
