91bdadc998aa7eb9b218414785678e0903fb5d89
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2023-12-15 11:57:29 -0800

    Update swap_visibility_to_private.rb

--- app/services/hyrax/workflow/swap_visibility_to_private.rb
+++ app/services/hyrax/workflow/swap_visibility_to_private.rb
@@ -5,7 +5,7 @@ module Hyrax
     # Hooked into workflow actions. When a work is tombstoned it runs this module and swaps the visibility to private
     module SwapVisibilityToPrivate
       def self.call(target:, **)
-        model = ActiveFedora::Base.find(target.id)
+        model = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: target.id)
         OregonDigital::SwapVisibilityJob.perform_later(model, Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE)
       end
     end
