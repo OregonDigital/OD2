4362791d1e86e8feb925bede71ed4d365f47c8b0
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2024-02-01 05:52:26 -0800

    Update content_block_controller.rb
    Remove AF::Base call

--- app/controllers/oregon_digital/content_block_controller.rb
+++ app/controllers/oregon_digital/content_block_controller.rb
@@ -6,7 +6,7 @@ module OregonDigital
     def update
       @content_block = ContentBlock.find_by(name: params[:name])
       @content_block.update(value: CGI.unescapeHTML(params[:content_block][:content]))
-      @work = ActiveFedora::Base.find(@content_block.name.split('-').last)
+      @work = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: @content_block.name.split('-').last)
       redirect_to polymorphic_path(@work)
     end
   end
