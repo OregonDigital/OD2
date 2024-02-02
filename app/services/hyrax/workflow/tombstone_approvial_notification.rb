6490a8bc40aa3bb3894f4562264a182d7539a686
straleyb <straleyb@onid.oregonstate.edu>
2024-02-02 13:59:11 -0800

    second pass at tombstones.

---
+++ app/services/hyrax/workflow/tombstone_approvial_notification.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module Workflow
+    # Tombstone Approval Notification
+    class TombstoneApprovalNotification < AbstractNotification
+      private
+
+      def subject
+        'Requested Tombstone has been approved'
+      end
+
+      def message
+        "The Tombstone request for #{title} (#{link_to work_id, document_path}) was approved by #{user.user_key}. #{comment}"
+      end
+
+      def users_to_notify
+        user_key = ActiveFedora::Base.find(work_id).depositor
+        super << ::User.find_by(email: user_key)
+      end
+    end
+  end
+end
