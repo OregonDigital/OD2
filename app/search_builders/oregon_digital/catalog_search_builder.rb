d03b3a95d108c141a79f0ad99af2f2ffdb8a186f
straleyb <straleyb@onid.oregonstate.edu>
2024-02-09 11:06:55 -0800

    Small bug fixes and filter tombstoned from catalog search

--- app/search_builders/oregon_digital/catalog_search_builder.rb
+++ app/search_builders/oregon_digital/catalog_search_builder.rb
@@ -3,5 +3,6 @@
 # Search Builder for the catalog
 class OregonDigital::CatalogSearchBuilder < ::SearchBuilder
   include OregonDigital::OnlyNonUserCollectionsBehavior
-  self.default_processor_chain += [:show_only_collections_not_created_users]
+  include OregonDigital::FilterTombstone
+  self.default_processor_chain += [:show_only_collections_not_created_users, :non_tombstoned_works]
 end
