057629f2083ab1089274b7687345f8b045db7faf
lamtu1 <tu.lam@oregonstate.edu>
2024-02-21 16:44:35 -0800

    Fix rubocop on file

--- app/inputs/label_and_uri_controlled_vocabulary_input.rb
+++ app/inputs/label_and_uri_controlled_vocabulary_input.rb
@@ -4,7 +4,7 @@
 class LabelAndUriControlledVocabularyInput < ControlledVocabularyInput
   private
 
-  def build_options_for_existing_row(attribute_name, index, value, options)
+  def build_options_for_existing_row(attribute_name, index, _value, options)
     solr_document_label_uri = SolrDocument.find(object.model.id).send(:label_uri_helpers, attribute_name)[index]
     options[:value] = "#{solr_document_label_uri['label']} (#{solr_document_label_uri['uri']})" || "Unable to fetch label for #{solr_document_label_uri['uri']}"
     options[:readonly] = true
