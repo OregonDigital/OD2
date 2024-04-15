e5ac4b02cac2e532f5c91b7a5a508aa992b8c90d
straleyb <straleyb@onid.oregonstate.edu>
2024-04-15 12:12:21 -0700

    update class

--- config/initializers/hacks/bulkrax_object_factory_interface_hack.rb
+++ config/initializers/hacks/bulkrax_object_factory_interface_hack.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  Hyrax::AttachFilesToWorkJob.class_eval do
+  Bulkrax::ObjectFactoryInterface.class_eval do
     def create
       attrs = transform_attributes
       attrs['full_size_download_allowed'] = attrs['full_size_download_allowed'].to_i
