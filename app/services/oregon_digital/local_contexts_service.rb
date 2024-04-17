b182706f9b23939b39adeff82528b6ecde42ad7f
lamtu1 <tu.lam@oregonstate.edu>
2024-04-17 15:02:14 -0700

    Add in some methods in the service

--- app/services/oregon_digital/local_contexts_service.rb
+++ app/services/oregon_digital/local_contexts_service.rb
@@ -17,5 +17,45 @@ module OregonDigital
     def select_sorted_all_options
       select_all_options.sort
     end
+
+    # METHOD: Parse out the label to get an array for the image location
+    def parse_labels_for_image(local_contexts_label)
+      # VARIABLE: Create an array to store the image location
+      local_contexts_arr = []
+
+      # LOOP: Loop through and parse out the label to get the image path
+      local_contexts_label.map do |lc|
+        local_contexts_arr << if lc.include?('(')
+                                lc[/\((.*?)\)/, 1].gsub(' ', '_').downcase
+                              else
+                                lc.gsub(' ', '_').downcase
+                              end
+      end
+
+      # RETURN: Return back the parse out content for image path
+      local_contexts_arr
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    # METHOD: Get the size of how many indicator to create for carousel
+    def get_carousel_indicator_size(lc_size)
+      # VARIABLE: Store the total indicators
+      indicator = 0
+
+      # CONDITION: Check the condition to make the size of the indicator to fit the display
+      if lc_size <= 4
+        indicator += 1
+      elsif (lc_size % 4).zero?
+        indicator = (lc_size / 4)
+      else
+        tmp = lc_size
+        tmp += 1 while tmp % 4 != 0
+        indicator = (tmp / 4)
+      end
+
+      # RETURN: Return the total size of indicators
+      indicator
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
--- spec/services/oregon_digital/local_contexts_service_spec.rb
+++ spec/services/oregon_digital/local_contexts_service_spec.rb
@@ -4,10 +4,38 @@ describe OregonDigital::LocalContextsService do
   # SETUP: Create a test variable
   let(:service) { described_class.new }
 
-  # TEST: Test to see if the service be able to fetch and return an active term
+  # TEST #1: Test to see if the service be able to fetch and return an active term
   describe '#all_labels' do
     it 'returns active terms' do
       expect(service.all_labels('http://localcontexts.org/label/tk-attribution/')).to eq ['TK Attribution (TK A)']
+    end
+  end
+
+  # TEST #2: Test to see if the service be able to parse out the labels
+  describe '#parse_labels_for_image' do
+    it 'returns parsable array path for image for display' do
+      expect(service.parse_labels_for_image(['TK Attribution (TK A)', 'Attribution Incomplete', 'BC Multiple Communities (BC MC)'])).to eq ['tk_a', 'attribution_incomplete', 'bc_mc']
+    end
+  end
+
+  # TEST #3: Test to see if the service be able to get the correct size for indicator
+  describe '#get_carousel_indicator_size' do
+    it 'returns 1 indicator for any number less or equal to 4' do
+      expect(service.get_carousel_indicator_size(3)).to eq 1
+      expect(service.get_carousel_indicator_size(4)).to eq 1
+      expect(service.get_carousel_indicator_size(2)).to eq 1
+    end
+
+    it 'returns many indicators for any number greater than 4 when divisible by 4' do
+      expect(service.get_carousel_indicator_size(20)).to eq 5
+      expect(service.get_carousel_indicator_size(12)).to eq 3
+      expect(service.get_carousel_indicator_size(32)).to eq 8
+    end
+
+    it 'returns many indicators for any number greater than 4 when it not divisible by 4' do
+      expect(service.get_carousel_indicator_size(23)).to eq 6
+      expect(service.get_carousel_indicator_size(13)).to eq 4
+      expect(service.get_carousel_indicator_size(38)).to eq 10
     end
   end
 end
