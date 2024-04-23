d9d9dd602863ca3e469b7d1f49d69ca95ff95de2
Corey Gillen <corey.gillen@oregonstate.edu>
2024-04-23 14:00:10 -0700

    Remove *_tsimv fields from indexers

--- app/indexers/generic_indexer.rb
+++ app/indexers/generic_indexer.rb
@@ -37,9 +37,7 @@ class GenericIndexer < Hyrax::WorkIndexer
       index_read_groups
       index_discover_groups
       solr_doc['all_text_timv'] = object.file_sets.map { |file_set| find_all_text_value(file_set, solr_doc) }
-      solr_doc['all_text_tsimv'] = object.file_sets.map { |file_set| find_all_text_value(file_set, solr_doc) }
       solr_doc['hocr_text_timv'] = object.file_sets.map { |file_set| find_hocr_text(file_set, solr_doc) }
-      solr_doc['hocr_text_tsimv'] = object.file_sets.map { |file_set| find_hocr_text(file_set, solr_doc) }
       solr_doc['file_format_sim'] = object.file_sets.map { |file_set| file_set.to_solr['file_format_sim'] } # Index file formats from file sets for faceting
       # for bulkrax
       solr_doc['bulkrax_identifier_sim'] = object.bulkrax_identifier
--- app/workers/re_extract_text_worker.rb
+++ app/workers/re_extract_text_worker.rb
@@ -43,10 +43,8 @@ class ReExtractTextWorker
     # Hyrax.persister.save(resource: file_set)
     # Hyrax.index_adapter.save(resource: file_set)
     # Hyrax.index_adapter.save(resource: file_set.parent)
-    Hyrax.index_adapter.delete(resource: file_set)
     file_set.save
-    Hyrax.index_adapter.delete(resource: file_set.parent) if file_set.parent
-    file_set.parent&.update_index
+    file_set.parent.update_index
   end
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
