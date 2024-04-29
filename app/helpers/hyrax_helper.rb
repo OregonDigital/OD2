bc20e55a654e35a79de56786a815b9d8890ae2cd
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2024-04-29 10:15:26 -0700

    Skeleton for content warnings (#3108)
    * Skeleton for content warnings
    
    * update small html nuances
    
    * lint
    
    * more linty lint
    
    * Space after comment
    
    * Updates collection to add content warning tab. Content warning tab guts.
    
    * removes helper override and adds method to hyrax helper

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -8,4 +8,13 @@ module HyraxHelper
   include OregonDigital::HyraxHelperBehavior
   include OregonDigital::ContactFormHelper
   include OregonDigital::RenderConstraintsHelperBehavior
+
+  # OVERRIDE FROM HYRAX. Adds in warning tab
+  def form_tabs_for(form:)
+    if form.instance_of? Hyrax::Forms::BatchUploadForm
+      %w[files metadata relationships warning]
+    else
+      %w[metadata files relationships warning]
+    end
+  end
 end
