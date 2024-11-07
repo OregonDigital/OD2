9a90d4fb5dfe910093b04524a49b24fe4fea676a
lamtu1 <tu.lam@oregonstate.edu>
2024-11-07 15:21:23 -0800

    Add more to the rake task

--- app/mailers/oregon_digital/failed_fetch_mailer.rb
+++ app/mailers/oregon_digital/failed_fetch_mailer.rb
@@ -4,9 +4,18 @@ module OregonDigital
   # MAILER: Email user/metadata team for failed fetch cv
   class FailedFetchMailer < ApplicationMailer
     # METHOD: Create an email and send a report to the user
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Style/IfInsideElse
     def failed_fetch_email
-      attachments[params[:filename]] = File.read("./tmp/failed_fetch/#{params[:filename]}") if File.exist?("./tmp/failed_fetch/#{params[:filename]}")
+      if params[:to] == 'kevin.jones@oregonstate.edu'
+        attachments[params[:filename]] = File.read("./tmp/#{params[:filename]}") if File.exist?("./tmp/#{params[:filename]}")
+      else
+        attachments[params[:filename]] = File.read("./tmp/failed_fetch/#{params[:filename]}") if File.exist?("./tmp/failed_fetch/#{params[:filename]}")
+      end
+
       mail(to: params[:to], subject: 'Oregon Digital: Failed Fetch Notice')
     end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Style/IfInsideElse
   end
 end
