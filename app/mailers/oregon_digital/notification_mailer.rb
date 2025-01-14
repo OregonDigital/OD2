755b5ef847852ab21c7c5d92b153de7f175d58e3
lsat12357 <lsato@uoregon.edu>
2025-01-14 10:48:34 -0800

    rework rake task

--- app/mailers/oregon_digital/notification_mailer.rb
+++ app/mailers/oregon_digital/notification_mailer.rb
@@ -4,10 +4,9 @@ module OregonDigital
   # Emails users that have reviews to do
   class NotificationMailer < ApplicationMailer
     def notification_email
-      @user = params[:user]
-      @need_keyword = params[:need_keyword]
-      @url = 'http://oregondigital.org/notifications'
-      mail(to: @user.email, subject: "Required #{@need_keyword} in Oregon Digital")
+      @email = params[:email]
+      @message = params[:message]
+      mail(to: @email, subject: "Required Actions in Oregon Digital")
     end
   end
 end
