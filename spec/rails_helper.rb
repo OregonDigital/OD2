a7f3fdd150dbb8e9b39ea68a2e6912b13bc87bd7
straleyb <straleyb@onid.oregonstate.edu>
2025-01-27 11:13:51 -0800

    remove required

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,7 +18,6 @@ require 'factory_bot_rails'
 require 'database_cleaner'
 require 'rspec/active_model/mocks'
 require 'webmock'
-require 'webdrivers'
 WebMock.allow_net_connect!
 # Add additional requires below this line. Rails is not loaded until this point!
 
