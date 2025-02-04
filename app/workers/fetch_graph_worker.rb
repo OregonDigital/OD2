bf6c2e368a2a0d48c03322244ef11eb4b70769b6
lamtu1 <tu.lam@oregonstate.edu>
2025-02-04 11:49:31 -0800

    Update the graph workers on feedback

--- app/workers/fetch_graph_worker.rb
+++ app/workers/fetch_graph_worker.rb
@@ -13,6 +13,9 @@ class FetchGraphWorker
     # Create an array to store failed fetch
     failed_cv = []
 
+    # Check for directory existence and clear out old files if needed
+    check_exist_directory
+
     # Here be dragons
     # A valkyrie work's controlled properties don't resolve to our OregonDigital::ControlledVocabularies::* objects
     # They resolve to RDF::URI, we need to find a way to get the right CV & index it
@@ -29,9 +32,12 @@ class FetchGraphWorker
         next
       end
     end
-    store_failed_fetch(failed_cv, pid) unless failed_cv.blank?
+
+    store_failed_fetch(pid) unless failed_cv.blank?
     work.update_index
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   # TODO: WILL INTEGRATE THIS WHEN REDOING EMAILING FOR THESE JOBS
   # def fetch_failed_callback(user, val)
@@ -47,28 +53,39 @@ class FetchGraphWorker
   end
 
   # METHOD: Create a method store all fails CV fetch
-  def store_failed_fetch(failed_cv, pid)
-    # CREATE: Create parent directory and check for exist
-    Dir.mkdir './tmp/failed_fetch/' unless File.exist? './tmp/failed_fetch/'
-
+  def store_failed_fetch(pid)
     # PATH: Setup a path for the txt file
-    path = "./tmp/failed_fetch/#{pid}.txt"
+    path = './tmp/failed_fetch/failed_pid.txt'
 
-    # SEARCH: Look for works to create a link
-    work = SolrDocument.find(pid)
-    url_path = Rails.application.routes.url_helpers.polymorphic_path(work)
-    url_path[0] = ''
-
-    # STORE: Add in data to txt file
+    # STORE: Add in pid to txt file
     File.open(path, 'w+') do |f|
-      f.write("Failed Fetch Record\n")
-      f.write("Work ID: #{pid}\n")
-      f.write("Work Link: #{[Rails.application.routes.url_helpers.root_url, url_path].join}\n")
-      failed_cv.each do |w|
-        f.write("Error Placement: #{w[:uri]}   Error on Value: #{w[:error]}\n")
+      f.write(pid.to_s)
+    end
+  end
+
+  # METHOD: To clear existing files in directory
+  def check_exist_directory
+    # CHECK: To see if directory exist
+    if File.exist? './tmp/failed_fetch/'
+      check_and_clear_exist_files
+    else
+      Dir.mkdir './tmp/failed_fetch/'
+    end
+  end
+
+  # METHOD: To clear existing files in directory
+  # rubocop:disable Style/GuardClause
+  def check_and_clear_exist_files
+    # GET: Get the list of files in the folder
+    files = Dir.entries('./tmp/failed_fetch') - ['.', '..']
+
+    # CHECK: Check if file not empty
+    unless files.empty?
+      files.each do |f|
+        file_path = File.join('./tmp/failed_fetch', f)
+        File.delete(file_path) if File.file?(file_path)
       end
     end
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Style/GuardClause
 end
