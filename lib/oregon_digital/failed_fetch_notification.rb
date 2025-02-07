bb1eef8c5c691b8c31ef268b6ee487e440444320
lamtu1 <tu.lam@oregonstate.edu>
2025-02-07 10:01:48 -0800

    Refactor and update more code

--- lib/oregon_digital/failed_fetch_notification.rb
+++ lib/oregon_digital/failed_fetch_notification.rb
@@ -7,13 +7,33 @@ module OregonDigital
   class FailedFetchNotification
     # METHOD: User mapping of all pid
     def user_map
-      @user_map ||= []
+      @user_map ||= {}
     end
 
     # METHOD: Add user to the 'map_user' list
     def add_list_of_users
       # CHECK: See if folder exist
       return unless folder_exist?
+
+      # FETCH: Get all of the pids from files
+      pid_arr = fetch_pids
+
+      # LOOP: Go through Solr Doc to find depositor to user_map
+      solr_docs = SolrDocument.find(pid_arr)
+      solr_docs.each do |doc|
+        user_map[doc['depositor_ssim'].first] << "#{doc['id']}.txt"
+      end
+    end
+
+    # METHOD: Add method to fetch metadeities
+    def fetch_metadeities
+      'kevin.jones@oregonstate.edu'
+    end
+
+    # METHOD: Fetch all pids
+    def fetch_pids
+      # SETUP: Add array to store pids
+      pids = []
 
       # LOOP: Loop through the directory to check on file
       Dir.foreach('./tmp/failed_fetch/') do |filename|
@@ -24,40 +44,16 @@ module OregonDigital
         pid = filename.to_s.gsub('.txt', '')
 
         # STORE: Add user to user_map
-        user_map << pid
+        pids << pid.to_s
       end
-    end
 
-    # METHOD: Add method to fetch depositor
-    def fetch_depositor(pid)
-      work = SolrDocument.find(pid)
-      work['depositor_ssim'].first
-    end
-
-    # METHOD: Add method to fetch metadeities
-    def fetch_metadeities
-      'kevin.jones@oregonstate.edu'
-    end
-
-    # METHOD: Add method to fetch a failed file
-    def fetch_file(pid)
-      Dir.foreach('./tmp/failed_fetch/') do |filename|
-        # CHECK: Skip hidden files and special directories
-        next if ['.', '..'].include?(filename)
-
-        # GET: Get the id from the txt file
-        file_id = filename.to_s.gsub('.txt', '')
-
-        # CHECK: If the pid matches, return the filename
-        return filename if file_id == pid
-      end
+      pids
     end
 
     # Create zip file to send to metadeities
     def create_zip_file
       # CHECK: Check if file exist if not create a new one
-      File.delete('./tmp/failed_fetch_items.zip') if File.file?('./tmp/failed_fetch_items.zip')
-      zip_file = File.new('./tmp/failed_fetch_items.zip', 'w+')
+      zip_file = zip_exist
 
       # ZIP: Add file and zip them together
       Zip::File.open(zip_file.path, Zip::File::CREATE) do |zip|
@@ -74,6 +70,12 @@ module OregonDigital
     # METHOD: Return a bool to see if folder exist
     def folder_exist?
       File.exist?('./tmp/failed_fetch/') && !Dir.empty?('./tmp/failed_fetch/')
+    end
+
+    # METHOD: To check if zip file exist
+    def zip_exist
+      File.delete('./tmp/failed_fetch_items.zip') if File.file?('./tmp/failed_fetch_items.zip')
+      File.new('./tmp/failed_fetch_items.zip', 'w+')
     end
   end
 end
