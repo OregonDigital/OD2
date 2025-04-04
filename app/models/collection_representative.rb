f5d6704b9db39ce89baf610f4ef512a89f67ecaf
straleyb <straleyb@onid.oregonstate.edu>
2025-04-04 12:30:39 -0700

    rescue from LDP::Gone is fileset doesnt exist

--- app/models/collection_representative.rb
+++ app/models/collection_representative.rb
@@ -3,6 +3,10 @@
 # Representative fileset entry for a collection
 class CollectionRepresentative < ApplicationRecord
   def fs_title
-    fileset_id.empty? ? '' : Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: fileset_id, use_valkyrie: false).title.first
+    begin
+      fileset_id.empty? ? '' : Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: fileset_id, use_valkyrie: false).title.first
+    rescue Ldp::Gone
+      ''
+    end
   end
 end
