6dfe3352a89ca12a20b0745aec7d0fcda0c7d398
lsat12357 <lsato@uoregon.edu>
2025-04-10 11:36:46 -0700

    changes to verify delivery (#3319)
    * changes to verify delivery
    
    * view not needed
    
    ---------
    
    Co-authored-by: Brandon R. Straley <straleyb@onid.oregonstate.edu>

--- app/controllers/concerns/oregon_digital/importer_controller_behavior.rb
+++ app/controllers/concerns/oregon_digital/importer_controller_behavior.rb
@@ -3,21 +3,6 @@
 module OregonDigital
   # adds custom behavior to bulkrax importer
   module ImporterControllerBehavior
-    def show_errors
-      @importer = Bulkrax::Importer.find(params[:importer_id])
-      @errors = compile_errors
-      render 'oregon_digital/importer/show_errors'
-    end
-
-    def verify
-      @importer = Bulkrax::Importer.find(params[:importer_id])
-      bvs = OregonDigital::BatchVerificationService.new(work_ids.map { |x| x[:work_id] }.reject(&:nil?))
-      bvs.verify
-      redirect_to importer_path(@importer.id), notice: 'Verification jobs are enqueued. Jobs may be delayed depending on number/type of jobs already enqueued; please wait 5-10 minutes before checking results.'
-    end
-
-    # returns an array of hashes, eg { work_id: 'abcde1234', entry_identifier: '80-101' }
-    # if solr returns an empty array, the work_id will be nil
     def work_ids
       ids = []
       @importer.entries.pluck(:identifier).each do |i|
@@ -27,23 +12,21 @@ module OregonDigital
       ids
     end
 
-    def compile_errors
-      errors = {}
-      work_ids.each do |item|
-        if item[:work_id].nil?
-          errors[item[:entry_identifier]] = { solr: ['Unable to load work for this entry.'] }
-        else
-          errors[item[:work_id]] = retrieve_errors(item[:work_id])
-        end
-      end
-      errors
+    # show_errors?importer_id=9&time=202504011111
+    def show_errors
+      render file: report_path, layout: false, content_type: 'application/json'
     end
 
-    def retrieve_errors(id)
-      work = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: id)
-      work.all_errors
-    rescue Valkyrie::Persistence::ObjectNotFoundError
-      { valkyrie: ['Unable to load work.'] }
+    def report_path
+      File.join(OD2::Application.config.local_path, "verify/batch_#{params[:importer_id]}_report_#{params[:time]}.json")
+    end
+
+    def verify
+      @importer = Bulkrax::Importer.find(params[:importer_id])
+      ids = work_ids.map { |x| x[:work_id] }.reject(&:nil?)
+      bvs = OregonDigital::BatchVerificationService.new(ids, { batch_id: @importer.id, user_mail: current_user.email, size: ids.size, reporter: 'OregonDigital::ImporterVerificationReporter' })
+      bvs.verify
+      redirect_to importer_path(@importer.id), notice: 'Verification jobs are enqueued. Jobs may be delayed depending on number/type of jobs already enqueued; an email will be sent to you when the report is complete.'
     end
 
     def importers_list
