b798763ec53ea41fde453ef4f86b798f17609a9d
Tu Lam <tu.lam@oregonstate.edu>
2025-04-10 11:50:53 -0700

    Revert homosaurus spec

--- spec/oregon_digital/controlled_vocabularies/vocabularies/homosaurus_spec.rb
+++ spec/oregon_digital/controlled_vocabularies/vocabularies/homosaurus_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe OregonDigital::ControlledVocabularies::Vocabularies::Homosaurus do
   let(:vocab) { described_class }
-  let(:data) { [{ 'skos:prefLabel': [{ '@language': 'en', '@value': 'blah' }] }.with_indifferent_access] }
+  let(:data) { [{ 'skos:prefLabel': { '@language': 'en', '@value': 'blah' } }.with_indifferent_access] }
   let(:query) { 'www.blah.com/blah' }
 
   it { expect(vocab.expression).to be_kind_of(Regexp) }
