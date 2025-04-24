f4342e4040e548b23b3b84bb202311c9e90507f1
straleyb <straleyb@onid.oregonstate.edu>
2025-04-24 08:22:16 -0700

    Updates home page

--- app/controllers/concerns/oregon_digital/works_controller_behavior.rb
+++ app/controllers/concerns/oregon_digital/works_controller_behavior.rb
@@ -16,8 +16,8 @@ module OregonDigital
     # We can use Hyrax::WorksControllerBehavior definition and add on additional params we want
     def attributes_for_actor
       attributes = super
-      oembed_urls = params.fetch(:oembed_urls, [])
-      attributes[:oembed_urls] = oembed_urls
+      #oembed_urls = params.fetch(:oembed_urls, [])
+      #attributes[:oembed_urls] = oembed_urls
 
       attributes
     end
