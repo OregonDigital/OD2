d62abc4bcb5d4b80c54dcd564da85bce5c740884
lsat12357 <lsato@uoregon.edu>
2025-07-11 10:31:11 -0700

    try global stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,13 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before :each do
+    WebMock.globally_stub_request { |request|
+      if request.uri.to_s =~ /.*tshealth/
+        { status: 200, body: '', headers: {} }
+      end
+    }
+  end
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
