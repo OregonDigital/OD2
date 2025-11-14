7fbd7a16dac110ac3754bbb13220f6aad0c26d9b
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2025-11-14 12:08:36 -0800

    Update audios_controller.rb

--- app/controllers/hyrax/audios_controller.rb
+++ app/controllers/hyrax/audios_controller.rb
@@ -25,13 +25,13 @@ module Hyrax
 
     def allow_listed_ip_addr?
       ips = ENV.fetch('IP_TURNSTILE_BYPASS', '') # '127.0.0.1-127.255.255.255,66.249.64.0-66.249.79.255'
-      ranges = ips.split(',')
-      ranges.each do |range|
-        range = range.split('-')
-        range = (IPAddr.new(range[0]).to_i..IPAddr.new(range[1]).to_i)
-        return true if range.include?(request.remote_ip)
-      end
-      false
+    ranges = ips.split(',')
+    ranges.each do |range|
+      range = range.split('-')
+      range = (IPAddr.new(range[0]).to_i..IPAddr.new(range[1]).to_i)
+      return true if range.include?(IPAddr.new(request.remote_ip).to_i)
+    end
+    false
     end
 
     self.curation_concern_type = ::Audio
