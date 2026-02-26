00196b334190cf19a4bacdf148543b79caefce14
Corey Gillen <corey.gillen@oregonstate.edu>
2026-02-26 10:31:45 -0800

    Fix some broken specs

--- config/initializers/hacks/blacklight_oai_solrdoc_wrapper_patch.rb
+++ config/initializers/hacks/blacklight_oai_solrdoc_wrapper_patch.rb
@@ -8,7 +8,7 @@ Rails.application.config.to_prepare do
       return next_set(options[:resumption_token]) if options[:resumption_token]
 
       if selector == :all
-        response = @controller.repository.search(conditions(options))
+        response = search_service.repository.search(conditions(options))
 
         if limit && response.total > limit
           return select_partial(BlacklightOaiProvider::ResumptionToken.new(options.merge(last: 0), nil, response.total))
@@ -20,16 +20,16 @@ Rails.application.config.to_prepare do
         end
         docs
       else
-        query = @controller.search_builder.where(document_model.unique_key => selector).query
+        query = search_service.search_builder.where(document_model.unique_key => selector).query
         # add requested urls to document
-        doc = @controller.repository.search(query).documents.first
+        doc = search_service.repository.search(query).documents.first
         doc['identifier_tesim'] = construct_urls(doc)
         doc
       end
     end
 
     def select_partial(token)
-      records = @controller.repository.search(token_conditions(token)).documents
+      records = search_service.repository.search(token_conditions(token)).documents
       raise ::OAI::ResumptionTokenException unless records
       # add requested urls to documents
       records.each do |r|
