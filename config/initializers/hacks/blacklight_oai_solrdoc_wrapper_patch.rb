50681fa314abb21b5240c6d21819f3fd850ef3ae
Corey Gillen <corey.gillen@oregonstate.edu>
2026-02-27 10:55:43 -0800

    Rework blacklight oai solr doc wrapper specs based on upstream

--- config/initializers/hacks/blacklight_oai_solrdoc_wrapper_patch.rb
+++ config/initializers/hacks/blacklight_oai_solrdoc_wrapper_patch.rb
@@ -15,16 +15,18 @@ Rails.application.config.to_prepare do
         end
         # add requested urls to document
         docs = response.documents
-        docs.each do |doc|
-          doc['identifier_tesim'] = construct_urls(doc)
+        docs.map do |doc|
+          doc_hash = doc.to_h
+          doc_hash['identifier_tesim'] = construct_urls(doc)
+          SolrDocument.new(doc_hash)
         end
-        docs
       else
         query = search_service.search_builder.where(document_model.unique_key => selector).query
         # add requested urls to document
         doc = search_service.repository.search(query).documents.first
-        doc['identifier_tesim'] = construct_urls(doc)
-        doc
+        doc_hash = doc.to_h
+        doc_hash['identifier_tesim'] = construct_urls(doc)
+        SolrDocument.new(doc_hash)
       end
     end
 
@@ -32,8 +34,10 @@ Rails.application.config.to_prepare do
       records = search_service.repository.search(token_conditions(token)).documents
       raise ::OAI::ResumptionTokenException unless records
       # add requested urls to documents
-      records.each do |r|
-        r['identifier_tesim'] = construct_urls(r)
+      records = records.map do |r|
+        r_hash = r.to_h
+        r_hash['identifier_tesim'] = construct_urls(r)
+        SolrDocument.new(r_hash)
       end
       OAI::Provider::PartialResult.new(records, token.next(token.last + limit))
     end
--- spec/blacklight_oai_provider/solr_document_wrapper_spec.rb
+++ spec/blacklight_oai_provider/solr_document_wrapper_spec.rb
@@ -4,41 +4,56 @@ RSpec.describe BlacklightOaiProvider::SolrDocumentWrapper do
   subject(:wrapper) { described_class.new(controller, options) }
 
   let(:options) { {} }
-  let(:controller) { CatalogController.new }
-  let(:search_service) { double }
-  let(:repository) { double }
-  let(:response) { double }
-  let(:doc) { SolrDocument.new(attributes) }
-  let(:attributes) do
-    {
-      'id' => 'abcde1234',
-      'has_model_ssim' => ['Image'],
-      'thumbnail_path_ss' => '/downloads/abcde1234?file=thumbnail',
-      'member_ids_ssim' => ['f0abcde1234']
-    }
-  end
-  let(:documents) { [doc] }
+  let(:controller_class) { CatalogController }
+  let(:controller) { instance_double(controller_class) }
+  let(:blacklight_config) { Blacklight::Configuration.new }
+
   let(:user) { create(:user) }
   let(:ability) { Ability.new(user) }
-  let(:oai_collection_type) { create(:collection_type, machine_id: :oai_set) }
-  let(:user_collection_type) { create(:collection_type, machine_id: :user_collection) }
   let(:uri_show) { 'https://localhost:3000/concern/images/abcde1234' }
   let(:uri_thumb) { 'http://localhost:8080/iiif/f0/ab/cd/e1/23/4-jp2.jp2/full/430,/0/default.jpg' }
 
-  before do
-    allow(controller).to receive(:params).and_return({})
-    allow(controller).to receive(:current_ability).and_return(ability)
-    allow(Hyrax::CollectionType).to receive(:find).with(machine_id: :oai_set).and_return(oai_collection_type)
-    allow(Hyrax::CollectionType).to receive(:find).with(machine_id: :user_collection).and_return(user_collection_type)
-    allow(controller).to receive(:search_service).and_return(search_service)
-    allow(search_service).to receive(:repository).and_return(repository)
-    allow(repository).to receive(:search).and_return(response)
-    allow(response).to receive(:documents).and_return(documents)
-    allow(response).to receive(:total).and_return(1)
+  shared_context "timestamp_searches" do
+    let(:expected_timestamp) { '2014-02-03 18:42:53.056000000 +0000' }
+    let(:attributes) do
+      {
+        'id' => 'abcde1234',
+        'has_model_ssim' => ['Image'],
+        'thumbnail_path_ss' => '/downloads/abcde1234?file=thumbnail',
+        'member_ids_ssim' => ['f0abcde1234'],
+        'timestamp' => expected_timestamp
+      }
+    end
+    let(:repository) { instance_double(Blacklight::Solr::Repository) }
+    let(:search_builder) { instance_double(Blacklight::SearchBuilder) }
+    let(:search_service) { instance_double(Blacklight::SearchService) }
+    let(:documents) { [SolrDocument.new(attributes)] }
+    let(:response) { OpenStruct.new(documents: documents, total: documents.length) }
+
+    before do
+      allow(controller).to receive_messages(params: {}, blacklight_config: blacklight_config)
+      # allow(controller).to receive(:params).and_return({})
+      allow(controller).to receive(:current_ability).and_return(ability)
+      allow(controller).to receive(:search_service).and_return(search_service)
+      allow(search_service).to receive_messages(repository: repository, search_builder: search_builder)
+    end
   end
 
   describe '#find' do
+    include_context "timestamp_searches"
+
+    subject(:result) { wrapper.find(selector) }
+
     context 'when selector is :all' do
+      let(:selector) { :all }
+      let(:query) { {} }
+      let(:limit) { 1 }
+
+      before do
+        allow(search_builder).to receive_messages(merge: search_builder, query: query)
+        allow(repository).to receive(:search).with(query).and_return(response)
+      end
+
       it 'returns a document with show and thumb uris' do
         results = wrapper.find(:all)
         expect(results.first['identifier_tesim']).to include(uri_show)
@@ -47,16 +62,34 @@ RSpec.describe BlacklightOaiProvider::SolrDocumentWrapper do
     end
 
     context 'when selector is an individual record' do
+      let(:selector) { 'abcde1234' }
+      let(:query) { {} }
+
+      before do
+        allow(search_builder).to receive(:query).and_return(query)
+        allow(repository).to receive(:search).with(query).and_return(response)
+        allow(search_builder).to receive(:where).with({'id' => selector}).and_return(search_builder)
+      end
+
       it 'returns a document with show and thumb uris' do
-        results = wrapper.find('abcde1234')
+        results = wrapper.find(selector)
         expect(results['identifier_tesim']).to include(uri_show)
         expect(results['identifier_tesim']).to include(uri_thumb)
       end
     end
+
   end
 
   describe '#select_partial' do
+    include_context "timestamp_searches"
     let(:token) { BlacklightOaiProvider::ResumptionToken.new({ last: 0 }, nil, 1) }
+    let(:next_response) { OpenStruct.new(documents: documents, total: documents.length) }
+
+    before do
+      allow(search_builder).to receive_messages(merge: search_builder, query: {})
+      allow(repository).to receive(:search).with({}).and_return(response)
+      allow(repository).to receive(:search).with(hash_including(start: 0)).and_return(next_response)
+    end
 
     it 'returns a document with show and thumb uris' do
       partial_result = wrapper.select_partial(token)
