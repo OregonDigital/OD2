d576c7a4e65e1c110009b81afd34a054db06c909
Tu Lam <tu.lam@oregonstate.edu>
2026-02-27 14:51:20 -0800

    Add in modification to modal close button

---
+++ app/assets/javascripts/oregon_digital/facets/facets_button.js
@@ -0,0 +1,35 @@
+// ON LOAD: When page refresh, it will switch on click for the label on facets display button
+$(function () {
+  // CHANGE: On click, change the button label between 'More' and 'Less'
+  $(".secondary-facets, .secondary-advanced-facets").on("click", function () {
+    // DECLARE: Get the modify text
+    const moreText = $(this).data("more-text");
+    const lessText = $(this).data("less-text");
+
+    // CHECK: Get the current text and check the value to switch on display when click
+    const currentText = $.trim($(this).text());
+    $(this).text(currentText === moreText ? lessText : moreText);
+  });
+});
+
+// ON LOAD: When page load, activate the switching of button click on modal
+$(function () {
+  // CHANGE: On click, change the behavior of the button when close facet
+  $("#modal-close-btn").on("click", function (event) {
+    // DECLARE: Get the URL currently on page
+    const url = new URL(window.location.href);
+
+    // CONDITION: If URL contain facet
+    if (url.pathname.indexOf('/facet/') !== -1) {
+      // Override the original Bootstrap
+      event.preventDefault();
+
+      // STRIP: Strip the URL for base URL
+      const base_url = url.pathname.split('/facet/')[0];
+      const locale = url.searchParams.get('locale');
+      window.location.href = base_url + (locale ? '?locale=' + locale : '');
+    }
+
+    // ELSE: Do it normally with the Bootstrap
+  });
+});
